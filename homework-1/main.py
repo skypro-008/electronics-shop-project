c43047c14759e97ad47a94848bdd19c06b1eaebd
alexandr50 <dmitrymaslov2016@mail.ru>
2023-03-07 23:11:55 +0300

    Add tests for class Item

--- homework-1/main.py
+++ homework-1/main.py
@@ -16,3 +16,4 @@ if __name__ == '__main__':
     print(item2.price)  # 20000
 
     print(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+def test_validate_data():
+    item = Item('short', 10, 4)
+    assert type(item.name) == str
+    assert type(item.price) == int
+    assert type(item.quantity) == int
+    assert item.calculate_total_price() == 40
+    item.pay_rate = 0.5
+    item.apply_discount()
+    item.price = 5.0
