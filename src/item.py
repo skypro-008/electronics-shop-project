1c711259953f062391098d3c14becef04eca6d05
Fedor Metsger <fedor.metsger@gmail.com>
2023-03-08 15:48:42 +0300

    Add Item methods implementation and tests

--- src/item.py
+++ src/item.py
@@ -13,18 +13,33 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        if not isinstance(name, str) or name == "":
+            raise ValueError("Название товара должно быть непустой строкой")
+        if not (isinstance(price, float) or isinstance(price, int)) or price <= 0:
+            raise ValueError("Цена должна быть положительным числом > 0")
+        if not isinstance(quantity, int) or quantity < 1:
+            raise ValueError("Количество товара должно быть целым числом > 0")
 
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
+
+    def __repr__(self):
+        return f"Item({self.name}, {self.price}, {self.quantity})"
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= Item.pay_rate
+
+    def get_name(self):
+        return self.name
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,45 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item_phone():
+    return Item("phone", 10000, 5)
+def test_item_init1(item_phone):
+    """Тестирует метод Item.__init__ с правильными значениями"""
+    assert item_phone.get_name() == "phone"
+    assert item_phone.price == 10000
+    assert item_phone.quantity == 5
+
+def test_item_init2():
+    """Тестирует метод Item.__init__ с некорректным названием"""
+    with pytest.raises(ValueError):
+        Item(2, 2, 2)
+    with pytest.raises(ValueError):
+        Item("", 2, 2)
+
+
+def test_item_init3():
+    """Тестирует метод Item.__init__ с некорректным названием"""
+    with pytest.raises(ValueError):
+        Item("phone", "0.0", 2)
+    with pytest.raises(ValueError):
+        Item("phone", 0, 2)
+
+def test_item_init4():
+    """Тестирует метод Item.__init__ с некорректным названием"""
+    with pytest.raises(ValueError):
+        Item("phone", 1000, "asd")
+    with pytest.raises(ValueError):
+        Item("phone", 1000, 0)
+
+def test_item_calc_tp(item_phone):
+    """Тестирует Item.calculate_total_price()"""
+    assert item_phone.calculate_total_price() == 50000
+
+def test_item_apply_disc(item_phone):
+    """Тестирует Item.apply_discount()"""
+    Item.pay_rate = 0.7
+    item_phone.apply_discount()
+    assert item_phone.price == 7000
