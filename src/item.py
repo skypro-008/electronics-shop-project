9060932e214cfd8a5b67196c8b10eece0006f3a8
Xena Ship <shipxena@gmail.com>
2023-03-09 18:57:35 +0300

    ans

--- src/item.py
+++ src/item.py
@@ -6,6 +6,9 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
+        Item.name = name
+        Item.price = price
+        Item.quantity = quantity
         """
         Создание экземпляра класса item.
 
@@ -13,7 +16,6 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +23,10 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
-    def apply_discount(self) -> None:
+    def apply_discount(self, pay_rate) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        return self.price - self.price * pay_rate / 100
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,26 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def x():
+    return Item('computer', 100, 10)
+
+
+@pytest.fixture
+def pay_rate():
+    return 1.0
+
+
+def test_item_init(x):
+    assert x.name == 'computer'
+
+
+def test_item_apply_discount(x, pay_rate):
+    assert x.apply_discount(pay_rate) == 99
+
+
+def test_item_calculate_total_price(x):
+    assert x.calculate_total_price() == 1000
+
