7f23ea889c87f2c28e021e53ee2b2ab93533522e
EAKapitonov <Kapitonov281087@mail.tu>
2023-03-10 00:21:30 +0900

    HW 13.1 full

--- src/item.py
+++ src/item.py
@@ -4,16 +4,17 @@ class Item:
     """
     pay_rate = 1.0
     all = []
-
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name:str, price:float, quantity:int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +22,10 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * Item.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
+from src.item import Item
+import pytest
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+@pytest.fixture
+def name():
+    return "iphone 5S"
+@pytest.fixture
+def price():
+    return 25000.0
+@pytest.fixture
+def quantity():
+    return 15
+
+def test_Item(name, price, quantity):
+    product = Item(name, price, quantity)
+    Item.pay_rate = 0.85
+    assert product.name == "iphone 5S"
+    assert product.price == 25000
+    assert product.calculate_total_price() == 375000
+    product.apply_discount()
+    assert product.price == 21250
