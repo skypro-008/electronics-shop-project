cbb2dce92d0be5e5195054ada1a4429553fc030e
EAKapitonov <Kapitonov281087@mail.tu>
2023-03-10 00:34:40 +0900

    HW 13.1 full

--- tests/test_item.py
+++ tests/test_item.py
@@ -6,7 +6,7 @@ def name():
     return "iphone 5S"
 @pytest.fixture
 def price():
-    return 25000.0
+    return 20000.0
 @pytest.fixture
 def quantity():
     return 15
@@ -15,7 +15,7 @@ def test_Item(name, price, quantity):
     product = Item(name, price, quantity)
     Item.pay_rate = 0.85
     assert product.name == "iphone 5S"
-    assert product.price == 25000
-    assert product.calculate_total_price() == 375000
+    assert product.price == 20000.0
+    assert product.calculate_total_price() == 300000
     product.apply_discount()
-    assert product.price == 21250
+    assert product.price == 17000
