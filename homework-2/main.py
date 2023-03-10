7cc355339d97f35647c0696a1e3ff43cc9593fd2
kirillskyeng <kirill.netreba@skyeng.ru>
2023-03-10 14:07:44 +0300

    Add homework-2

---
+++ homework-2/main.py
@@ -0,0 +1,22 @@
+from src.item import Item
+
+if __name__ == '__main__':
+    item = Item('Телефон', 10000, 5)
+
+    # длина наименования товара меньше 10 символов
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+    # длина наименования товара больше 10 символов
+    item.name = 'СуперСмартфон'
+    # Exception: Длина наименования товара превышает 10 символов.
+
+    Item.instantiate_from_csv()  # создание объектов из данных файла
+    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
