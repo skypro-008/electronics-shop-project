9409eda0466d4a6ec1b921ebde783aa2104d489d
Ilia Perfilov <ind.climb@gmail.com>
2023-03-16 14:45:47 +0300

    item.py modified - added methods

--- src/item.py
+++ src/item.py
@@ -1,9 +1,26 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+
+    @staticmethod
+    def string_to_number(str_num: str):
+        try:
+            return int(float(str_num))
+        except:
+            return 'Введено некорректное значение'
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all.clear()
+        with open('../src/items.csv', newline='') as f:
+            for row in csv.DictReader(f):
+                cls(row['name'], row['price'], row['quantity'])
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,10 +30,22 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        if len(new_name) > 10:
+            print('Длина наименования товара превышает 10 символов.')
+            # raise Exception('Длина наименования товара превышает 10 символов.')
+        else:
+            self.__name = new_name
 
     def calculate_total_price(self) -> float:
         """
