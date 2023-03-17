89a7624598209f2d898f6fa6b5e61030d4e40e96
sergey <szaprudnov@hotmail.com>
2023-03-17 18:21:48 +0300

    examination & add foo

--- src/item.py
+++ src/item.py
@@ -48,20 +48,18 @@ class Item:
         """
         self.price *= self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls, url='../src/items.csv'):
+        cls.all.clear()
+        with open(url, 'r', encoding='windows-1251') as file:
+            file_reader = DictReader(file, delimiter=',')
+            for value in file_reader:
+                name, price, quantity = value.values()
+                cls(name, cls.string_to_number(price), cls.string_to_number(quantity))
 
-@classmethod
-def instantiate_from_csv(cls, url='../src/items.csv'):
-    cls.all.clear()
-    with open(url, 'r', encoding='windows-1251') as file:
-        file_reader = DictReader(file, delimiter=',')
-        for value in file_reader:
-            name, price, quantity = value.values()
-            cls(name, cls.string_to_number(price), cls.string_to_number(quantity))
-
-
-@staticmethod
-def string_to_number(string):
-    return int(float(string))
+    @staticmethod
+    def string_to_number(string):
+        return int(float(string))
 
 
 def __repr__(self):
