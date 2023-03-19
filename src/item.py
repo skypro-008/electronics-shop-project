3ee9b7df0bf5d07b7c4e335dfbf0c5db662b1099
Great <great.goodwin.92@gmail.com>
2023-03-19 18:05:44 +0300

    Выполнены 1-3 домашки, добавлен файл к 4й

--- src/item.py
+++ src/item.py
@@ -1,9 +1,14 @@
+from csv import DictReader
+from pathlib import Path
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+    csv_file = Path.joinpath(Path(__file__).parent, 'items.csv')
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,7 +18,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self._name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        try:
+            if len(name) > 10:
+                raise ValueError('Длина наименования товара превышает 10 символов')
+            self._name = name
+        except ValueError as exception:
+            print(f'Exception: {exception}')
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +42,35 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= Item.pay_rate
+
+    @staticmethod
+    def string_to_number(num: str):
+        """
+        Метод возвращающий число из числа-строки
+        """
+        return int(float(num))
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Класс-метод инициализирующий экземпляры класса Item данными из файла src/items.csv
+        """
+        with open(cls.csv_file, 'r') as file:
+            reader = DictReader(file)
+            for row in reader:
+                cls(row['name'],
+                    cls.string_to_number(row['price']),
+                    cls.string_to_number(row['quantity']))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self._name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self._name
---
+++ src/src/phone.py
@@ -0,0 +1,46 @@
+from src.item import Item
+
+
+class Phone(Item):
+
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+        """
+        Создание экземпляра класса Phone.
+
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        :param number_of_sim: Количество поддерживаемых сим-карт
+        """
+        super().__init__(name, price, quantity)
+        self._number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, name: str):
+        try:
+            if len(name) > 10:
+                raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
+            self._name = name
+        except ValueError as exception:
+            print(f'Exception: {exception}')
+
+    def __add__(self, new_item):
+
+        if isinstance(new_item, Phone):
+            return Phone(self.quantity + new_item.quantity)
+
+        elif isinstance(new_item, Item):
+            return Item(self.quantity + new_item.quantity)
+
+        else:
+            raise TypeError('Нельзя сложить Phone или Item с экземплярами не Phone или Item классов.')
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self._name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self._name
