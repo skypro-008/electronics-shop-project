34885b650abc88a1bdd4ca5c28ea9b82818ff10a
Xena Ship <shipxena@gmail.com>
2023-03-19 15:50:54 +0300

    tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -7,17 +7,17 @@ def x():
     return Item('computer', 100, 10)
 
 
-@pytest.fixture
-def pay_rate():
-    return 1.0
-
-
 def test_item_init(x):
     assert x.name == 'computer'
+    assert x.price == 100
+    assert x.quantity == 10
+    assert Item.all[0] is x
 
 
-def test_item_apply_discount(x, pay_rate):
-    assert x.apply_discount(pay_rate) == 99
+def test_item_apply_discount(x):
+    Item.pay_rate = 0.85
+    x.apply_discount()
+    assert x.price == 85
 
 
 def test_item_calculate_total_price(x):
