38eabcbce2052315a95a7fb92310bd86fc4fa788
sergey <szaprudnov@hotmail.com>
2023-03-22 12:52:25 +0300

    доработан репр и добавлен стр

--- src/item.py
+++ src/item.py
@@ -62,5 +62,8 @@ class Item:
         return int(float(string))
 
 
-#def __repr__(self):
- #   return f'{self.name}, {self.price}, {self.quantity}'
+    def __repr__(self):
+        return f'{self.name}, {self.price}, {self.quantity}'
+
+    def __str__(self):
+        return f'{self.name}'
