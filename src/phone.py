b8873cfab373b0676fdf58add1d3d096861ea200
sergey <szaprudnov@hotmail.com>
2023-03-29 19:20:49 +0300

    add foo

--- src/phone.py
+++ src/phone.py
@@ -5,3 +5,22 @@ class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, sim: int):
         super().__init__(name, price, quantity)
         self.__sim = sim
+
+    @property
+    def sim(self):
+        return self.__sim
+
+    @sim.setter
+    def sim(self, value: int):
+        try:
+            if 1 <= value == int(value):
+                self.__sim = value
+            else:
+                raise ValueError
+        except ValueError:
+            print("Должно быть целым числом и больше нуля")
+            return self.__sim
+
+    def __repr__(self):
+        super().__repr__()
+        return f"{self.__class__.__name__}('{self.name}', {int(self.price)}, {self.quantity}, {self.__sim})"
