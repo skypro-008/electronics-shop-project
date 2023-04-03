5fd275062fef50656cf91fa8f13b5520fb888a0d
sergey <szaprudnov@hotmail.com>
2023-04-03 20:22:52 +0300

    add class MixinLog & Keyboard

---
+++ src/keyboard.py
@@ -0,0 +1,21 @@
+from src.item import Item
+
+
+class MixinLog:
+    def __init__(self):
+        self.__language = 'EN'
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == 'EN':
+            self.__language == 'RU'
+        else:
+            self.__language == 'EN'
+        return self
+
+
+class Keyboard(Item, MixinLog):
+    pass
