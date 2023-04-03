1564c565d6e65979b6fefbe4b084186e2b9470cf
sergey <szaprudnov@hotmail.com>
2023-04-03 20:39:01 +0300

    add test_keyboard

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -1,7 +1,17 @@
 import pytest
 from src.keyboard import KeyBoard
 
+
 @pytest.fixture
 def test_data_one():
     return 'Dark Project KD87A', 9600, 5
 
+
+def test_keyboard(test_data_one):
+    kl = KeyBoard(*test_data_one)
+    assert str(kl) == "Dark Project KD87A"
+    assert str(kl.language) == "EN"
+    kl.change_lang()
+    assert str(kl.language) == "RU"
+    kl.change_lang()
+    assert str(kl.language) == "EN"
