fbffad29bd5fb510a0ca4fc4e64c802bb89656e1
sergey <szaprudnov@hotmail.com>
2023-04-03 20:50:11 +0300

    amendment

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -8,10 +8,10 @@ def test_data_one():
 
 
 def test_keyboard(test_data_one):
-    kl = KeyBoard(*test_data_one)
-    assert str(kl) == "Dark Project KD87A"
-    assert str(kl.language) == "EN"
-    kl.change_lang()
-    assert str(kl.language) == "RU"
-    kl.change_lang()
-    assert str(kl.language) == "EN"
+    kb = KeyBoard(*test_data_one)
+    assert str(kb) == 'Dark Project KD87A'
+    assert str(kb.language) == 'EN'
+    kb.change_lang()
+    assert str(kb.language) == 'RU'
+    kb.change_lang()
+    assert str(kb.language) == 'EN'
