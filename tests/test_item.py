a1e2bf460a818716cc7b9a1f351979de66d91585
Sandi_Navoiy <santes.au@gmail.com>
2023-04-04 17:52:58 +0300

    Реализация с тестами

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,3 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 
 from src.item import Item
@@ -8,8 +7,17 @@ from src.item import Item
 def return_date():
     return Item("Смартфон", 10000, 20)
 def test_calculate_total_price(return_date):
+    """Тест для функции подсчета полной суммы запасов товара"""
     assert return_date.calculate_total_price() == 200000
 
 def test_apply_discount(return_date):
+    """Тест для функции расчета цены со скидкой"""
+    return_date.pay_rate = 0.8
     return_date.apply_discount()
+    assert return_date.price  == 8000
+
+def test_init(return_date):
+    """Тест для init, просто интересно было, смысла в нем нет"""
     assert return_date.price == 10000
+    assert return_date.name == 'Смартфон'
+    assert return_date.quantity == 20
