ef7460bebbba406465886a162aaabec01bc21c55
Sandi_Navoiy <santes.au@gmail.com>
2023-04-04 17:54:23 +0300

    Реализация с тестами, итог на 04.04. на мерж и сдачу задания 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -6,15 +6,19 @@ from src.item import Item
 @pytest.fixture
 def return_date():
     return Item("Смартфон", 10000, 20)
+
+
 def test_calculate_total_price(return_date):
     """Тест для функции подсчета полной суммы запасов товара"""
     assert return_date.calculate_total_price() == 200000
+
 
 def test_apply_discount(return_date):
     """Тест для функции расчета цены со скидкой"""
     return_date.pay_rate = 0.8
     return_date.apply_discount()
-    assert return_date.price  == 8000
+    assert return_date.price == 8000
+
 
 def test_init(return_date):
     """Тест для init, просто интересно было, смысла в нем нет"""
