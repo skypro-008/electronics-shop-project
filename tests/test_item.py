0e85203c8589011d4454800ed2951f24b03bc40c
SerovaK4 <ksusha1354@gmail.com>
2023-04-08 02:46:50 +0300

    added first number

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def smartphone():
+    return Item("Телевизор", 50000, 5)
+
+
+def test_calculate_total_price(smartphone):
+    item1 = Item("tea", 3000, 10)
+    assert item1.price == 3000
+
+
+def test_apply_discount():
+    item1 = Item("tea", 3000, 10)
+    item1.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 2400
+
+
