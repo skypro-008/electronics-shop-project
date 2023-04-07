b83327572addf7d89dccef83b130d3d49b67a0a1
Sandi_Navoiy <santes.au@gmail.com>
2023-04-07 21:49:30 +0300

    Тесты но очень сырые

--- tests/test_item.py
+++ tests/test_item.py
@@ -19,9 +19,9 @@ def test_apply_discount(return_date):
     return_date.apply_discount()
     assert return_date.price == 8000
 
+
 def test_string_to_number():
     assert Item.string_to_number("6.67") == 6
-
 
 
 def test_init(return_date):
@@ -29,3 +29,25 @@ def test_init(return_date):
     assert return_date.price == 10000
     assert return_date.name == 'Смартфон'
     assert return_date.quantity == 20
+
+def test_name():
+    item = Item("Утюг", 5, 10)
+    item.name = "Чайник"
+    assert item.name == "Чайник"
+    item.name = "Чайник и Чайник и Чайник и Чайник и Чайник"
+    assert item.name == "Чайник и Чайник и Чайник и Чайник и Чайник"
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+    assert item1.price == 100
+    assert item1.quantity == 1
+
+    item2 = Item.all[1]
+    assert item2.name == 'Ноутбук'
+    assert item2.price == 1000
+    assert item2.quantity == 3
+
+
