f94c9c2588072faf30c2d843e74843f7d343cbf8
Alex <santes.au@gmail.com>
2023-04-17 14:18:33 +0300

    Начало задания 4

--- homework-4/main.py
+++ homework-4/main.py
@@ -6,13 +6,12 @@ if __name__ == '__main__':
     # смартфон iPhone 14, цена 120_000, количество товара 5, симкарт 2
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert str(phone1) == 'iPhone 14'
-    print(repr(phone1))
-    #assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
     assert phone1.number_of_sim == 2
 
     item1 = Item("Смартфон", 10000, 20)
-    #assert item1 + phone1 == 25
-    #assert phone1 + phone1 == 10
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
 
     #phone1.number_of_sim = 0
     # ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.
--- src/phone.py
+++ src/phone.py
@@ -8,11 +8,17 @@ class Phone(Item):
         self.number_of_sim = number_of_sim
 
     def __repr__(self):
-        """метод вывода "Phone('iPhone 14', 120000, 5, 2)"""
+        """метод вывода / переопределение"""
 
-        module_name = "__main__"
         class_name = self.__class__.__name__
-        return f"{class_name}('{self.__name}', {self.price}, {self.quantity} {self.number_of_sim})"
+        return f"{class_name}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+
+    def __add__(self, other):
+        """метод сложения количества"""
+
+        return self.quantity + other.quantity
+
 
 
 
