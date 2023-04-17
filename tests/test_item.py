107d13e10a9af6541cd2919cc3e40949e4f96291
RosGus80 <124379507+RosGus80@users.noreply.github.com>
2023-04-17 20:48:51 +0300

    finished the whole work

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+item1 = Item("name", 10.0, 1000)
+
+
+def test_Item___init__():
+    assert item1.name == "name"
+    assert len(Item.all) == 1
+    item1.pay_rate = 0.9
+    item1.apply_discount()
+    assert item1.price == 9.0
+    assert item1.calculate_total_price() == 9000.0
