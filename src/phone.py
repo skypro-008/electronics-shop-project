c66afc8dc95b44057409cfaa166ed80f67b6c590
Alex <santes.au@gmail.com>
2023-04-17 13:50:53 +0300

    Начало задания 4

--- src/phone.py
+++ src/phone.py
@@ -7,10 +7,12 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
-    #def __repr__(self):
-        """метод вывода"""
+    def __repr__(self):
+        """метод вывода "Phone('iPhone 14', 120000, 5, 2)"""
 
-    #    return f"{super().__repr__(), self.number_of_sim}"
+        module_name = "__main__"
+        class_name = self.__class__.__name__
+        return f"{class_name}('{self.__name}', {self.price}, {self.quantity} {self.number_of_sim})"
 
 
 
