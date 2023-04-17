26b06967d4bfad6d722d44d930be9e3307caaa90
avedineev <avedineev69@gmail.com>
2023-04-17 22:14:11 +0500

    тесты реализованы

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item3 = Item('Пылесос', 45000, 5)
+
+
+def test_item_init():
+    assert item3.name == "Пылесос"
+    assert item3.price == 45000
+    assert item3.quantity == 5
+
+
+def test_calculate_total_price():
+    assert item3.calculate_total_price() == 225000
+
+
+def test_apply_discount():
+    Item.pay_rate = 0.8
+    item3.apply_discount()
+    assert item3.price == 36000
+
+
