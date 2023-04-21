d93246e1f931a1ae0f9d25b3487768762dfc636b
Alexey Grazhdankin <alex.alexxx56@gmail.com>
2023-04-22 00:47:16 +0300

    added changes in main.py

--- src/item.py
+++ src/item.py
@@ -29,4 +29,4 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        # self.price = self.price * self.pay_rate
+        self.price = self.price * self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -2,9 +2,11 @@
 import pytest
 from src.item import Item
 
-
+def class_example_fixture_1():
+    return Item("Чайник", 5000, 10)
 def test_calculate_total_price():
     # pass
     assert Item("Чайник", 5000, 10).calculate_total_price() == 50000
     assert Item("Миксер", 3000, 3).calculate_total_price() == 9000
     assert Item("Пылесос", 15000, 3).calculate_total_price() == 45000
+
