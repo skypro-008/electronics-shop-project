dca8e2c5fc07a863f14598f38c0b186151814fe5
Alexey Grazhdankin <alex.alexxx56@gmail.com>
2023-04-22 00:02:35 +0300

    added method calculate_total_price for class Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+def test_calculate_total_price():
+    # pass
+    assert Item("Чайник", 5000, 10).calculate_total_price() == 50000
+    assert Item("Миксер", 3000, 3).calculate_total_price() == 9000
+    assert Item("Пылесос", 15000, 3).calculate_total_price() == 45000
