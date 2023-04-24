befc3b57a3697e4031ebad3632333de73879e1a3
Thorinka <mmlentyshenko@gmail.com>
2023-04-24 14:41:35 +0300

    Class methods done, tests done, main.py works correctly

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+def test_apply_discount():
+    assert item1.apply_discount() == 10000
+    assert item2.apply_discount() == 20000
