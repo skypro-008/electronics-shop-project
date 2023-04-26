a20a00e35e04b67ce3dbc5669c71ca85089ac7ff
RosGus80 <124379507+RosGus80@users.noreply.github.com>
2023-04-26 21:09:56 +0300

    Third hw and tests

--- src/item.py
+++ src/item.py
@@ -20,6 +20,12 @@ class Item:
         self.quantity = quantity
         Item.all.append(self)
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.__name
+
     @classmethod
     def instantiate_from_csv(cls):
         import csv
