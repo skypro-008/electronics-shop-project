8b033e45dc1ff1b9aca6f76cd4700a50c7c8da7e
Alexandp L <alexandp83@mail.ru>
2023-04-26 23:26:59 +0300

    Сделал homework2

--- homework-1/src/tests/test_item.py
+++
@@ -1,7 +0,0 @@
-from src.item import Item
-
-item1 = Item("Смартфон", 10000, 20)
-
-
-def test_calculate_total_price():
-    assert Item.calculate_total_price(item1) == 100000.0
--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +17,20 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self._name = name
+        self.price = price
+        self.quantity = quantity
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self._name = name
+        else:
+            return print('Длина наименования товара превышает 10 символов')
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +38,25 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_"""
+        file_path = os.path.join(os.path.dirname(__file__), 'items.csv')
+        with open(file_path, "r") as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                item = cls(row['name'], cls.string_to_number(row['price']), int(row['quantity']))
+                cls.all.append(item)
+
+    @staticmethod
+    def string_to_number(value):
+        return int(float(value))
