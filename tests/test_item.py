699a20fa60a457935f8f705a1166078684a0c42f
1P <sur141097@gmail.com>
2023-04-29 11:53:31 +0300

    commit item test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,27 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    epm_1 = Item('Suren', 14, 20)
+    assert epm_1.calculate_total_price() == 280
+
+
+def test_apply_discount():
+    epm_1 = Item('Suren', 14, 20)
+    Item.pay_rate = 0.5
+    epm_1.apply_discount()
+    assert epm_1.price == 7
+
+
+def test_main():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000
+    assert item2.price == 20000
+    for object_ in Item.all:
+        assert isinstance(object_, Item)
