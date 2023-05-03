a84c6a4d3148a4d4168432ed6e9309398ac59769
Taures <taurest1988@gmail.com>
2023-05-03 16:41:36 +0300

    использовал геттер и сеттер, сделал name приватным

--- src/item.py
+++ src/item.py
@@ -1,3 +1,4 @@
+import csv
 class Item:
     """
     Класс для представления товара в магазине.
@@ -6,7 +7,7 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(str(self))
@@ -35,3 +36,33 @@ class Item:
     """
     Применяет установленную скидку для конкретного товара.       
     """
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) < 11:
+            self.__name = name
+            print(name)
+        #else:
+            #raise ValueError('Ошибка: в наименовании товара больше 10 символов')
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            pass
+
+    @staticmethod
+    def string_to_number(num):
+        return float(num)
+
+
+
+
+item = Item('Телефон', 10000, 5)
+item.name = 'СуперСмартфон'
+print(item.name)
+print(Item.instantiate_from_csv())
