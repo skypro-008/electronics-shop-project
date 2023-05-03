5da71c110e92f9494bad7109d9c3ea4b48071c0d
MishaRocks <tehobox@yandex.ru>
2023-05-03 23:26:02 +0300

    Сделана домашка 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def test_hm_1():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(test_hm_1):
+    assert test_hm_1.calculate_total_price() == 200000
+
+
