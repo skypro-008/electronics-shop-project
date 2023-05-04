7999a81d4d7182629575e4da2210ba37cf408d0e
Taures <taurest1988@gmail.com>
2023-05-04 07:42:20 +0300

    написал класс-метод и статический метод

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -6,7 +9,7 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(str(self))
@@ -25,7 +28,6 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        #Item.all.append(total)
         return total
 
     def apply_discount(self) -> None:
@@ -35,3 +37,35 @@ class Item:
     """
     Применяет установленную скидку для конкретного товара.       
     """
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) < 11:
+            self.__name = name
+            print(name)
+        else:
+            print(Exception('Ошибка: в наименовании товара больше 10 символов'))
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        with open('items.csv', 'r', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                item = cls(row.get('name'), float(row.get('price')), int(row.get('quantity')))
+                cls.all.append(item)
+
+    @staticmethod
+    def string_to_number(num):
+
+        return float(num).__int__()
+
+
+item = Item('Телефон', 10000, 5)
+item.name = 'СуперСмартфон'
+print(item.name)
+print(Item.instantiate_from_csv())
+print(Item.string_to_number('5'))
