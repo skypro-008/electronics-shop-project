3f1b3a9a72351d1df7b11e82501da842d21d750e
blau <serzh.plichko@mail.ru>
2023-05-08 15:23:38 +1100

    complete class Item, write tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+test_item = Item('Test', price=100, quantity=5)
+
+def test_init():
+    assert list(vars(test_item).values()) == ['Test', 100, 5]
+
+def test_calculate_total_price():
+    assert test_item.calculate_total_price() == 500
+
+def test_apply_discount():
+    Item.pay_rate = 0.9
+    test_item.apply_discount()
+    assert test_item.price == 90
+
