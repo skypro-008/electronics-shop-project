fa97f727108a43b514df4d10134f136946383060
Violetta <lotsmanovavioletta@gmail.com>
2023-05-11 19:29:19 +0300

    write class Item and test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def item1():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 200000
+
+
+def test_apply_discount(item1):
+    item1.apply_discount()
+    assert item1.price == 10000
+
+
+def test_apply_discount_rate(item1):
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000.0
