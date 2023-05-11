00977fe8254c6afe55836e4904fde15b845d3900
Thorinka <mmlentyshenko@gmail.com>
2023-05-11 12:17:33 +0300

    keyboard.py added, tests work

---
+++ src/keyboard.py
@@ -0,0 +1,38 @@
+from src.item import Item
+
+class MixinLanguage:
+    """
+    Mixin-класс для метода change_lang
+    """
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+
+    def change_lang(self):
+        """
+        Метод для изменения языка клавиатуры
+        """
+        if self._language == "EN":
+            self._language = "RU"
+        elif self._language == "RU":
+            self._language = "EN"
+        return self
+
+    @property
+    def language(self):
+        return self._language
+
+class Keyboard(MixinLanguage, Item):
+    """
+    Класс Keyboard (наследование от MixinLanguage, Item)
+    """
+
+    def __init__(self, name: str, price: float, quantity: int, language="EN"):
+        """
+        Метод инициализации объекта класса Keyboard
+        name: имя
+        price: цена
+        quantity: количество
+        language: язык клавиатуры, по умолчанию EN
+        """
+        super().__init__(name, price, quantity)
+        self._language = language
