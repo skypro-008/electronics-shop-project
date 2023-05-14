0db11fa3ae1aa1381ca2d47252320d7bb9435be3
Petr Davydov <Davydov.atc@yandex.ru>
2023-05-14 12:44:58 +0300

    modify homework-2, add some method in item and write test for it%

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import os
+from csv import DictReader
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,7 +9,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: int, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
@@ -13,10 +17,31 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
+        if not isinstance(name, str):
+            raise ValueError("Название должно быть строкой")
+        else:
+            self.__name = name
+
+        if not isinstance(price, int):
+            raise ValueError("Цена должна быть целым числом")
+        else:
+            self.price = price
+
+        if not isinstance(quantity, int):
+            raise ValueError("Кол-во должно быть целым числом")
+        else:
+            self.quantity = quantity
+
+        self.all.append(self)
+
+    def __repr__(self):
+        return f"{self.name}, {self.price}, {self.quantity}"
+
+    def set_name(self, name):
         self.name = name
-        self.price = price
-        self.quantity = quantity
-        self.__class__.all.append(self)
+
+    def get_name(self):
+        return self.name
 
     def calculate_total_price(self) -> float:
         """
@@ -24,10 +49,57 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        return self.price * self.quantity
+        total_item_price = self.price * self.quantity
+        return total_item_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price *= self.pay_rate
+        self.price = self.price * self.pay_rate
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        try:
+            if len(name) <= 10:
+                self.__name: str = name
+        except:
+            raise Exception(f"Длина наименования товара {name} превышает 10 симвовов")
+
+    @staticmethod
+    def load_csv() -> list:
+        data = []
+        filedir = os.path.dirname(os.path.abspath(__file__))
+        with open(os.path.join(f'{filedir}', 'items.csv'), 'r+', encoding='windows-1251') as csv_file:
+            csv_reader = DictReader(csv_file)
+            for item in csv_reader:
+                data.append(item)
+            return data
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        cls.all = []
+        data = cls.load_csv()
+        for line in data:
+            cls(
+                line['name'],
+                cls.string_to_number(line['price']),
+                cls.string_to_number(line['quantity'])
+            )
+
+    @staticmethod
+    def string_to_number(string) -> int:
+        if "." in string:
+            str_to_float: float = float(string)
+            str_to_int: int = int(str_to_float)
+        else:
+            str_to_int: int = int(string)
+        return str_to_int
+
+    def test_instantiate_from_csv(self):
+        Item.instantiate_from_csv()
+        assert len(Item.all) == 5
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,8 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+import os
+import pytest
+from csv import DictReader
 
 
 def test_calculate_total_price():
@@ -18,3 +21,25 @@ def test_all_instances():
     item1 = Item("Product1", 10, 5)
     item2 = Item("Product2", 20, 3)
     assert Item.all == [item1, item2]
+
+
+@pytest.fixture(scope="module")
+def csv_data():
+    filedir = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(f'{filedir}/../src', 'items.csv'), 'r+', encoding='windows-1251') as csv_file:
+        csv_reader = DictReader(csv_file)
+        return [item for item in csv_reader]
+
+def test_load_csv(csv_data):
+    assert len(csv_data) > 0
+
+def test_instantiate_from_csv(csv_data):
+    Item.instantiate_from_csv()
+    assert len(Item.all) == len(csv_data)
+
+def test_string_to_number():
+    assert Item.string_to_number('15') == 15
+    assert Item.string_to_number('15.5') == 15
+    assert Item.string_to_number('17.0') == 17
+
+
