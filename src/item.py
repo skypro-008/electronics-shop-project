d054163c0fa51d9d11344138575dcd7dfd9faeb7
QIC Bel <tsyplakov-a@list.ru>
2023-05-16 23:41:33 +0300

    added functions and tests

--- src/item.py
+++ src/item.py
@@ -13,7 +13,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = float(price)
+        self.quantity = int(quantity)
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +24,13 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        total_price = self.price * self.quantity
+        return total_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= (1 - self.pay_rate/100)
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.calculate_total_price() == 200000.0
+
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    Item.pay_rate = 0.8
+    assert item1.apply_discount() == None
