53a156f8f01d3bd419b2ed021e96379b1e2a7850
Kate & Miki <nathovaekaterina@gmail.com>
2023-05-16 11:55:56 +0300

    Написаны тесты для проверки

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,26 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+"""Тесты для модуля Item"""
+
+from src.item import Item
+
+"""Выводим стоимость товара"""
+
+if __name__ == '__main__':
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+"""Устанавливаем скидку на товары"""
+item1.pay_rate = 0.8
+item2.pay_rate = 0.8
+
+"""Применяем скидку на товары"""
+item1.apply_discount()
+item2.apply_discount()
+
+assert item1.price == 16000.0
+assert item2.price == 8000.0
+
+
+
