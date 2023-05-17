81882f6dd55db70c5d5b5298d2eb7269a4a77474
dude <zlukcss@gmail.com>
2023-05-17 16:09:42 -0300

    реализация дз 13.1

--- src/item.py
+++ src/item.py
@@ -2,7 +2,7 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate = 1.0
+    pay_rate = 0.85
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
@@ -13,7 +13,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = float(price)
+        self.quantity = int(quantity)
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +24,10 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = round(self.price * Item.pay_rate, 2)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,42 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item_obj() -> Item:
+    """
+    Create Item object.
+    """
+    return Item(name="Phone", price=19999.9, quantity=5)
+
+
+def test_class_attributes():
+    """
+    Attrs test.
+    """
+    assert isinstance(Item.pay_rate, float)
+    assert Item.pay_rate < 1
+    assert isinstance(Item.all, list)
+
+
+def test_init(item_obj):
+    """
+    __init__ test.
+    """
+    assert isinstance(item_obj.name, str)
+    assert isinstance(item_obj.price, float)
+    assert isinstance(item_obj.quantity, int)
+
+def test_calculate_total_price(item_obj):
+    """
+    calculate_total_price method test.
+    """
+    assert item_obj.calculate_total_price() == 19999.9 * 5
+
+def test_apply_discount(item_obj):
+    """
+    apply_discount method test.
+    """
+    item_obj.apply_discount()
+    assert item_obj.price == round(19999.9 * 0.85, 2)
