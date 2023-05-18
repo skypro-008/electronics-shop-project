30207ee95e620b2c45174acecc9af2b586ee3cea
Renat <matador.99@mail.ru>
2023-05-18 15:42:13 +0500

    "Дописал код и написал тесты"

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+# TestCase#1 TotalPrise
+item1 = Item('product1', 10000, 2)
+assert item1.calculate_total_price() == 20000
+
+# TestCase#2  ApplyDiscount
+item1 = Item('product1', 20000, 2)
+item1.apply_discount()
+assert item1.calculate_total_price() == 40000
+assert item1.apply_discount() is None
+
+
+
