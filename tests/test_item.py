8a46b5108307cea83e47ba84282ac04703894673
Yaroslav Galiulin <yaroslav66@list.ru>
2023-05-20 13:19:25 +0500

    Создание тестов для новых методов из задания

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,6 +2,7 @@
 import pytest
 from src.item import Item
 
+
 def test_item():
     # классы для проверки
     item1 = Item('Car', 250.0, 8)
@@ -22,3 +23,25 @@ def test_item():
 
     # Проверка списка экзепляров
     assert Item.all == [item1, item2]
+
+def test_instantiate_from_csv():
+
+    item = Item('Телефон', 10000, 5)
+
+    # длина наименования товара меньше 10 символов
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+    # длина наименования товара больше 10 символов
+    item.name = 'СуперСмартфон'
+    # Exception: Длина наименования товара превышает 10 символов.
+
+    Item.instantiate_from_csv()  # создание объектов из данных файла
+    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
