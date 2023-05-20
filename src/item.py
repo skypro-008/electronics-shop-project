30ddcb658971e07b875155df222bee90511292d8
Polexa84 <polexa84@gmail.com>
2023-05-21 01:45:39 +0300

    Внес изменения в класс `Item`:
    - атрибут `name` сделал приватным
    - добавил геттер и сеттер для `name`, используя @property
    - в сеттере `name` проверяет, что длина наименования товара не больше 10 симвовов

--- src/item.py
+++ src/item.py
@@ -5,15 +5,15 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, _name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
-        :param name: Название товара.
+        :param _name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self._name = _name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
@@ -31,3 +31,25 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        """
+        Геттер для наименования товара.
+        """
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        """
+        Сеттер для наименования товара.
+
+        :param new_name: Новое наименование товара.
+        """
+        try:
+            if len(new_name) <= 10:
+                self._name = new_name
+            else:
+                raise ValueError("Длина наименования товара превышает 10 символов.")
+        except ValueError as e:
+            print(e)
