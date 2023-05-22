6edc2ea0bdcc9f0904919e06691d1df4ab101f75
Denis <denis30510@gmail.com>
2023-05-22 14:00:36 +0300

    added clss Item, tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def start_data():
+    return Item("Пижама", 999.9, 10)
+
+def test_Item(start_data):
+    assert Item("Пижама", 999.9, 10).name == 'Пижама'
+
+def test_calculate_total_price(start_data):
+    assert start_data.calculate_total_price() == 999.9*10
+
+def test_apply_discount(start_data):
+    start_data.apply_discount()
+    assert start_data.price == 999.9
