eec1629642ae7bcfcb3cd5a6d37745570019f279
Denis <denis30510@gmail.com>
2023-05-22 15:48:37 +0300

    fixed code Flake8

--- src/item.py
+++ src/item.py
@@ -5,6 +5,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
+
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -17,6 +18,8 @@ class Item:
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -24,6 +27,7 @@ class Item:
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
+
 
     def apply_discount(self) -> None:
         """
