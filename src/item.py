b7c543cf0ee877fbbfaff2b8c8db6d7604f6cc48
Polexa84 <polexa84@gmail.com>
2023-05-24 00:50:31 +0300

    Внес следующие изменения в класс Item:
        атрибут name сделал приватным
        добавил геттер и сеттер для name, используя @property
        в сеттере name проверять, что длина наименования товара не больше 10 симвовов
    
    Добавьте в Item следующие методы:
    
        instantiate_from_csv() - класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
        string_to_number() - статический метод, возвращающий число из числа-строки

--- src/item.py
+++ src/item.py
@@ -1,7 +1,12 @@
+import csv
+from ctypes import Union
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
+    CSV = None
     pay_rate = 1.0
     all = []
 
@@ -53,3 +58,105 @@ class Item:
                 raise ValueError("Длина наименования товара превышает 10 символов.")
         except ValueError as e:
             print(e)
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """
+        Создание экземпляров класса `Item` на основе данных из файла _src/items.csv_.
+        """
+        cls.all.clear()
+        try:
+            with open('/home/polexa/electronics-shop-project/src/items.csv', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    cls(row['name'], row['price'], row['quantity'])
+        except FileNotFoundError:
+            print('Файл не найден')
+
+
+import csv
+
+
+class Item:
+    """
+    Класс для представления товара в магазине.
+    """
+    CSV = None
+    pay_rate = 1.0
+    all = []
+
+    def __init__(self, _name: str, price: float, quantity: int) -> None:
+        """
+        Создание экземпляра класса item.
+
+        :param _name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        """
+        self._name = _name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
+
+    def calculate_total_price(self) -> float:
+        """
+        Рассчитывает общую стоимость конкретного товара в магазине.
+
+        :return: Общая стоимость товара.
+        """
+        return self.price * self.quantity * self.pay_rate
+
+    def apply_discount(self) -> None:
+        """
+        Применяет установленную скидку для конкретного товара.
+        """
+        self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        """
+        Геттер для наименования товара.
+        """
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        """
+        Сеттер для наименования товара.
+
+        :param new_name: Новое наименование товара.
+        """
+        try:
+            if len(new_name) <= 10:
+                self._name = new_name
+            else:
+                raise ValueError("Длина наименования товара превышает 10 символов.")
+        except ValueError as e:
+            print(e)
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """
+        Создание экземпляров класса `Item` на основе данных из файла _src/items.csv_.
+        """
+        cls.all.clear()
+        try:
+            with open('/home/polexa/electronics-shop-project/src/items.csv', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    cls(row['name'], row['price'], row['quantity'])
+        except FileNotFoundError:
+            print('Файл не найден')
+
+    @staticmethod
+    def string_to_number(line):
+        numb = int(float(line))
+        return numb
+
+    def calculate_total_price(self):
+
+        return self.price * self.quantity
+
+    def apply_discount(self):
+
+        self.price *= self.pay_rate
