f01e1f97ac50f049a6d99a29b17ce9eb73c4f2a1
Kirill Aliyev <kirill.linas@gmail.com>
2023-05-23 19:14:18 +0400

    hw 1

--- src/item.py
+++ src/item.py
@@ -1,9 +1,15 @@
+from csv import DictReader
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
+
     pay_rate = 1.0
     all = []
+    path_to_cvs = os.path.join(os.path.dirname(__file__), "items.csv")
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,24 +19,87 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        if type(name) is str:
-            self.name = name
-        else:
-            raise ValueError("Наименование должно быть строкой")
 
-        if type(price) is float:
-            self.price = price
-        elif type(price) is int:
-            self.price = float(price)
-        else:
-            raise ValueError("Цена должна быть числом")
+        self.__name = None
+        self.name = name
 
-        if type(quantity) is int:
-            self.quantity = quantity
-        else:
-            raise ValueError("Количество должно быть выражено целым числом")
+        self.__price = None
+        self.price = price
+
+        self.__quantity = None
+        self.quantity = quantity
 
         self.all.append(self)
+
+    def __repr__(self) -> str:
+        """Возвращает строку, в которой перечислены свойства класса для просмотра в режиме отладки"""
+
+        return f"{self.__class__.__name__}(\'{self.name}\', {self.price}, {self.quantity})"
+
+    def __str__(self) -> str:
+        """Возвращает строку с наименованием товара, рекомендовано для просмотра пользователем"""
+
+        return f"{self.name}"
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        """
+        Устанавливает наименование товара.
+
+        Проверки:
+
+        наименование должно быть строкой
+        длина наименования не должна превышать 10 символов
+        """
+
+        if not isinstance(name, str):
+            raise ValueError("Наименование должно быть строкой")
+        elif len(name) > 10:
+            raise Exception("Длина наименования товара превышает 10 символов.")
+        else:
+            self.__name = name
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, price: float | int) -> None:
+        """
+        Устанавливает стоимость товара.
+
+        Проверки:
+
+        стоимость должна быть целым числом, либо вещественным
+        """
+
+        if type(price) not in (int, float):
+            raise Exception("Цена должна быть числом")
+        else:
+            self.__price = float(price)
+
+    @property
+    def quantity(self) -> int:
+        return self.__quantity
+
+    @quantity.setter
+    def quantity(self, quantity: int) -> None:
+        """
+        Устанавливает количество товара.
+
+        Проверки:
+
+        количество должно быть целым числом
+        """
+
+        if type(quantity) is not int:
+            raise Exception("Количество должно быть выражено целым числом")
+        else:
+            self.__quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
@@ -38,22 +107,51 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        if not isinstance(self.price, (int, float)):
-            raise ValueError("Цена задана неверно!")
-        elif not isinstance(self.quantity, int):
-            raise ValueError("Количество задано неверно!")
-        else:
-            return self.price * self.quantity
+
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        if not isinstance(self.price, (int, float)):
-            raise ValueError("Цена задана неверно!")
-        elif not isinstance(self.pay_rate, float):
+
+        if not isinstance(self.pay_rate, float):
             raise ValueError("Размер скидки задан неверно!")
         elif self.pay_rate > 1:
             raise ValueError("Размер скидки задан неверно!")
         else:
             self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Класс-метод для создания объектов класса в соответствии с характеристиками,
+        указанными в csv-файле.
+        Путь к файлу является атрибутом класса.
+        """
+
+        with open(cls.path_to_cvs) as csv_file:
+            data_cvs = DictReader(csv_file)
+
+            for row in data_cvs:
+                name, price, quantity = row.values()
+                price = cls.string_to_number(price)
+                quantity = cls.string_to_number(quantity)
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(string):
+        """Метод для превращения строки в формат числа: целого или с плавающей точкой"""
+
+        if "." in string:
+            try:
+                number = float(string)
+            except ValueError:
+                raise ValueError("Нельзя превратить в число!")
+            return number
+        else:
+            try:
+                number = int(string)
+            except ValueError:
+                raise ValueError("Нельзя превратить в целое число!")
+            return number
--- tests/test_item.py
+++ tests/test_item.py
@@ -5,18 +5,18 @@ import pytest
 
 @pytest.fixture
 def item():
-    return Item("Принтер Epson L121 (C11CD76414)", 5299, 8)
+    return Item("Принтер", 5299, 8)
 
 
 def test_create_float(item):
-    assert item.name == "Принтер Epson L121 (C11CD76414)"
+    assert item.name == "Принтер"
     assert item.price == float(5299)
     assert item.quantity == 8
     assert item.pay_rate == 1.0
 
 
 def test_create_int(item):
-    assert item.name == "Принтер Epson L121 (C11CD76414)"
+    assert item.name == "Принтер"
     assert item.price == 5299
     assert item.quantity == 8
     assert item.pay_rate == 1.0
@@ -24,34 +24,30 @@ def test_create_int(item):
 
 def test_create_incorrect():
     with pytest.raises(ValueError):
-        item = Item(8, 5299, 8)
+        Item(8, 5299, 8)
 
-    with pytest.raises(ValueError):
-        item = Item("Принтер Epson L121 (C11CD76414)", "5299", 8)
+    with pytest.raises(Exception):
+        Item("Принтер Epson L121 (C11CD76414)", 5299, 8)
 
-    with pytest.raises(ValueError):
-        item = Item("Принтер Epson L121 (C11CD76414)", 5299, "8")
+    with pytest.raises(Exception):
+        Item("Принтер Epson L121 (C11CD76414)", "5299", 8)
+
+    with pytest.raises(Exception):
+        Item("Принтер Epson L121 (C11CD76414)", 5299, "8")
+
+
+def test_price_incorrect(item):
+    with pytest.raises(Exception):
+        item.price = "18"
+
+
+def test_quantity_incorrect(item):
+    with pytest.raises(Exception):
+        item.quantity = 19.6
 
 
 def test_calculate_total_price(item):
     assert item.calculate_total_price() == 5299 * 8
-
-
-def test_calculate_total_price_errors(item):
-    item.price = "5299"
-    item.quantity = "8"
-    with pytest.raises(ValueError):
-        assert item.calculate_total_price()
-
-    item.price = "5299"
-    item.quantity = 8
-    with pytest.raises(ValueError):
-        assert item.calculate_total_price()
-
-    item.price = 5299.0
-    item.quantity = "8"
-    with pytest.raises(ValueError):
-        assert item.calculate_total_price()
 
 
 def test_apply_discount(item):
@@ -62,17 +58,45 @@ def test_apply_discount(item):
 
 
 def test_apply_discount_incorrect(item):
-    item.pay_rate = 85
-    item.price = 6000.0
+    item.pay_rate = 2
     with pytest.raises(ValueError):
         item.apply_discount()
 
-    item.pay_rate = 0.85
-    item.price = "6000.0"
+    item.pay_rate = 1.6
     with pytest.raises(ValueError):
         item.apply_discount()
 
-    item.pay_rate = "1.0"
-    item.price = 6000.0
+
+def test_instantiate_from_csv():
+    Item.all = []
+    Item.instantiate_from_csv()
+    assert Item.all[0].name == 'Смартфон'
+    assert Item.all[3].price == 50.0
+    assert Item.all[4].quantity == 5
+
+    with pytest.raises(IndexError):
+        print(Item.all[5])
+
+
+def test_string_to_number_correct():
+    assert Item.string_to_number("89") == 89
+    assert type(Item.string_to_number("89")) is int
+
+    assert Item.string_to_number("169.11") == 169.11
+    assert type(Item.string_to_number("169.11")) is float
+
+
+def test_string_to_number_incorrect():
     with pytest.raises(ValueError):
-        item.apply_discount()
+        Item.string_to_number("восемь")
+
+    with pytest.raises(ValueError):
+        Item.string_to_number("восемь.одиннадцать")
+
+
+def test_repr(item):
+    assert repr(item) == f"Item(\'{item.name}\', {item.price}, {item.quantity})"
+
+
+def test_str(item):
+    assert str(item) == f"{item.name}"
