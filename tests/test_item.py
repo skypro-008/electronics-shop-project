8b381ac87df305f23d39e42e0000c897146d0e7b
Evgeny <vajnbiy@gmail.com>
2023-05-23 19:55:03 -0700

    добавил Item класс

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from pytest import fixture
+
+from src.item import Item
+
+
+@fixture
+def item():
+    return Item("Смартфон", 10000, 20)
+
+def test_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+
+def test_discount(item):
+    item.apply_discount()
+    assert item.price == 10000.0
