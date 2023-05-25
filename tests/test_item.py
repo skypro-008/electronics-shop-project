e50622601d16a68e9db918e9b663015ccd465a14
Vitaliy Petrenko <mail@v-petrenko.ru>
2023-05-25 16:31:22 +0300

    homework #1 complete

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+"""Здесь тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+
+def test_init():
+    item = Item('test', 200.0, 100)
+    Item.pay_rate = 0.5
+    assert item.name == 'test'
+    assert item.price == 200.0
+    assert item.quantity == 100
+    item.apply_discount()
+    assert item.calculate_total_price() == 10000.0
