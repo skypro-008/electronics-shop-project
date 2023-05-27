cf1a959f101b948355967afb8d33918d7c94c4bf
Kate & Miki <nathovaekaterina@gmail.com>
2023-05-27 16:28:22 +0300

    Реализованы магические методы __str__ и __repr__

--- src/item.py
+++ src/item.py
@@ -1,20 +1,15 @@
 import csv
 
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
+    """Класс для представления товара в магазине."""
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
+        """Создание экземпляра класса Item.
         :param name: Название товара.
         :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
+        :param quantity: Количество товара в магазине."""
         self.__name = str(name)
         self.price = float(price)
         self.quantity = int(quantity)
@@ -52,18 +47,21 @@ class Item:
         return int(str)
 
     def calculate_total_price(self, price) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
-        """
-
+        """ Рассчитывает общую стоимость конкретного товара в магазине. """
         return self.quantity * self.price
 
     def apply_discount(self, total_price=None, pay_rate=None) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
+        """ Применяет установленную скидку для конкретного товара. """
         return self.price * self.pay_rate
+
+    def __repr__(self):
+        """Метод repr возвращает строку с данными, которые мы зададим. Ввела данные по требованиям в файле main"""
+        return f"{self.__class__.__name__}('{self.name}', '{self.price}', {self.quantity})"
+
+    def __str__(self):
+        """Вывод пользовательской информации"""
+        return self.name
+
+
 
     
