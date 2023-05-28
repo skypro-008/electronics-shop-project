68c22891d877a556dffe9eaf2a9d0e72636f7e19
Margarita Rastriga <rasm@1Ci.com>
2023-05-29 02:57:31 +0300

    item class and test for item class

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,4 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
-
 from src import item
 
 
@@ -8,9 +6,9 @@ def test_calculate_total_price():
     assert item.Item("Смартфон", 10000, 20).calculate_total_price() == 200000
     assert item.Item("Ноутбук", 20000, 5).calculate_total_price() == 100000
 
-def test_apply_discount():
 
-    testItem = item.Item("Смартфон", 10000, 20)
+def test_apply_discount():
+    test_item = item.Item("Смартфон", 10000, 20)
     item.Item.pay_rate = 0.3
-    testItem.apply_discount()
-    assert testItem.price == 3000
+    test_item.apply_discount()
+    assert test_item.price == 3000
