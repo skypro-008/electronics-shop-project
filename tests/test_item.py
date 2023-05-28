6747d51fe9e9663f597b8587cff162835585c030
User <eco-64@mail.ru>
2023-05-28 21:31:40 +0500

    new test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import calculate_total_price, apply_discount
+
+
+def test_calculate_total_price():
+    test_price_1 = 10000
+    test_quantity_1 = 5
+    assert calculate_total_price(test_price_1, test_quantity_1) == 50000
+
+
+def test_apply_discount():
+    test_price_1 = 10000
+    test_Item_pay_rate_1 = 0.85
+    assert apply_discount(test_price_1, test_Item_pay_rate_1) == 8500
+    test_price_1 = 10000
+    test_Item_pay_rate_1 = 0
+    assert apply_discount(test_price_1, test_Item_pay_rate_1) == None
