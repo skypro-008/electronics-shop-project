8c3929705d57496c2453971542370781479deba0
Evgeny <vajnbiy@gmail.com>
2023-05-28 13:15:48 -0700

    dz2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,4 @@
+import csv
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +14,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
@@ -31,3 +32,28 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) < 10:
+            self.__name = name
+        else:
+            raise Exception("Более 10 букв")
+
+    @staticmethod
+    def string_to_number(number):
+        return int(float(number))
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open("../src/items.csv", encoding="utf-8") as file:
+            data = csv.DictReader(file)
+            for i in data:
+                obj = (cls(name=i["name"], price=i["price"], quantity=i["quantity"]))
+
+
+
