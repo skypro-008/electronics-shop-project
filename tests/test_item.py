438dcbc47ba285bbe3ebe8a69df36e0d41396288
MaxSlidzen <slidzon94@gmail.com>
2023-05-29 20:18:30 +0300

    added test__add__ for item and fixtures. Also added test for option of __add__ (under #)

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,29 +1,59 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+# from src.phone import Phone
 import pytest
 import os
 
 
 @pytest.fixture
-def item():
+def item_1():
     return Item("Ноутбук", 50000, 3)
 
 
-def test__repr__(item):
-    assert repr(item) == "Item('Ноутбук', 50000, 3)"
+@pytest.fixture
+def item_2():
+    return Item("Телевизор", 65000, 2)
 
 
-def test__str__(item):
-    assert str(item) == 'Ноутбук'
+# @pytest.fixture
+# def phone_1():
+#     return Phone("Samsung", 34000, 8, 0)
 
 
-def test_calculate_total_price(item):
-    assert item.calculate_total_price() == 150000
+# @pytest.fixture
+# def phone_2():
+#     return Phone("Sony", 46000, 6, 2)
 
 
-def test_apply_discount(item):
+def test__repr__(item_1):
+    assert repr(item_1) == "Item('Ноутбук', 50000, 3)"
+
+
+def test__str__(item_1):
+    assert str(item_1) == 'Ноутбук'
+
+
+def test__add__(item_1, item_2):
+    assert item_1 + item_2 == 5
+    with pytest.raises(Exception):
+        item_1 + 3
+        '10' + item_2
+
+# Вариант для сложения строго в определенном классе
+# def test__add__(item_1, item_2, phone_1, phone_2):
+#     assert item_1 + item_2 == 5
+#     with pytest.raises(Exception):
+#         item_1 + phone_2
+#         phone_1 + item_2
+
+
+def test_calculate_total_price(item_1):
+    assert item_1.calculate_total_price() == 150000
+
+
+def test_apply_discount(item_1):
     Item.pay_rate = 0.8
-    assert item.apply_discount() == 40000.0
+    assert item_1.apply_discount() == 40000.0
 
 
 def test_instantiate_from_csv():
@@ -39,9 +69,9 @@ def test_string_to_number():
     assert Item.string_to_number('4') == 4
 
 
-def test_name(item):
-    assert item.name == 'Ноутбук'
-    item.name = 'Телефон'
-    assert item.name == 'Телефон'
+def test_name(item_1):
+    assert item_1.name == 'Ноутбук'
+    item_1.name = 'Телефон'
+    assert item_1.name == 'Телефон'
     with pytest.raises(Exception):
-        item.name = 'Суперсмартфон'
+        item_1.name = 'Суперсмартфон'
