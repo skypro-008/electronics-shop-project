b900020835d1ba7e11c31c0f6b71d56e3ea25cf0
Khalitov Ilnur <halit_ilnur@mail.ru>
2023-05-30 12:45:47 +0300

    made tests for item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.calculate_total_price() == 200000
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000.0
