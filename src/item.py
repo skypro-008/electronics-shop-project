c8c7d5fae7cba5074d1d8e1c879d87847621eae4
AnnaKolmychek <a.kolmychek@gmail.com>
2023-06-01 22:47:29 +0300

    add src & repr in class Item()

--- src/item.py
+++ src/item.py
@@ -31,6 +31,12 @@ class Item:
             raise Exception('Длина наименования товара превышает 10 символов.')
         self.__name = new_name
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.__name
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
