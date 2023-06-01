ba627a0e6082d27111aa898e41d978264c374ca0
Evgeny <vajnbiy@gmail.com>
2023-05-31 20:47:35 -0700

    add testdz

--- tests/test_item.py
+++ tests/test_item.py
@@ -6,6 +6,7 @@ from pytest import fixture
 from src.item import Item
 
 
+
 @fixture
 def item():
     return Item("Смартфон", 10000, 20)
@@ -17,3 +18,19 @@ def test_total_price(item):
 def test_discount(item):
     item.apply_discount()
     assert item.price == 10000.0
+
+
+def test_string_to_number(item):
+    item.string_to_number('2')
+    assert item.string_to_number(2) == 2
+
+
+def test_instantiate_from_csv(item):
+    item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+
+def test_name(item):
+    with pytest.raises(Exception):
+        item.name = "knbknknelnblenwblnbl"
+
