00ccd13cabc551097735550458f7f90141d7b2ee
KotBeginmot <iliynaea@gmail.com>
2023-06-01 20:35:55 +0300

    function and test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def items():
+    return Item('str', 200000, 20)
+
+def test_item(items):
+    assert items.name == 'str'
+    assert items.price == 200000
+    assert items.quantity == 20
+    assert items.calculate_total_price() == 200000 * 20
+
