6a6e714be99ddce2eeb4a6d5fea7253343363df8
Volk0vAlex <volkov.alex1986@gmail.com>
2023-06-04 20:31:50 +0700

    homework_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def test_item():
+    return Item("test_notebook", 35000, 4)
+
+
+def test_init(test_item):
+    assert test_item.name == "test_notebook"
+    assert test_item.price == 35000
+    assert test_item.quantity == 4
+
+
+def test_calculate_total_price(test_item):
+    assert test_item.calculate_total_price() == 140000
+
+
+def test_apply_discount(test_item):
+    test_item.pay_rate = 0.9
+    test_item.apply_discount()
+    assert test_item.price == 31500
