1c82fe4fb7af033028a4373d15884f810432ea71
abykov <abykov@uma.tech>
2023-06-04 19:23:10 +0300

    выполнение задания 1 из дз 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+import math
+import os.path
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -8,27 +13,65 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        Item.all.append(self)
+        self._name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
-        return self.price * self.quantity
+        total_price = self.price * self.quantity
+        return total_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price *= Item.pay_rate
+        self.price *= self.pay_rate
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        try:
+            if len(new_name) <= 10:
+                self._name = new_name
+        except Exception:
+            return "Длина наименования товара превышает 10 символов"
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        Item.all = []
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        data = os.path.join(current_dir, 'items.csv')
+        with open(data, newline='', encoding='utf-8-sig') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                name = row['name']
+                price = cls.string_to_number(row['price'])
+                quantity = cls.string_to_number(row['quantity'])
+                item = cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(num):
+        number = int(math.floor(float(num.strip())))
+        return number
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.__str__()
+
+    @classmethod
+    def get_all_items(cls):
+        return [str(item) for item in cls.all]
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,28 +1,44 @@
+import pytest
+
 from src.item import Item
 
 
 def test_calculate_total_price():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-
-    assert item1.calculate_total_price() == 200000
-    assert item2.calculate_total_price() == 100000
+    prod1 = Item("Телевизор", 50, 5)
+    assert prod1.calculate_total_price() == 250
 
 
-def test_apply_discount():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-
-    Item.pay_rate = 0.8
-    item1.apply_discount()
-
-    assert item1.price == 8000.0
-    assert item2.price == 20000
+@pytest.fixture
+def item():
+    Item.all = []
+    item = Item(name="Товар", price=45.97, quantity=40)
+    return item
 
 
-def test_all_items():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
+def test_all_items_list(item):
+    assert len(Item.all) == 1
+    assert Item.all[0] == item
 
-    assert Item.all == [item1, item2]
 
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+    assert isinstance(Item.all[0], Item)
+
+    # Проверка значений объектов, созданных из CSV файла
+    item1 = Item.all[0]
+    assert item1.name == "Смартфон"
+    assert item1.price == 100
+    assert item1.quantity == 1
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_invalid_name_length():
+    prod1 = Item("Товар", 50, 5)
+    prod1.name = "Наименование, которое не должно пройти проверку"
+    assert prod1.name == "Товар"
