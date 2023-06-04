45079b0d83d09f54445234065fb3f908d958e0c4
Goldenkov <oldfild1@gmail.com>
2023-06-04 23:06:28 +0300

    add code and test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+import pytest
+
+
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    assert Item.calculate_total_price(item1) == 200000
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    Item.pay_rate = 0.8
+    Item.apply_discount(item1)
+    assert item1.price == 8000
