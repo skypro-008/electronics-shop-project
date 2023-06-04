4c6c755261e5c84de3263df4b0141e585123003f
Sergey Sologub <sergey.sologub@rtlabs.ru>
2023-06-04 21:40:53 +0300

    add class and tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+class TestItem:
+    item = Item('iPhone 14 Pro Max', 100000, 50)
+
+    def test_calculate_total_price(self):
+        assert self.item.calculate_total_price() == 5000000
+
+    def test_apply_discount(self):
+        self.item.pay_rate = 0.8
+        self.item.apply_discount()
+        assert self.item.calculate_total_price() == 4000000.0
