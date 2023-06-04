a22a52ed5a460228ddb2484f409d1e99f9dcf2b3
Nickolay <psyangel93@mail.ru>
2023-06-04 12:57:24 +0300

    fix file item and test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+item1 = Item("Смартфон", 10000, 20)
+
+
+def test_init():
+    assert item1.name == "Смартфон"
+    assert item1.price == 10000
+    assert item1.quantity == 20
+    assert Item.all == [item1]
+    assert len(Item.all) == 1
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 200000
+
+
+def test_apply_discount():
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000.0
