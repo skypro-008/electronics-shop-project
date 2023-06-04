a10b8405c2669568c4934fd5e45d1dfc2ade2554
Margarita Rastriga <rasm@1Ci.com>
2023-06-04 19:48:40 +0300

    reading data from file

--- src/item.py
+++ src/item.py
@@ -1,10 +1,13 @@
+import csv
+import math
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
-
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -14,12 +17,25 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        try:
+            if len(name) <= 10:
+                self.__name = name
+            else:
+                raise Exception(f"Длина наименования {name} товара превышает 10 символов.")
+        except Exception as ex:
+            print(ex)
 
     def calculate_total_price(self) -> float:
         """
@@ -29,9 +45,28 @@ class Item:
         """
         return self.price * self.quantity
 
-
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Инициализирует экземпляры класса `Item`
+        """
+        cls.all = []
+        with open('../src/items.csv', 'r', newline='') as csvfile:
+
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+
+                Item(row["name"], row["price"], row["quantity"])
+
+    @staticmethod
+    def string_to_number(num):
+        """
+        Возвращает число из числа-строки
+        """
+        return math.floor(float(num))
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,8 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
 from src import item
+from src.item import Item
 
 
 def test_calculate_total_price():
@@ -12,3 +15,17 @@ def test_apply_discount():
     item.Item.pay_rate = 0.3
     test_item.apply_discount()
     assert test_item.price == 3000
+
+
+def test_name():
+    item1 = Item("Смартфон", 10000, 20)
+    item1.name = "НеСмартфон"
+    assert item1.name == 'НеСмартфон'
+
+    item1.name = "СуперПуперМегаДрайвДваНеСмартфон"
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
