d345a2f48449600eba5881e73d6ded0957140fcf
Nathova <122864131+Nathova@users.noreply.github.com>
2023-06-04 19:13:03 +0300

    Создан класс Keyboard

---
+++ src/keyboard.py
@@ -0,0 +1,27 @@
+from src.item import Item
+
+
+class MixinLang:
+    __lang = 'EN'
+
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+
+    @property
+    def language(self):
+        return self.__lang
+
+    def change_lang(self):
+        if self.__lang == 'EN':
+            self.__lang = 'RU'
+            return self
+        else:
+            self.__lang = 'EN'
+            return self
+
+
+class KeyBoard(MixinLang, Item):
+
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+
