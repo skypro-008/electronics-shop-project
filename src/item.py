cdd12f708b6dc550817d5168a5b232a5b28625b7
Мария Юркшайтене <masha.afan@bk.ru>
2023-06-04 12:28:59 +0300

    домашка 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,30 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) <= 10:
+            self.__name = new_name
+        return f"Название товара не соответствует"
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open("items.csv", encoding='utf-8') as r_file:
+            file_reader = csv.DictReader(r_file, delimiter=",")
+            return file_reader
+
+    @staticmethod
+    def string_to_number(string: str):
+        return int(string)
 
     def calculate_total_price(self) -> float:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,15 +1,41 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
+
+from src import item
 from src.item import Item
+
 
 @pytest.mark.parametrize('price, quantity, expected', [
     (10000, 20, 200000),
     (20000, 5, 100000)
 ])
 def test_calculate_total_price(price, quantity, expected):
-    assert calculate_total_price(price, quantity) == expected
+    assert Item.calculate_total_price(price, quantity) == expected
 
 
 def test_apply_discount():
-    assert apply_discount() == 10000
-    assert apply_discount() == 20000
+    assert Item.apply_discount() == 10000
+    assert Item.apply_discount() == 20000
+
+
+def test_name('Смартфон'):
+    assert isinstance(item, Item)
+    assert item.name == 'Смартфон'
+
+
+def test_name('СуперСмартфон'):
+    assert isinstance(item, Item)
+    assert item.name == 'Название товара не соответствует'
+
+
+def test_instantiate_from_csv():
+    assert len(Item.all) == 5
+
+
+@pytest.mark.parametrize('number ,expected', [
+    ('5', 5),
+    ('5.0', 5),
+    ('5.5', 5)
+])
+def test_string_to_number(number, expected):
+    assert Item.string_to_number(number) == expected
