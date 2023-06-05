4558bd60ca2d533ee85a270c557aef22226651e4
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-05 22:06:47 +0300

    add test for apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -8,3 +8,18 @@ def test_calculate_total_price():
 
     assert test_item_1.calculate_total_price() == 99999
     assert test_item_2.calculate_total_price() == 150
+
+
+def test_apply_discount():
+    test_item_1.apply_discount()
+    test_item_2.apply_discount()
+    assert test_item_1.price_with_discount == 999.99
+    assert test_item_2.price_with_discount == 50
+
+    Item.pay_rate = 0.9
+
+    test_item_1.apply_discount()
+    test_item_2.apply_discount()
+    
+    assert test_item_1.price_with_discount == 899.991
+    assert test_item_2.price_with_discount == 45
