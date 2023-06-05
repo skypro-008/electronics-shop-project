af9d40957ad23ba83341e8ff85c50cf90a4db2e8
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-05 21:45:25 +0300

    add test for test_calculate_total_price

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+test_item_1 = Item('test_1', 999.99, 100)
+test_item_2 = Item('test_2', 50, 3)
+
+
+def test_calculate_total_price():
+
+    assert test_item_1.calculate_total_price() == 99999
+    assert test_item_2.calculate_total_price() == 150
