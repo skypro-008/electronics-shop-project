e8a4a1366cc47832179769455d1b95ec75479e24
Alexandra <alexandrasolovjova@mail.ru>
2023-06-05 22:39:08 +0300

    added methods for class Item

--- src/item.py
+++ src/item.py
@@ -2,6 +2,7 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
+
     pay_rate = 1.0
     all = []
 
@@ -13,7 +14,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +25,13 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        cost = self.price * self.quantity
+        return cost
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        discount = self.price * self.pay_rate
+        self.price = discount
+
