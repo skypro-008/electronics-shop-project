ad876802be55d0ef36700c961150d485a39bc92a
Trashcorp567 <Trashcorp567@gmail.com>
2023-06-07 04:43:06 +0300

    Решение и тесты для первого дз

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert item1.apply_discount() == None
+    assert item1.price == 10000.0
+
+    assert item2.apply_discount() == None
+    assert item2.price == 20000
+
