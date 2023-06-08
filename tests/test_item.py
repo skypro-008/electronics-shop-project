fcd72513f5d8e1f79b73cf3644187cb44237c397
n.geraskin <psyangel93@mail.ru>
2023-06-08 09:53:43 +0300

    Update file test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -23,15 +23,23 @@ def test_apply_discount():
     assert item1.price == 8000.0
 
 
-def test_name():
+def test_name_getter():
     assert item1.name == "Смартфон"
 
 
 def test_instantiate_from_csv():
-    assert len(Item.all) == 1
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
 
 
 def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
+
+def test_name_setter():
+    item1.name = "Смартфон"
+    assert item1.name == "Смартфон"
+    item1.name = "АвадаКедавра"
+    assert Exception
