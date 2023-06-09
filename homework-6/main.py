b6aaf58bdcd777cea25b176517f9ffb09ae35b5a
fniad <artkamproject@gmail.com>
2023-06-10 00:30:46 +0300

    hw 6: add excepts and tests this

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,10 +1,14 @@
-from src.item import Item
+from src.item import Item, InstantiateCSVError
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv("../src/not_found_items.csv")
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv("../tests/without_colum_items.csv")
     # InstantiateCSVError: Файл item.csv поврежден
+
+    # Проверка, что данные с файла без ошибок считываются корректно
+    Item.instantiate_from_csv("../src/items.csv")
+    print(len(Item.all))
