79ad74846d7d504aa04be207215da523416592fd
Olesja1509 <olesja.janjic@gmail.com>
2023-06-10 10:49:41 +0200

    update test_item_apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -5,8 +5,6 @@ from src.item import Item
 
 
 @pytest.fixture
-
-
 def item1():
     return Item('Наушники', 5000, 10)
 
@@ -21,6 +19,5 @@ def test_item_calculate_total_price(item1):
     assert item1.calculate_total_price() == 50000
 
 
-def est_item_apply_discount(item1):
-    Item.pay_rate = 0.5
-    assert item1.apply_discount() == 2500.0
+def test_item_apply_discount(item1):
+    assert item1.apply_discount() is None
