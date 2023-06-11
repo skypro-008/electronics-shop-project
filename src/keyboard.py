e7d05afd2e7a139fad95e7e2d1574a9334b6ff3f
dmitry <dima.trenirovki@mail.ru>
2023-06-11 23:56:22 +0300

    Home_work5.2

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,10 +1,18 @@
-from src.item import Item
-
-class Keyboard(Item):
+class Developer:
     def __init__(self, name, price, quantity):
-        super().__init__(name, price, quantity)
+        self.name = name
+        self.price = price
+        self.quantity = quantity
         self.language = 'EN'
+    def __str__(self):
+        return f'{self.name}'
 
+    @property
+    def languages(self):
+        return self.language
+
+
+class MixinLog:
     def change_lang(self):
         if self.language == 'EN':
             self.language = 'RU'
@@ -12,3 +20,7 @@ class Keyboard(Item):
         else:
             self.language = 'EN'
             return self
+
+
+class Keyboard(Developer, MixinLog):
+    pass
