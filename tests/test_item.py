ffef1a39dcbbc4f1a0ff37207bc600beebc0e904
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-13 19:05:16 +0300

    add test for __repr__ method

--- tests/test_item.py
+++ tests/test_item.py
@@ -56,3 +56,7 @@ def test_str():
     assert str(test_item_1) == 'test_1'
     assert str(test_item_2) == 'test_2'
 
+
+def test_repr():
+    assert repr(test_item_1) == "Item('test_1', 999.99, 100)"
+    assert repr(test_item_2) == "Item('test_2', 50, 3)"
