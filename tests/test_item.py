ae820e39f66f9b3c637e415e53985459f4d98ac3
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-13 19:00:35 +0300

    add test for __str__ method

--- tests/test_item.py
+++ tests/test_item.py
@@ -50,3 +50,9 @@ def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('20.4') == 20
     assert Item.string_to_number('0') == 0
+
+
+def test_str():
+    assert str(test_item_1) == 'test_1'
+    assert str(test_item_2) == 'test_2'
+
