ab558f4f3754931a224b557887504b8ff62a2648
Evgeny <vajnbiy@gmail.com>
2023-06-16 10:22:50 -0700

    add HW4_1

--- src/phone.py
+++ src/phone.py
@@ -1,12 +1,29 @@
 from src.item import Item
 
 
+
+
 class Phone(Item):
 
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, add_sim: int):
+
+        if add_sim >= 0 and isinstance(add_sim, int):
+            self.__number_of_sim = add_sim
+
+        else:
+            raise ValueError('Неверное количество сим')
+
 
 
     def __repr__(self) -> str:
