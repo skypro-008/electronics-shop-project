07f9b727b10f0e1eea5455e59e30999e7d74aa88
Grandic <d.bitkov@gmail.com>
2023-06-19 22:13:17 +0300

    Add test and 1_homework is complete

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def exemple():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(exemple):
+    assert exemple.calculate_total_price() == 10000
+
+
+def test_apply_discount(exemple):
+    exemple.pay_rate = 0.8
+    exemple.apply_discount()
+    assert exemple.price == 8000
