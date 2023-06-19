f8d9b52e7ee4fe9ea53d7d30cc287c75f2a9ba7f
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-19 20:18:15 +0300

    update Phone class

--- src/phone.py
+++ src/phone.py
@@ -2,7 +2,23 @@ from src.item import Item
 
 
 class Phone(Item):
-    def __init__(self,name, price, quantity, number_of_sim):
+    def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        if 0 < number_of_sim == int(number_of_sim):
+            self.__number_of_sim = number_of_sim
+        else:
+            raise ValueError('Number of sim must be positive and int type')
+        
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
 
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim):
+        if 0 < number_of_sim == int(number_of_sim):
+            self.__number_of_sim = number_of_sim
+        else:
+            raise ValueError('Number of sim must be positive and int type')
