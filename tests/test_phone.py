d8ca800a68b0378ac920f1bfa3f008ca3cccb696
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-19 19:47:49 +0300

    add __add__ method of class Item

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,10 +1,19 @@
 from src.item import Item
 from src.phone import Phone
 
+phone_1 = Phone('test_phone', 50000, 10, 2)
+item_1 = Item('test_item', 10000, 7)
+
 
 def test_phone_init():
-    phone_1 = Phone('test_phone', 50000, 10, 2)
     assert phone_1.name == 'test_phone'
     assert phone_1.price == 50000
     assert phone_1.quantity == 10
     assert phone_1.number_of_sim == 2
+
+
+def test_phone_add():
+    assert item_1 + phone_1 == 17
+    assert phone_1 + phone_1 == 20
+    assert phone_1 + 6 is None
+
