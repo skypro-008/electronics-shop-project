133d00ead5722607df173ef4701bc3023200d787
Lumonza <mshpenkov@gmail.com>
2023-06-19 22:26:26 +0300

    pishem item.py

--- src/item.py
+++ src/item.py
@@ -35,13 +35,13 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        self.price += self.quantity
+        self.price *= self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price += self.pay_rate
+        self.price *= self.pay_rate
 
     @property
     def name(self):
@@ -86,3 +86,20 @@ class Item:
 
         except KeyError:
             raise InstantiateCSVError
+
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.__name
+
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            raise ValueError('складывать можно только объекты Item')
+        return int(self.quantity) + int(other.quantity)
+
+    @staticmethod
+    def string_to_number(num):
+        numb = float(num)
+        return int(numb)
