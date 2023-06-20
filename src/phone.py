ab0ff261fd567946aa74c08e4893a4fd06361289
Sergey <smirsand@mail.ru>
2023-06-20 19:14:06 +0300

    added file phone.py

---
+++ src/phone.py
@@ -0,0 +1,14 @@
+from src.item import Item
+
+
+class Phone(Item):
+    """
+    Класс для телефона
+    """
+
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+        super().__init__(name, price, quantity)
+        self.number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
