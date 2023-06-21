3b6c121f5630bf5ee1d3e365d067725375bbb517
Gerasimov <geraasim97@mail.ru>
2023-06-21 11:08:57 +0300

    done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,9 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+import pytest
 
+@pytest.fixture
+def exmp():
+    return Item("Смарфтон", 10000, 20)
 
-assert Item == Item("Сковородка", 1, 1)
+def calc(exmp):
+    assert exmp.calculate_total_price() == 10000
 
-assert 20000 * Item.pay_rate == Item.price
-
-assert Item.price == 80000
+def test_apply_discount(exmp):
+    exmp.pay_rate = 0.8
+    exmp.apply_discount()
+    assert exmp.price == 8000
