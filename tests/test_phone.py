e88a50b1041e17e8e52d541c14eab45d9f93ef09
Sergey <smirsand@mail.ru>
2023-06-21 10:37:49 +0300

    add code to test_phone.py

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,8 +1,20 @@
+import pytest
+
 from src.phone import Phone
 
 
 def test_phone():
-    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    phone1 = Phone("iPhone 14", 120000, 5, 2)
     assert str(phone1) == 'iPhone 14'
     assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
     assert phone1.number_of_sim == 2
+
+    phone1.number_of_sim = 3
+    assert phone1.number_of_sim == 3
+
+    with pytest.raises(ValueError):
+        phone1.number_of_sim = 0
+    with pytest.raises(ValueError):
+        phone1.number_of_sim = -1
+    with pytest.raises(ValueError):
+        phone1.number_of_sim = '1'
