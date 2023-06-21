a29d454a81d2afe677e531c16f9540eda98d3eac
Sergey <smirsand@mail.ru>
2023-06-21 10:38:44 +0300

    add code to phone.py

--- src/phone.py
+++ src/phone.py
@@ -8,7 +8,20 @@ class Phone(Item):
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        if not isinstance(number_of_sim, int) or number_of_sim <= 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self.__number_of_sim = number_of_sim
 
     def __repr__(self):
         return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if isinstance(value, int) and value > 0:
+            self.__number_of_sim = value
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
