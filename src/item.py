b265de8e3ba015f3dc477d7fe876116c639120f4
Sudarev_D <89365265993@mail.ru>
2023-06-21 10:23:14 +0300

    add item.py and tests

--- src/item.py
+++ src/item.py
@@ -13,7 +13,15 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
+
+
+
+
+
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +29,14 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        self.total_price = self.price * self.quantity
+        return self.total_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * Item.pay_rate
+
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+
+def test_Item():
+    assert item1.name == "Смартфон"
+    assert item1.price == 10000
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    total_price = item1.calculate_total_price() == 200000
+
