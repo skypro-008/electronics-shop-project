94af4b2acefe09a41721d24617fa12c9fd9198a8
Olesja1509 <olesja.janjic@gmail.com>
2023-06-22 14:17:11 +0200

    update class Phone

--- src/phone.py
+++ src/phone.py
@@ -4,7 +4,6 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
-        self.__name = name
         self.__number_of_sim = number_of_sim
 
     @property
@@ -20,7 +19,7 @@ class Phone(Item):
 
     def __repr__(self):
         super().__repr__()
-        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
 
     def __add__(self, other):
         """Сложение экземпляров класса по количеству товара в магазине"""
