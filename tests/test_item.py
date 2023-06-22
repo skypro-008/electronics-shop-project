da4796116cc4bdd1a94f50549ea4098501c026fd
Albert <albert.minnibaeff@yandex.ru>
2023-06-22 23:32:20 +0400

    homework-1

--- tests/test_item.py
+++ tests/test_item.py
@@ -8,13 +8,16 @@ from src.item import Item
 def item_test():
     return Item("Ноутбук", 50000, 5)
 
+
 def test_item_init(item_test):
     assert item_test.name == "Ноутбук"
     assert item_test.price == 50000
     assert item_test.quantity == 5
 
+
 def test_calculate_total_price(item_test):
     assert item_test.calculate_total_price() == 250000
+
 
 def test_apply_discount(item_test):
     Item.pay_rate = 0.5
