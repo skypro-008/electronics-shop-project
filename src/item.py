d531001058ca30a68b03377b294f90678b0de54a
Grandic <d.bitkov@gmail.com>
2023-06-22 22:36:43 +0300

    homework_2 was complete

--- src/item.py
+++ src/item.py
@@ -1,11 +1,13 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
-
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
@@ -13,11 +15,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
-
+        self.__name = name
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -31,3 +32,30 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        Item.all.clear()
+        with open('/home/dmitry/PycharmProjects/electronics-shop-project/src/items.csv', newline='', encoding="cp1251") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row["name"]
+                price = row["price"]
+                quantity = row["quantity"]
+                product = cls(name, price, quantity)
+        return product
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) <= 10:
+            self.__name = value
+        else:
+            self.__name = value[:10]
+
+    @staticmethod
+    def string_to_number(number):
+        return float(number[0])
