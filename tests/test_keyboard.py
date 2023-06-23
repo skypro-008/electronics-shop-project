c03fb37461afab0120111b96cf33eebd9011b2b9
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-23 19:58:05 +0300

    add test for __init__ method of Keyboard class

---
+++ tests/test_keyboard.py
@@ -0,0 +1,16 @@
+from src.keyboard import Keyboard
+
+
+k1 = Keyboard('test_1', 1000, 50)
+k2 = Keyboard('test_2', 10000, 5)
+
+
+def test_keyboard_init():
+    assert k1.name == 'test_1'
+    assert k2.name == 'test_2'
+    assert k1.price == 1000
+    assert k2.price == 10000
+    assert k1.quantity == 50
+    assert k2.quantity == 5
+    assert k1.language == "EN"
+    assert k2.language == "EN"
