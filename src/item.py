160bcec920ff526ea060a3bf6fdf3a86c6f3197e
Gerasimov <geraasim97@mail.ru>
2023-06-23 21:15:26 +0300

    done

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +15,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -30,3 +32,28 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+
+    def name(self, name):
+        if len(name) <=10:
+            self.__name = name
+        else:
+            print("Длина наименования превышает 10 символов")
+
+    def instantiate_from_csv():
+        with open('../src/items.csv', encoding='windows-1251') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                print(row['name'], row['price'])
+
+    @staticmethod
+    def string_to_number():
+        return int(float())
+
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -13,3 +13,17 @@ def test_apply_discount(exmp):
     exmp.pay_rate = 0.8
     exmp.apply_discount()
     assert exmp.price == 8000
+
+def test_name_setter(item):
+    item.name="Телефон"
+    assert item.name == "Телефон"
+    item.name = "Супертелефон"
+    assert item.name == "Супертелефон"
+
+def test_instantiate_from_csv(item):
+    item.instantiate_from_csv(5)
+    assert len(item.all) == 5
+    assert item.all[0].name == "Смарфтон"
+
+def test_string_to_nember(item):
+    assert isinstance(item.string_to_number(item.quantity), int)
