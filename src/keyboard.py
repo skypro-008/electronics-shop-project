aac5c37d115339b5922704d3f1b626d77eb22724
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-23 20:01:39 +0300

    add __init__ method of Keyboard class

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,4 +2,7 @@ from src.item import Item
 
 
 class Keyboard(Item):
-    pass
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        self.language = "EN"
+
