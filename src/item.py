d06aa6328505bda6e6499d66cc875c1cadffa35c
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-24 10:31:40 +0300

    add __init__ add calculate_total_price in item.py for class Item

--- src/item.py
+++ src/item.py
@@ -13,7 +13,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
@@ -21,7 +23,7 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return round(self.price * self.quantity, 1)
 
     def apply_discount(self) -> None:
         """
