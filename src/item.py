4fbeda1dab7fbfa406e1f8e645827250932780fa
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-24 11:00:21 +0300

    add apply_discount in item.py for class Item

--- src/item.py
+++ src/item.py
@@ -16,6 +16,7 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -29,4 +30,6 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * Item.pay_rate
+
+
