2e403a9917fc36fcfc95eb5f5ffd0f04739f8313
evgeni <9232485@gmail.com>
2023-06-26 16:47:18 +0300

    homework-2 Временный Пуш - для обратного обновленния с репозитория файла items.csv

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+
+import src
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,18 +18,15 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
 
-        # self.__class__.all.append(self)
-
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
 
@@ -37,3 +39,44 @@ class Item:
         """
         self.price = float(self.price * self.pay_rate)
         return self.price
+
+    @property
+    def name(self):
+        """
+        Возвращает название товара
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        Принимает новое название товара.
+        Сохраняет название товара с количеством символов не более 10
+        """
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Инициализирует экземпляр класса Item с данными из файла _src/items.csv_
+        """
+
+        with open('../src/items.csv', newline='', encoding='windows-1251') as csv_file:
+            reader_csv_file = csv.DictReader(csv_file)
+            for row in reader_csv_file:
+                name, price, quantity = row.split(',')
+                cls(name, price, quantity)
+
+
+
+
+                # return print()
+
+
+
+
+
+    # `string_to_number()` - статический метод, возвращающий число из числа-строки
