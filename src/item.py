7d0fc62cb2baaf95fca6daa254c28d2d0c7d7185
Olesja1509 <olesja.janjic@gmail.com>
2023-06-26 12:11:01 +0200

    edit item.py

--- src/item.py
+++ src/item.py
@@ -17,6 +17,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
+        super().__init__()
         self.__name = name
         self.price = price
         self.quantity = quantity
