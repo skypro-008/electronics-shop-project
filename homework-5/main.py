25248d29030ca08f6dc3a4c372f129bccdb7a6fb
Nickolay <psyangel93@mail.ru>
2023-06-27 23:06:23 +0300

    Update file

--- homework-5/main.py
+++ homework-5/main.py
@@ -1,7 +1,7 @@
 from src.keyboard import Keyboard
 
 if __name__ == '__main__':
-    kb = KeyBoard('Dark Project KD87A', 9600, 5)
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
     assert str(kb) == "Dark Project KD87A"
 
     assert str(kb.language) == "EN"
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     assert str(kb.language) == "RU"
 
     # Сделали RU -> EN -> RU
-    kb.change_lang().change_lang()
+    kb.change_lang()
+    kb.change_lang()
     assert str(kb.language) == "RU"
 
     kb.language = 'CH'
