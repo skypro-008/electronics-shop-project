4673e9df79d5bc4f26f7bb765d1cb40e179ea9df
Sergey <smirsand@mail.ru>
2023-06-28 02:02:04 +0300

    add code to keyboard.py

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,10 +3,17 @@ from src.item import Item
 
 class KeyboardMixin:
     def __init__(self):
-        self.language = 'RU'
+        self._language = 'EN'
+
+    @property
+    def language(self):
+        return self._language
 
     def change_lang(self):
-        self.language = 'RU'
+        if self._language == 'EN':
+            self._language = 'RU'
+        else:
+            self._language = 'EN'
         return self
 
 
@@ -17,4 +24,7 @@ class Keyboard(Item, KeyboardMixin):
 
     def __init__(self, name, price, quantity, language='EN'):
         super().__init__(name, price, quantity)
-        self.language = language
+        self._language = language
+
+    def __str__(self):
+        return self.name
