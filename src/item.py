9706d0ab7cec6fcf974ebaaa35dbd2e89d6a7dac
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-01 20:28:43 +0300

    pep8

--- src/item.py
+++ src/item.py
@@ -26,7 +26,7 @@ class Item:
         Item.all.append(self)
 
     def __repr__(self):
-        return f'{self.__class__.__name__}(\'{self.name}\', {(self.price)}, {str(self.quantity)})'
+        return f'{self.__class__.__name__}(\'{self.name}\', {self.price}, {str(self.quantity)})'
 
     def __str__(self):
         return f'{self.name}'
