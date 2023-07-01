7d62315d7f6596895224023db4feb86fc1dbd00c
Joyboy <khanmagomedovruslan77@gmail.com>
2023-07-01 17:59:16 +0300

    just testing

--- src/item.py
+++ src/item.py
@@ -1,3 +1,4 @@
+import csv
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +14,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self,name):
+        if len(self.__name) > 10:
+            raise ValueError('Длинна товара превышает 10 символов')
+        else:
+            self.__name = name[:10]
+
+
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +46,23 @@ class Item:
         """
         self.price *= self.pay_rate
         return self.price
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('C:/Users/Ruslan/PycharmProjects/electronics-shop-project/src/items.csv ', 'r', newline='') as cvsfile:
+            cls.all = []
+            reader = csv.DictReader(cvsfile)
+            for row in reader:
+                cls.__name = row['name']
+                cls.price = float(row['price'])
+                cls.quantity = int(row['quantity'])
+                item = Item(name, price, quantity)
+                cls.all.append(item)
+            print(cls.all)
+
+    @staticmethod
+    def string_to_number(stroka):
+        return int(''.join(filter(str.isdigit, stroka)))
+
+
+
