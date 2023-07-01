826d18dea708d405a2d3c4fb36d080ec177a72b0
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-01 23:12:48 +0300

    add @pytest.fixture add test_repr, test_str

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,22 +2,45 @@ import pytest
 from src.item import Item
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 
-test_item1 = Item("Товар1", 100, 2)
-test_item2 = Item("Товар2", 50, 3)
+
+@pytest.fixture
+def test_item1():
+    return Item("Товар1", 100, 2)
 
 
-def test_calculate_total_price():
+@pytest.fixture
+def test_item2():
+    return Item("Товар2", 50, 3)
+
+
+@pytest.fixture
+def test_item_all():
+    return Item.all
+
+
+def test_repr(test_item1):
+    assert test_item1.__repr__() == "Item('Товар1', 100, 2)"
+
+
+def test_str(test_item2):
+    assert test_item2.__str__() == 'Товар2'
+
+
+def test_instantiate_from_csv(test_item_all):
+    assert len(test_item_all) == 2
+
+
+def test_calculate_total_price(test_item1):
     assert test_item1.calculate_total_price() == 200.0
-    assert test_item2.calculate_total_price() == 150.0
 
 
-def test_apply_discount():
+def test_apply_discount(test_item1):
     Item.pay_rate = 0.8
     test_item1.apply_discount()
     assert test_item1.price == 80.0
 
 
-def test_name():
+def test_name(test_item1):
     test_item1.name = "Комп"
     assert test_item1.name == 'Комп'
     test_item1.name = "СуперКомпьютер"
@@ -27,7 +50,3 @@ def test_name():
 def test_string_to_number():
     assert Item.string_to_number('6.0') == 6
     assert Item.string_to_number('6') == 6
-
-
-def test_instantiate_from_csv():
-    assert len(Item.all) == 2
