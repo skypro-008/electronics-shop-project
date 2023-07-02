7585022f3fedb36fa2bf984613bf90f6b3b0533b
hexely <ihistos91@gmail.com>
2023-07-02 21:05:04 +0300

    Merge pull request #1 from hexely/task/feature
    homework 2 and tests

--- src/item.py
+++ src/item.py
@@ -11,7 +11,6 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
