b61b2ae4a11982378b3e9355af42b9231f99ed33
Dmitri.Chirov <dim.ch.it80@gmail.com>
2023-07-03 22:28:39 +0500

    make name private, add getter and setter

--- src/item.py
+++ src/item.py
@@ -13,11 +13,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
 
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        if type(value) is not str:
+            raise TypeError('name must be a string')
+        elif len(value) <= 10:
+            self.__name = value
+        else:
+            self.__name = value[:10]
 
     def calculate_total_price(self) -> float:
         """
@@ -26,7 +38,6 @@ class Item:
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
-
 
     def apply_discount(self) -> None:
         """
