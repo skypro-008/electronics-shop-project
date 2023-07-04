0195274c050dee05a78ca4401037e42c2d99b252
daren <titadarenka@gmail.com>
2023-07-04 22:11:38 +0300

    added class and tests

--- src/item.py
+++ src/item.py
@@ -5,6 +5,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
+
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -13,7 +14,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +25,11 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
+
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+@pytest.fixture
+
+def item():
+    return Item("cake", 5.5, 3)
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 16.5
+def test_apply_discount(item):
+    Item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 4.4
