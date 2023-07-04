18b5b343891b5f02dacd19e26a67b7014c63be28
Artem Osipov <tema.osipov@mail.ru>
2023-07-04 23:31:36 +0300

    write class Item for electronics shop

--- src/item.py
+++ src/item.py
@@ -2,29 +2,32 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate = 1.0
-    all = []
+
+    pay_rate = 1
+    all = ['<__main__.Item object at 0x7f2d476b8670>', '<__main__.Item object at 0x7f2d476b86d0>']
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
-        pass
+        self.total = float(self.price * self.quantity)
+        return self.total
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+
