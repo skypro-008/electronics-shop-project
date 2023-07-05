0ffb3ebd2152c155955298ff49e4754bd4749629
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-05 18:20:12 +0300

    add __number_of_sim add property add @number_of_sim.setter in phone.py

--- src/item.py
+++ src/item.py
@@ -31,6 +31,10 @@ class Item:
     def __str__(self):
         return f'{self.name}'
 
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
--- src/phone.py
+++ src/phone.py
@@ -3,7 +3,18 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
     def __repr__(self):
         return f'{self.__class__.__name__}(\'{self.name}\', {self.price}, {str(self.quantity)}, {self.number_of_sim})'
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, num_sim):
+        if num_sim > 0:
+            self.__number_of_sim = num_sim
+        else:
+            print('ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.')
