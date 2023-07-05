06615bf9ba6b609c91c61116a90bf82eb881508a
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-05 18:44:26 +0300

    add tests in test_phone.py

---
+++ tests/test_phone.py
@@ -0,0 +1,22 @@
+import pytest
+from src.phone import Phone
+
+
+@pytest.fixture
+def test_phone1():
+    return Phone("Телефон1", 100, 2, 2)
+
+
+def test_repr(test_phone1):
+    assert test_phone1.__repr__() == "Phone('Телефон1', 100, 2, 2)"
+
+
+def test_str(test_phone1):
+    assert test_phone1.__str__() == 'Телефон1'
+
+
+def test_number_of_sim(test_phone1):
+    test_phone1.number_of_sim = 1
+    assert test_phone1.number_of_sim == 1
+    test_phone1.number_of_sim = 0
+    assert test_phone1.number_of_sim == 1
