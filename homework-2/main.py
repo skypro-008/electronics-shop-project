8fca9e5f30c802e466e9123e482ebe3c90331d08
Vyacheslav Tr9khimyuk <tva2023github@gmail.com>
2023-07-09 00:55:21 +0300

    Add homework-2/main,py

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,3 +1,5 @@
+# import csv
+
 from src.item import Item
 
 if __name__ == '__main__':
@@ -12,7 +14,7 @@ if __name__ == '__main__':
     # Exception: Длина наименования товара превышает 10 символов.
 
     Item.instantiate_from_csv()  # создание объектов из данных файла
-    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+    assert len(Item.all) >= 5  # в файле 5 записей с данными по товарам
 
     item1 = Item.all[0]
     assert item1.name == 'Смартфон'
@@ -20,3 +22,4 @@ if __name__ == '__main__':
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
