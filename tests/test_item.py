811635c15b7a6f48c188e7ced2c8bb81da670337
Mikhail Gribkov <mishacool4@gmail.com>
2023-07-11 15:13:01 +0300

    added class Item and tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item1():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_item_init(item1):
+    assert item1.name == "Смартфон"
+    assert item1.price == 10000
+    assert item1.quantity == 20
+
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 200000
+
+
+def test_apply_discount(item1):
+    item1.apply_discount()
+    assert item1.price == 10000
