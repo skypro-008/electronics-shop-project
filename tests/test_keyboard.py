555b71f3d4b81381ca8d9d4f1d8a904191c218ec
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-14 16:09:17 +0300

    добавил тесты

---
+++ tests/test_keyboard.py
@@ -0,0 +1,18 @@
+import pytest
+from src.keyboard import Keyboard
+
+@pytest.fixture
+def test_kb1():
+    return Keyboard("KB1", 100, 2)
+
+def test_repr(test_kb1):
+    assert test_kb1.__repr__() == "Keyboard('KB1', 100, 2)"
+
+def test_language(test_kb1):
+    assert test_kb1.language == 'EN'
+
+def test_change_lang(test_kb1):
+    test_kb1.change_lang()
+    assert test_kb1.language == 'RU'
+    test_kb1.change_lang()
+    assert test_kb1.language == 'EN'
