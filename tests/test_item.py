9a6cd8f1d6754fd7063ede1eafde986cb67717ef
Alexandra Sazhaeva <alexa.sazhaeva@gmail.com>
2023-07-17 06:08:01 +0200

    Wrote test_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -14,3 +14,15 @@ def test_total_price():
     test_item2 = item.Item("Планшет", 16000, 8)
     assert test_item1.calculate_total_price() == 230000
     assert test_item2.calculate_total_price() == 128000
+
+
+def test_discount():
+    test_item1 = item.Item("Смартфон", 15000, 5)
+    test_item2 = item.Item("Фен", 6000, 15)
+
+    item.Item.pay_rate = 0.8
+    test_item1.apply_discount()
+    test_item2.apply_discount()
+
+    assert test_item1.price == 12000
+    assert test_item2.price == 4800
