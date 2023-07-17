5d6768a6d8d52d35d8243f093c8cea0f1f9c92d6
Vyacheslav Tr9khimyuk <tva2023github@gmail.com>
2023-07-17 22:59:48 +0300

    Add item.py

--- src/item.py
+++ src/item.py
@@ -21,6 +21,14 @@ class Item:
         self.all.append(self)
 
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+
+    def __str__(self):
+        return f'{self.__name}'
+
+
     @property
     def name(self):
         return self.__name
