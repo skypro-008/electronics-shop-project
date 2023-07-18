829e53f6894f731d2439909b77881cdd23ca3123
Andrey <doctor_linchenko@mail.ru>
2023-07-19 01:05:36 +0300

    its all

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture()
+def item():
+    return Item("Смартфон", 10000, 20)
+
+
+class TestItem:
+    def test_init_item(self, item):
+        assert item.price == 10000
+        assert item.name == "Смартфон"
+        assert item.quantity == 20
+        assert item.all == [item]
+
+
+    def test_calculate_total_price(self, item):
+        assert isinstance(item.calculate_total_price(), int)
+        assert item.calculate_total_price() == 200000
