7a3b9d6b3987888bc35ba9d41eef45b2f08bd4d1
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-19 19:33:51 +0300

    Добавил исключения

--- src/item.py
+++ src/item.py
@@ -4,6 +4,17 @@ import os
 PATH_TO_ITEMS_CSV = os.path.join("..", "src", "items.csv")
 
 
+class InstantiateCSVError(Exception):
+    def __init__(self):
+        self.message = 'InstantiateCSVError: Файл item.csv поврежден'
+
+    def __str__(self):
+        return self.message
+
+
+InstantiateCSV = InstantiateCSVError()
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -64,11 +75,21 @@ class Item:
 
     @classmethod
     def instantiate_from_csv(cls):
-        Item.all = []
-        with open(PATH_TO_ITEMS_CSV, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                cls(row['name'], float(row['price']), int(row['quantity']))
+
+        try:
+            Item.all = []
+            with open(PATH_TO_ITEMS_CSV, newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    cls(row['name'], float(row['price']), int(row['quantity']))
+        except FileNotFoundError:
+            print('FileNotFoundError: Отсутствует файл item.csv')
+        except TypeError:
+            print(InstantiateCSV)
+        except ValueError:
+            print(InstantiateCSV)
+        except KeyError:
+            print(InstantiateCSV)
 
     @staticmethod
     def string_to_number(string_num):
