6a3a907d3b57349015da259f7a62aba9f205b43a
u_alex90 <u_alex90@mail.ru>
2023-07-19 21:06:13 +1000

    Add comment in tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -6,16 +6,19 @@ def test_item():
     """
     Тестирование класса Item.
     """
+    # Test case 1 # attributes
     item = Item('test_name', 1000, 5)
     assert item.name == 'test_name'
     assert item.price == 1000
     assert item.quantity == 5
     assert item.calculate_total_price() == 5000
 
+    # Test case 2 # methods
     Item.pay_rate = 0.5
     item.apply_discount()
     assert item.price == 500
 
+    # Test case 3 # class variables
     assert len(Item.all) == 1
     item2 = Item('test_name2', 2000, 10)
     assert len(Item.all) == 2
