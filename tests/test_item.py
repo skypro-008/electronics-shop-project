3f69477afe5501ae022f1dedbc4d4d6047c39a35
ValentinaGuryanova <gvalushka@mail.ru>
2023-07-20 22:25:46 +0300

    Добавлены методы и тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+def test_apply_discount():
+    assert Item.pay_rate == 1
