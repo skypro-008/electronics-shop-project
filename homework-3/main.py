68cd950413218a8071243ee5560b344ffb1a1fcc
Alexandra Sazhaeva <alexa.sazhaeva@gmail.com>
2023-07-21 01:20:13 +0200

    Wrote __repr__

--- homework-3/main.py
+++ homework-3/main.py
@@ -3,4 +3,4 @@ from src.item import Item
 if __name__ == '__main__':
     item1 = Item("Смартфон", 10000, 20)
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
-    assert str(item1) == 'Смартфон'
+    #assert str(item1) == 'Смартфон'
