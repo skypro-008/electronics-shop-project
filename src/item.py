14e115bc488feaefd5369ed96f7964b1b1745e18
ValentinaGuryanova <gvalushka@mail.ru>
2023-07-23 23:12:09 +0300

    Добавлены класс-метод, статический метод и написаны тесты

--- src/item.py
+++ src/item.py
@@ -1,13 +1,20 @@
+import csv
+from pathlib import Path
+from accessify import private
+
+ROOT_DIR = Path(__file__).parent.parent
+SHOP_DATA = Path.joinpath(ROOT_DIR, "items.csv")
+
+
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
+    """ Класс для представления товара в магазине"""
+
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
-        Создание экземпляра класса item.
+        Создание экземпляра класса Item.
 
         :param name: Название товара.
         :param price: Цена за единицу товара.
@@ -16,6 +23,7 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+        # self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -25,9 +33,44 @@ class Item:
         """
         return float(self.price * self.quantity)
 
-    def apply_discount(self) -> float:
+    def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
-        return self.price
+
+    @classmethod
+    def all(cls):
+        return cls.instances
+
+    @property
+    def product_name(self):
+        """Getter для метода name"""
+
+        return self.__name
+
+    @product_name.setter
+    def product_name(self, name):
+        """Setter для метода name. Проверяем длину наименования товара """
+
+        if len(name) >= 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """Класс-метод, инициализирующий экземпляры класса Item"""
+
+        cls.all = []
+        with open('..\src\items.csv', encoding='windows-1251') as file:
+            DictReader_obj = csv.DictReader(file)
+            for item in DictReader_obj:
+                print(item)
+                new = cls(item['name'], float(item['price']), int(item['quantity']))
+                cls.all.append(new)
+
+    @staticmethod
+    def string_to_number(string):
+        """Возвращает число из числа-строки"""
+        return int(float(string))
--- tests/test_item.py
+++ tests/test_item.py
@@ -10,3 +10,12 @@ def test_calculate_total_price():
 
 def test_apply_discount():
     assert Item.pay_rate == 1
+
+#@classmethod
+def test_instantiate_from_csv():
+    assert Item.instantiate_from_csv() == None
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
