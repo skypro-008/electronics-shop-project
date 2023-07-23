30fc30bafce4ef94a3d87902db243483516f8302
Anastasia <aburlakova45@gmail.com>
2023-07-23 21:29:35 +0300

    first commit

--- src/item.py
+++ src/item.py
@@ -13,7 +13,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
