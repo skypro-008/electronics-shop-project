1934f1529ff44619c2b93875fb56af697d2cfeaf
Anastasia <aburlakova45@gmail.com>
2023-07-24 19:07:04 +0300

    тест функции apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -18,3 +18,12 @@ def test_calculate_total_price(make_items):
     item2.calculate_total_price()
     assert item1.total_price == 200000
     assert item2.total_price == 100000
+
+
+def test_apply_discount(make_items):
+    item1 = make_items[0]
+    item2 = make_items[1]
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000.0
+    assert item2.price == 20000
