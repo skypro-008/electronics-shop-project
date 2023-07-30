0daa824e91ad33c1add9117a261065b682699386
TurboYoga <kovin1333@gmail.com>
2023-07-30 15:17:08 +0300

    Test fixes

--- tests/test_item.py
+++ tests/test_item.py
@@ -4,13 +4,23 @@
 from src.item import Item
 
 
-# TestCase#1
 test_item = Item("Смартфон", 10000, 20)
-assert test_item.calculate_total_price() == 200000
 test_item_2 = Item("Ноутбук", 20000, 5)
+
+# TestCase#1 Item.__init__
+assert test_item.price == 10000
+assert test_item.name == "Смартфон"
+assert test_item.quantity == 20
+assert Item.all == [test_item, test_item_2]
+
+# TestCase#2 Item.calculate_total_price
+assert test_item.calculate_total_price() == 200000
 assert test_item_2.calculate_total_price() == 100000
 
-# TestCase#2
+# TestCase#3 Item.apply_discount
 assert test_item.apply_discount() == None
+assert test_item.price == 10000
 test_item.pay_rate = 0.8
-assert test_item.apply_discount() == None
+test_item.apply_discount()
+assert test_item.price == 8000
+
