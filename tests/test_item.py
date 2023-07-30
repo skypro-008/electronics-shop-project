a07819fad26caf7e928c329af14d2504728534ab
TurboYoga <kovin1333@gmail.com>
2023-07-30 14:37:36 +0300

    add calculate_total_price add TestCase#1 calculate_total_price

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+from src.item import Item
+
+
+# TestCase#1
+test_item = Item("Смартфон", 10000, 20)
+assert test_item.calculate_total_price() == 10000
+test_item_2 = Item("Ноутбук", 20000, 5)
+assert test_item_2.calculate_total_price() == 20000
