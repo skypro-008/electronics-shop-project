5591b54db8cba7b5ecc4884f95c0a83b024d03d2
Victor.S <vikcnc@mail.ru>
2023-07-31 19:25:29 +0500

    tests done!

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from pytest import fixture
+
+from src.item import Item
+
+
+@fixture
+def item():
+    return Item("Смартфон", 10000, 20)
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+def test_apply_discount(item):
+    item.apply_discount()
+    assert item.price == 10000
+
