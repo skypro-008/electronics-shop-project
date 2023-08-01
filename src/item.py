8b2cb3902b77859a1bdb32a9f7b0aaf784cb1a4b
Yulya Govorova <tishina1512@mail.ru>
2023-08-01 23:22:27 +0900

    added change

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -31,3 +34,70 @@ class Item:
         """
         self.price = self.price * self.quantity
 
+    def __repr__(self):
+        return f"Item(name={self.name}, price={self.price}, quantity={self.quantity})"
+
+    @property
+    def name(self) -> str:
+        """
+        Возвращает название товара.
+
+        :return: Название товара.
+        """
+        return self._name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        """
+        Устанавливает название товара.
+
+        :param value: Новое название товара.
+        """
+        if len(value) > 10:
+            self._name = value[:10]
+        else:
+            self._name = value
+
+    def calculate_total_price(self) -> float:
+        """
+        Рассчитывает общую стоимость конкретного товара в магазине.
+
+        :return: Общая стоимость товара.
+        """
+        return self.price * self.quantity
+
+    def apply_discount(self) -> None:
+        """
+        Применяет установленную скидку для конкретного товара.
+        """
+        self.price = self.price * Item.pay_rate
+
+    def __repr__(self):
+        return f"Item(name={self.name}, price={self.price}, quantity={self.quantity})"
+
+    def __str__(self):
+        return f"Item: {self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """
+        Инициализирует экземпляры класса Item данными из файла src/items.csv.
+        """
+        with open("src/items.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row["name"]
+                price = cls.string_to_number(row["price"])
+                quantity = int(row["quantity"])
+                item = cls(name, price, quantity)
+                cls.all.append(item)
+
+    @staticmethod
+    def string_to_number(value: str) -> float:
+        """
+        Возвращает число из числа-строки.
+
+        :param value: Число-строка.
+        :return: Число.
+        """
+        return float(value)
