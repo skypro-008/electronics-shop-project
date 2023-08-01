353f2689c6ea2556214b8d2ff6c77c6ac79c0d5e
Yulya Govorova <tishina1512@mail.ru>
2023-08-01 23:22:27 +0900

    added change

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,7 +1,4 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
-
-from item import Item
+from src.item import Item
 
 
 def test_calculate_total_price():
@@ -40,3 +37,56 @@ def test_apply_discount_with_hundred_percent_discount():
     item2 = Item("Ноутбук", 20000, 5)
     item2.apply_discount(100)
     assert item2.price == 0.0
+
+def test_name():
+    item = Item("Apple", 1.0, 10)
+    assert item.name == "Apple"
+
+def test_set_name():
+    item = Item("Apple", 1.0, 10)
+    item.name = "Banana"
+    assert item.name == "Banana"
+
+def test_set_long_name():
+    item = Item("Apple", 1.0, 10)
+    item.name = "LongNameForItem"
+    assert item.name == "LongNameFo"
+
+def test_calculate_total_price():
+    item = Item("Apple", 1.0, 10)
+    assert item.calculate_total_price() == 10.0
+
+def test_apply_discount():
+    Item.pay_rate = 0.9
+    item = Item("Apple", 1.0, 10)
+    item.apply_discount()
+    assert item.price == 0.9
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+if __name__ == '__main__':
+    test_name()
+    test_set_name()
+    test_set_long_name()
+    test_calculate_total_price()
+    test_apply_discount()
+    test_instantiate_from_csv()
+
+
+def test_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
