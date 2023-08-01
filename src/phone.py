781b5035078a6bde6442b40fdb5d4534588a4245
Anastasia <aburlakova45@gmail.com>
2023-08-01 06:09:53 +0300

    создание класса Phone

--- src/phone.py
+++ src/phone.py
@@ -3,6 +3,6 @@ from src.item import Item
 
 class Phone(Item):
 
-    def __int__(self, name, price, quantity, count_sim_card):
+    def __init__(self, name, price, quantity, count_sim_card):
         super().__init__(name, price, quantity)
         self.count_sim_card = count_sim_card
