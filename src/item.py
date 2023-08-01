91f772a4ea67f49d0377a79f08b11e8ef65d4284
Anastasia <aburlakova45@gmail.com>
2023-08-01 04:22:39 +0300

    реализация метода __repr__

--- src/item.py
+++ src/item.py
@@ -21,6 +21,9 @@ class Item:
         self.quantity = quantity
         self.all.append(self)
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
