77b5567a17a83b3e66888969ac5589e972525d2c
Anastasia <aburlakova45@gmail.com>
2023-08-01 20:01:28 +0300

    коментариии к классу phone

--- src/phone.py
+++ src/phone.py
@@ -4,9 +4,13 @@ from src.item import Item
 class Phone(Item):
 
     def __init__(self, name, price, quantity, number_of_sim):
+        """
+        Создание экземпляра класса Phone
+        """
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
     def __repr__(self):
+        """Вывод информации для разработчика"""
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
