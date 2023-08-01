9c0fef4729c291e053a8cf9219f0f42065ef76c5
Anastasia <aburlakova45@gmail.com>
2023-08-01 06:14:26 +0300

    реализация метода __repr__ для дочернего класса

--- src/phone.py
+++ src/phone.py
@@ -3,6 +3,10 @@ from src.item import Item
 
 class Phone(Item):
 
-    def __init__(self, name, price, quantity, count_sim_card):
+    def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.count_sim_card = count_sim_card
+        self.number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
