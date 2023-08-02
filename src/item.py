227603ec381c685ff1933c2997744db7a49448f3
TurboYoga <kovin1333@gmail.com>
2023-08-02 18:18:57 +0300

    fix getter and setter

--- src/item.py
+++ src/item.py
@@ -24,16 +24,16 @@ class Item:
         Item.all.append(self)
 
 
-        @property
-        def name(self):
-            return self.__name
+    @property
+    def name(self):
+        return self.__name
 
-        @name.setter
-        def name(self, name):
-            if len(name) <= 10:
-                self.__name = name
-            else:
-                self.__name = name[:10]
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
 
 
     def calculate_total_price(self) -> float:
