1fed6a3e367c6f4c3cd093a3f45cb83829e898d9
Victor Sadykov <svn1409@gmail.com>
2023-08-05 20:18:20 +0800

    update

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,18 +1,28 @@
-from src.item import Item
+import src.item as my_item
 
 if __name__ == '__main__':
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
+    item1 = my_item.Item("Смартфон", 10000, 20)
+    item2 = my_item.Item("Ноутбук", 20000, 5)
 
-    print(item1.calculate_total_price())  # 200000
-    print(item2.calculate_total_price())  # 100000
+    # print(item1.calculate_total_price())  # 200000
+    # print(item2.calculate_total_price())  # 100000
+    print(*[item.calculate_total_price() for item in my_item.Item.all], sep='\n')
 
     # устанавливаем новый уровень цен
-    Item.pay_rate = 0.8
-    # применяем скидку
+    my_item.Item.pay_rate = 0.8
+    # применяем скидку на первый товар
     item1.apply_discount()
 
-    print(item1.price)  # 8000.0
-    print(item2.price)  # 20000
+    # print(item1.price)  # 8000.0
+    # print(item2.price)  # 20000
+    print(*[item.price for item in my_item.Item.all], sep='\n')
 
-    print(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
+    print(my_item.Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
+
+    #мой вывод
+    print()
+    for item in my_item.Item.all:
+        print(f'{item.name} в количестве {item.quantity} по цене {my_item.nice_number_output(item.price)} руб. '
+              f'Итоговая сумма = {my_item.nice_number_output(item.calculate_total_price())} руб.')
+    # print(f'{item2.name} в количестве {item2.quantity} по цене {my_item.nice_number_output(item2.price)} руб. '
+    #       f'Итоговая сумма = {my_item.nice_number_output(item2.calculate_total_price())} руб.')
--- src/item.py
+++ src/item.py
@@ -1,3 +1,31 @@
+def nice_number_output(number) -> str:
+    """
+    возвращает красиво номер 100000.011 в виде "100 000.011'
+    """
+    result = ''
+    str_number = str(number)
+    # ищем точку в числе
+    if str_number.count('.') > 0:
+        is_dot = False
+        index_dot = 0
+        for i, str_n in enumerate(str_number[::-1]):
+            result = str_n + result
+            if is_dot:
+                if (i - index_dot) % 3 == 0:
+                    result = ' ' + result
+            if str_n == '.':
+                is_dot = True
+                index_dot = i
+    else:
+        # точки нет - выводим число с пробелами
+        for i, str_n in enumerate(str_number[::-1]):
+            result = str_n + result
+            if i % 3 == 2:
+                result = ' ' + result
+
+    return result
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -6,17 +34,18 @@ class Item:
     pay_rate = 0.85
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: float, quantity=1) -> None:
         """
         Создание экземпляра класса item.
 
         :param name: Название товара.
         :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
+        :param quantity: Количество товара в магазине, по умолчанию 1
         """
         self.name = name
         self.price = price
         self.quantity = quantity
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -52,3 +81,5 @@ class Item:
 # temp_item.apply_discount()
 # print(repr(temp_item))
 # print(f'{temp_item}, Итоговая сумма = {temp_item.calculate_total_price()} руб.')
+# print(nice_number_output(10000000))
+# print(nice_number_output(10000000.0111))
