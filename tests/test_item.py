d5f0870db69c3f25919c311014c80ca700541a42
TakuroBreath <maxsmurffy@gmail.com>
2023-08-05 16:34:10 +0300

    Update test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,11 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
 from src.item import Item
+
 
 def test_item():
     item1 = Item("Ноут", "20.0", 10)
 
-    assert item1.name == "Ноут"
+    assert item1.name == "Нт"
     assert item1.price == "20.0"
     assert item1.quantity == 10
-
