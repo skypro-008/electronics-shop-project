e7ac08a2169be3d353f2c26b8abbc2c7ad3de250
TurboYoga <kovin1333@gmail.com>
2023-08-08 18:34:52 +0300

    add repr and str add test repr and test str

--- src/item.py
+++ src/item.py
@@ -36,6 +36,14 @@ class Item:
             self.__name = name[:10]
 
 
+    def __repr__(self):
+        return f"Item('{self.__name}', {self.price}, {self.quantity})"
+
+
+    def __str__(self):
+        return f'{self.__name}'
+
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
