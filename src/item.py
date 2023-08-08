e710537affa72440f9ad0ab1ec42286016443f0a
TakuroBreath <maxsmurffy@gmail.com>
2023-08-08 08:30:20 +0300

    2nd hw

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,9 +15,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -30,3 +33,29 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            print("Длина наименования товара превышает 10 символов.")
+            self.__name = value[:10]
+        else:
+            self.__name = value
+
+    @staticmethod
+    def instantiate_from_csv():
+        """
+        Создание экземпляра класса item из csv-файла.
+        """
+        with open('../src/items.csv', "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                Item.all.append(row)
+
+    @staticmethod
+    def string_to_number(string):
+        return int(string)
