ca0de80d8bf70dcb7ddcf99232107c423c3518de
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-08-14 22:35:55 +0300

    Реализация тестов

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture()
+def test_item():
+    return Item(name="Phone", price=1000, quantity=10)
+
+
+def test_calculate_total_price(test_item):
+    assert test_item.calculate_total_price() == 10000
+
+
+def test_apply_discount(test_item):
+    test_item.pay_rate = 0.8
+    test_item.apply_discount()
+    assert 1000 * test_item.pay_rate == test_item.price
