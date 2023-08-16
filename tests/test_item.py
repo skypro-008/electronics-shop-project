77fcc477c9b84ecedfdc96639f2fa4f73762a9eb
fkotyashichev <fkotyashichev@gmail.com>
2023-08-16 23:43:46 +0300

    fixed tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -15,3 +15,21 @@ def test_calculate_total_price(product):
 def test_apply_discount(product):
     product.apply_discount()
     assert product.calculate_total_price() == 180000.0
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_name(product):
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+    item1.name = 'СуперСмартфон'
+    assert item1.name == 'СуперСмарт'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()  # создание объектов из данных файла
+    assert len(Item.all) == 5
