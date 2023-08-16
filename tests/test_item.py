9bb5c5884256fc7a61a6428fb532afd5930f3b62
Dmitry Dronov <cskafan1911@gmail.com>
2023-08-16 21:39:10 +0300

    Написан тест для функции apply_discount класса Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -14,3 +14,11 @@ def test_calculate_total_price(test_class):
     """
 
     assert test_class.calculate_total_price() == 24000
+
+
+def test_apply_discount(test_class):
+    """
+    Тестирует функцию применения скидки на определенный товар
+    """
+
+    assert test_class.apply_discount() == Item.pay_rate * Item.price
