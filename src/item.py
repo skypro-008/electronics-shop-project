79a79419f40d7460aabc35935aad87f5588e13e8
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-16 15:42:00 +0300

    add classmethod instantiate_from_csv for Item

--- src/item.py
+++ src/item.py
@@ -1,3 +1,9 @@
+import math
+import csv
+
+CSV_FILE = '..\\src\\1.csv'
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,6 +19,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
+        self.__name = ""
         self.name = name.strip()
         if not (isinstance(price, float) or isinstance(price, int)):
             raise ValueError('Цена за единицу товара должна быть только числом.')
@@ -39,3 +46,37 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            value = value[:10]
+        self.__name = value
+
+    @staticmethod
+    def string_to_number(value: str) -> int:
+        float_value = float(value)
+        return math.floor(float_value)
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        # Обнуляем список объектов класса
+        cls.all = []
+        with open(CSV_FILE, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Item(row.get('name'), float(row.get('price')), int(row.get('quantity')))
+
+
+if __name__ == "__main__":
+    print(Item.string_to_number('5.9'))
+    print(Item.string_to_number('5'))
+    print(Item.string_to_number('5.0'))
+
+    Item.instantiate_from_csv()
+    print(len(Item.all))
+    print(Item.all[0].name)
