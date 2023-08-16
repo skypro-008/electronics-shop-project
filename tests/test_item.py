0913961e3b43cd354655034112a405db9e9731ef
Dmitry Dronov <cskafan1911@gmail.com>
2023-08-16 21:38:16 +0300

    Написан тест для функции calculate_total_price класса Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture()
+def test_class():
+
+    return Item("Смартфон", 12000, 2)
+
+
+def test_calculate_total_price(test_class):
+    """
+    Тестирует функцию подсчета общей стоимости конкретного товара
+    """
+
+    assert test_class.calculate_total_price() == 24000
