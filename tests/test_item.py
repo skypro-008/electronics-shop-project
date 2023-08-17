7a9e5ebd029c5cc83f7defbfc99cce5991f3f5ae
Anaxuname <anaxuname@gmail.com>
2023-08-17 21:46:35 +0300

    Homework-1. Item class and tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_item():
+    item = Item("Телевизор", 100_000, 3)
+    assert item.name == "Телевизор"
+    assert item.price == 100_000
+    assert item.quantity == 3
+
+
+def test_calculate_total_price():
+    item = Item("Телевизор", 100_000, 3)
+    assert item.calculate_total_price() == 300_000
+
+
+def test_apply_discount():
+    item = Item("Телевизор", 100_000, 3)
+    Item.pay_rate = 0.5
+    item.apply_discount()
+    assert item.price == 50_000
