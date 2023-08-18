9a43bc86ab8ab198220f1b6803f5d180a9fc0fcf
Dmitry Dronov <cskafan1911@gmail.com>
2023-08-18 17:59:16 +0300

    Отредактирована функция apply_discount

--- src/item.py
+++ src/item.py
@@ -8,7 +8,6 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
@@ -16,6 +15,8 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -29,4 +30,4 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price = self.price * Item.pay_rate
+        self.price = self.price * self.pay_rate
