836eff9a35bca45a4095f727a62b39a47ea15b3b
erkenraimbaev <erken.orsk.2015@gmail.com>
2023-08-19 11:21:18 +0500

    В item.py дописаны свойства и методы Написаны тесты с покрытием 100% Добавлены новые зависимости

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,26 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+# from src.item import calculate_total_price, apply_discount
+
+
+@pytest.fixture
+def item1():
+    return Item('Lenovo', 50000, 5)
+
+
+def test_item_quantity(item1):
+    assert item1.quantity == 5
+
+
+def test_item_name(item1):
+    assert item1.name == 'Lenovo'
+
+
+def test_item_price(item1):
+    assert item1.calculate_total_price() == 250000
+
+
+def test_apply_discount(item1):
+    assert item1.apply_discount() == 50000
+
+
