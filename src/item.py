28db8da66f4a618f4f195b0e8daf2acdc906f13c
erkenraimbaev <erken.orsk.2015@gmail.com>
2023-08-20 16:24:29 +0500

    В item.py дописан код для ожидаемого поведения main.py 2 задания, написаны тесты с покрытием 76% Добавлены новые зависимости

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -8,15 +10,52 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
+        Item.all.append(self)
+
+
+    @property
+    def name(self):
+        """
+        Метод геттер для свойства name
+        """
+        return self.__name
+
+
+    @name.setter
+    def name(self, name):
+        """
+        Этот метод сеттер проверяет что длина наименования товара не больше 10 симвовов.
+        В противном случае, обрезает строку (оставляет первые 10 символов).
+        """
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all = []
+        with open('../src/items.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row.get('name'), float(row.get('price')), int(row.get('quantity')))
+
+
+
+    @staticmethod
+    def string_to_number(value: str):
+        """статический метод, возвращающий число из числа-строки
+        """
+        return int(float(value))
+
 
     def calculate_total_price(self) -> float:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,6 @@
 import pytest
 from src.item import Item
-# from src.item import calculate_total_price, apply_discount
+
 
 
 @pytest.fixture
@@ -23,4 +23,24 @@ def test_item_price(item1):
 def test_apply_discount(item1):
     assert item1.apply_discount() == 50000
 
+
+def test_instantiate_from_csv():
+    """Тестируем инициализацию списка элементов класса Item из файла src/items.csv"""
+    Item.instantiate_from_csv()
+    # Общее количество элементов в загруженном списке
+    assert len(Item.all) == 5
+    # Проверяем корректность загрузки первого элемента
+    item_test = Item.all[0]
+    assert item_test.name == 'Смартфон'
+    # Проверяем корректность загрузки четвертого элемента
+    item_test = Item.all[3]
+    assert item_test.name == 'Мышка'
+    assert item_test.price == 50
+
+
+def test_string_to_number():
+    """Тестируем статический метод, возвращающий число из числа-строки."""
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
 
