97cfa8f53b41f15dbca3a224487f0166c6122342
Dmitry Dronov <cskafan1911@gmail.com>
2023-08-22 22:06:29 +0300

    Отредактирован main.py (assert item1.get_name == 'Смартфон') для получения приватного атрибута

--- homework-2/main.py
+++ homework-2/main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
 
     item1 = Item.all[0]
-    assert item1.name == 'Смартфон'
+    assert item1.get_name == 'Смартфон'
 
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
