080e2378c23b92457d5bf797234eea92da8bf1df
IgorSorokin1985 <igorsorokin1985@gmail.com>
2023-08-23 15:01:28 +0300

    add getter, setter and functions

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+
+FILENAME = '../src/items.csv'
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +17,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -32,3 +36,23 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= Item.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name[:9]
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open(FILENAME, encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                item = Item(row['name'], row['price'], row['quantity'])
+
+    @staticmethod
+    def string_to_number(number):
+        result = int(float(number))
+        return result
