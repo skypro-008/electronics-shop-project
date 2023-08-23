b3830b10081b31084e9fcdce9b66ca740eb561e0
Andrey1989-G <goreglyad.andrey@gmail.com>
2023-08-23 22:02:01 +0500

    is ready

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+def test_item():
+    assert item1.price * item1.pay_rate == 10000.0
+    assert item2.name == "Ноутбук"
