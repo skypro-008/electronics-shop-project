8ba13baa18b66174b34922f5da17d5d9122860f0
nasty <nastyastep246@gmail.com>
2023-08-23 15:24:20 +0400

    homework 3

--- src/item.py
+++ src/item.py
@@ -23,6 +23,19 @@ class Item:
         self.quantity = quantity
         Item.all.append(self)
 
+    def __repr__(self):
+        """
+        выводит информацию об объекте для backend
+        """
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+    def __str__(self):
+        """
+        выводит информацию об объекте для пользователя
+        """
+        return f'{self.name}'
+
+
+
     @property
     def name(self):
         return self.__name
--- tests/test_item.py
+++ tests/test_item.py
@@ -47,3 +47,18 @@ def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
+def test_repr():
+    item5 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    item3 = Item('Телефон', 10000, 5)
+    assert repr(item5) == "Item('Смартфон', 10000, 20)"
+    assert repr(item2) == "Item('Ноутбук', 20000, 5)"
+    assert repr(item3) == "Item('Телефон', 10000, 5)"
+def test_str():
+    item5 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    item3 = Item('Телефон', 10000, 5)
+    assert str(item5) == 'Смартфон'
+    assert str(item2) == 'Ноутбук'
+    assert str(item3) == 'Телефон'
