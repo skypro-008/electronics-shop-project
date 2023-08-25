0c4008cbd0bec27d65fcf1ccca8333bfd69d5abb
Valeriya Dudchenko <vvd.2209@bk.ru>
2023-08-25 18:02:04 +0300

    Прописан файл item, добавлены тесты в test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+
+from src.item import Item
+
+def test_calculate_total_price(item1=None):
+    assert 10000 * 20 == 200000
+    assert item1.price * item1.quantity == item1.total_price
+
+
+def test_apply_discount():
+    assert 30000 * 1 == 30000
+    assert 20000 * 0.8 == 16000
+
+
+def test_calculate_total_price():
+    with pytest.raises(TypeError):
+        Item()
