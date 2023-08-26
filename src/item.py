b9ac697d805ac07268804d88605e1b1f0b5a1b44
ilyas <jubiev2017@gmail.com>
2023-08-26 19:27:38 +0300

    Merge remote-tracking branch 'origin/main'

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -33,4 +36,25 @@ class Item:
         """
         self.price = self.price * self.pay_rate
 
+    @property
+    def name(self):
+        return self.__name
 
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            name = name[0:10]
+        self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open("../src/items.csv", newline="")as csfile:
+            reader = csv.DictReader(csfile)
+            for row in reader:
+                print(row["name"], row["price"], row["quantity"])
+                cls(row["name"], row["price"], row["quantity"])
+
+    @staticmethod
+    def string_to_number(str_number):
+        str_number = int(str_number)
+        return str_number
