99eaf2158b8ea718b0d0d1553726cbeb6b5a5fea
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-08-28 19:52:07 +0300

    Реализация класса Phone, __init__, __repr__

---
+++ src/phone.py
@@ -0,0 +1,10 @@
+from src.item import Item
+
+
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        self.number_of_sim = number_of_sim
+        super().__init__(name, price, quantity)
+
+    def __repr__(self):
+        return f"{__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
