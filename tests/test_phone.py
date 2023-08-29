dc785303e7d70957863f3799a7856e0b4e1a6a71
Dmitry Dronov <cskafan1911@gmail.com>
2023-08-29 22:35:39 +0300

    Написан тест для метода add класса Phone

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.phone import Phone
+from src.item import Item
 
 
 @pytest.fixture
@@ -23,3 +24,14 @@ def test_repr(test_class):
     """
     phone1 = test_class
     assert repr(phone1) == "Phone('Iphone 14', 100000, 3, 2)"
+
+
+def test_add(test_class):
+    """
+    Тестируем метод add
+    """
+    item1 = Item("Смартфон", 10000, 20)
+    phone1 = test_class
+
+    #assert item1 + phone1 == 23
+    assert phone1 + phone1 == 6
