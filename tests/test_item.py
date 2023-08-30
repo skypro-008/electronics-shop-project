91d06daec39693bbb709ef717d319663b44cb229
Pavel Glavan <pglavan1998@mail.ru>
2023-08-31 00:03:47 +0300

    add home_work 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("Samsung Galaxy", 60000, 10)
+
+
+def test_init():
+    assert item1.name == "Samsung Galaxy"
+    assert item1.price == 60000
+    assert item1.quantity == 10
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 600000
+
+
+def test_apply_discount():
+    assert item1.apply_discount() == 60000
