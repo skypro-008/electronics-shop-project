0261cf116a89390dbbd874f9e3b033c4da3e709c
Pavel Glavan <pglavan1998@mail.ru>
2023-08-31 02:46:49 +0300

    add home_work 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -30,4 +33,39 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        return self.price * self.pay_rate
+        self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name_):
+        """
+        присваивает название товара если его длина менее 10 символов
+        :name_: название товара
+        """
+        if len(self.__name) < 10:
+            self.__name = name_
+        else:
+            self.__name = self.__name[0:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        """
+        открывает csv файл и создает на их основе экземпляры класса
+        :path: путь к файлу
+        """
+        Item.all = []
+        with open(path, "r") as file:
+            file_reader = csv.DictReader(file)
+            for product in file_reader:
+                cls(product["name"], float(product["price"]), int(product["quantity"]))
+
+    @staticmethod
+    def string_to_number(string_number):
+        """
+        меняет тип данных из str в int
+        :string_number:число-строка
+        """
+        return int(float(string_number))
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,17 +1,37 @@
-from src.item import Item
+import pytest
 
-item1 = Item("Samsung Galaxy", 60000, 10)
+from src.item import Item
+from src.settings import CSV
+
+item1 = Item("Samsung Galaxy", 10000, 10)
 
 
 def test_init():
     assert item1.name == "Samsung Galaxy"
-    assert item1.price == 60000
+    assert item1.price == 10000
     assert item1.quantity == 10
 
 
 def test_calculate_total_price():
-    assert item1.calculate_total_price() == 600000
+    assert item1.calculate_total_price() == 100000
 
 
 def test_apply_discount():
-    assert item1.apply_discount() == 60000
+    Item.pay_rate = 0.9
+    item1.apply_discount()
+    assert item1.price == 9000
+
+
+def test_property_name():
+    assert item1.name == "Samsung Galaxy"
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv(CSV)
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
