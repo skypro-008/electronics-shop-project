9b94f78202656283c2be40946257ef0c46c99490
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-08-31 19:24:56 +0300

    Редактирование main согласно исходника

--- homework-5/main.py
+++ homework-5/main.py
@@ -3,15 +3,15 @@ from src.keyboard import Keyboard
 if __name__ == '__main__':
     kb = Keyboard('Dark Project KD87A', 9600, 5)
     assert str(kb) == "Dark Project KD87A"
-
+    print(kb.__dict__)
     assert str(kb.language) == "EN"
 
     kb.change_lang()
     assert str(kb.language) == "RU"
 
-    # Сделали RU -> EN -> RU
-    kb.change_lang().change_lang()
-    assert str(kb.language) == "RU"
+    # Сделали EN -> RU -> EN
+    kb.change_lang()
+    assert str(kb.language) == "EN"
 
     kb.language = 'CH'
     # AttributeError: property 'language' of 'Keyboard' object has no setter
