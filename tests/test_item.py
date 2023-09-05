af37dce39db37f421c9ed318dd15d27656cba4bc
Shepetkov Timerlan <sergeevp284@gmail.com>
2023-09-05 21:10:46 +0300

    home_work1 ready

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
+from src.item import Item
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.calculate_total_price() == 200000
+
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    item1.pay_rate = 0.5
+    item1.apply_discount()
+    assert item1.price == 5000
