b936a491f5684646b29cff6c57fd48fc121dd98e
Roman Sharoyan <sharoyan24@yandex.ru>
2023-09-09 20:26:55 +0300

    5_home

---
+++ src/keyboard.py
@@ -0,0 +1,39 @@
+from src.item import Item
+
+
+class Mix_Language:
+    """
+    класс миксин для хранения и изменения раскадки клавиатуры
+    """
+    def __init__(self, en, ru):
+        self._EN = en
+        self._RU = ru
+        self._language = self._EN
+
+    def change_language(self):
+        if self._language == self._EN:
+            self._language = self._RU
+        else:
+            self._language = self._EN
+        return self
+
+
+class Keyboard(Item, Mix_Language):
+
+    """
+    класс для смены языка клавиатуры
+    """
+    def __init__(self, name: str, price: float, quantity: int, language="EN") -> None:
+        super().__init__(name, price, quantity)
+        Mix_Language.__init__(self, 'EN', 'RU')
+        self._language = language
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name}', {self.price}, {self.quantity}, {self.language}"
+
+    def __str__(self):
+        return f"{self.name}"
+
+    @property
+    def language(self):
+        return self._language
