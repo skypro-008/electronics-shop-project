a3ec22426c961d930f1c2158c856503bb6d3ae99
AnnaK <njutcka@yandex.ru>
2023-09-10 18:17:24 +0700

    homework-3

--- tests/test_item.py
+++ tests/test_item.py
@@ -23,3 +23,11 @@ def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
+def test__repr__():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+def test__str__():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
