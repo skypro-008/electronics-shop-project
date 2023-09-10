05500b52653e5dcb9aad9e532b67091c0f89b0ad
Sergei <you@example.com>
2023-09-10 21:55:17 +0800

    homework-1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+@pytest.fixture()
+def item2():
+    return Item("Ноутбук", 20000, 5)
+
+def test_calculate_total_price(item2):
+    assert item2.calculate_total_price() == 20000 * 5
+
+def test_apply_discount(item2):
+    item2.pay_rate = 0.4
+    item2.apply_discount()
+    assert item2.price == 0.4 * 20000
