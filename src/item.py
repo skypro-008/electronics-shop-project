bb0b365ffeeffe5afa1d636a7a4828e7bf363716
denis ukrajev <humanwithbeard@gmail.com>
2023-09-12 19:34:31 +0300

    added

--- src/item.py
+++ src/item.py
@@ -1,30 +1,18 @@
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+    def __init__(self, name, price, quantity) -> None:
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
-        """
-        pass
+        return self.price*self.quantity
 
     def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+        discount_amount = self.price = self.price * Item.pay_rate
+        return discount_amount
+
+
