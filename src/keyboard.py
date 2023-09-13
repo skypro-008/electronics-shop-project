29eb3a0caccafa443fd6530a1668f4891eca9228
Andrey1989-G <goreglyad.andrey@gmail.com>
2023-09-13 19:42:44 +0500

    homework 4 is ready

--- src/keyboard.py
+++ src/keyboard.py
@@ -7,20 +7,23 @@ class MixinLog:
     def __init__(self):
         self.language = self.ENG
 
-    def change_lang(self):
+    def change_lang(self) -> str:
         if self.language == "EN":
             self.language = "RU"
         elif self.language == "RU":
             self.language = "EN"
-        return self.language
+        return self
 
     def get_language(self):
         return self.language
 
 
 class Keyboard(Item, MixinLog):
-    pass
+    def __init__(self, name: str, price: float, quantity: int):
+        super().__init__(name, price, quantity)
+        MixinLog.__init__(self)
 
 # s = MixinLog()
 # s.language = "RU"
 # print(s.language)
+
