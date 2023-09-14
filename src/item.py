2d545e64ac871fc0c937eeef71eb40bd221e0830
denis ukrajev <humanwithbeard@gmail.com>
2023-09-14 18:26:25 +0300

    addeda

--- src/item.py
+++ src/item.py
@@ -1,18 +1,40 @@
+import csv
+
+
 class Item:
     pay_rate = 1.0
     all = []
 
     def __init__(self, name, price, quantity) -> None:
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
 
     def calculate_total_price(self) -> float:
-        return self.price*self.quantity
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         discount_amount = self.price = self.price * Item.pay_rate
         return discount_amount
 
+    @property
+    def name(self):
+        return self.__name
 
+    @name.setter
+    def name(self, value):
+        if len(value) > 0:
+            self.__name = value[:10]
+        else:
+            self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls, csv_data):
+        with open(csv_data, 'r', encoding='windows-1251') as file:
+            file = csv.DictReader(file)
+            for info in file:
+                print(info)
+
+
+Item.instantiate_from_csv('../src/items.csv')
