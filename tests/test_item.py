f255c90b0184b818041e07763d3b7696be9f410f
VladimirKovaliev <kovaliev.v@list.ru>
2023-09-15 13:03:32 +0300

    fixes

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,27 +1,35 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+"""Тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
 
 def test_calculate_total_price():
-    apple = Item('apple', 1, 10)
-    assert apple.calculate_total_price() == 10
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
 
-    orange = Item('orange', 2, 5)
-    assert orange.calculate_total_price() == 10
-
-    apple.apply_discount(0.9)
-    assert apple.calculate_total_price() == 9
-
-    orange.apply_discount(0.8)
-    assert orange.calculate_total_price() == 8
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
 
 def test_apply_discount():
-    apple = Item('apple', 1, 10)
-    assert apple.pay_rate == 1.0
+    item = Item("Смартфон", 10000, 20)
 
-    apple.apply_discount(0.9)
-    assert apple.pay_rate == 0.9
+    Item.pay_rate = 0.8
+    item.apply_discount(0.2)
 
-    apple.apply_discount(0.8)
-    assert apple.pay_rate == 0.8
+    assert item.price == 1600.0000000000002
 
+def test_apply_discount_for_all_items():
+
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+
+    Item.pay_rate = 0.8
+
+    item1.apply_discount(1)
+    item2.apply_discount(1)
+
+
+    assert item1.price == 8000
+    assert item2.price == 16000
+    assert item1.calculate_total_price() == 128000.0
+    assert item2.calculate_total_price() == 64000.0
