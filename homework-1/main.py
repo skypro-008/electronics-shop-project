e900e6a406759e819d1aa7af17de35875852f7f8
VladimirKovaliev <kovaliev.v@list.ru>
2023-09-15 12:50:02 +0300

    fixes

--- homework-1/main.py
+++ homework-1/main.py
@@ -4,13 +4,13 @@ if __name__ == '__main__':
     item1 = Item("Смартфон", 10000, 20)
     item2 = Item("Ноутбук", 20000, 5)
 
-    print(item1.calculate_total_price())  # 200000
-    print(item2.calculate_total_price())  # 100000
+    print(int(item1.calculate_total_price()))  # 200000
+    print(int(item2.calculate_total_price()))  # 100000
 
     # устанавливаем новый уровень цен
     Item.pay_rate = 0.8
     # применяем скидку
-    item1.apply_discount()
+    item1.apply_discount(1)
 
     print(item1.price)  # 8000.0
     print(item2.price)  # 20000
