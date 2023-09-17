1c22d0686c4af20ed9f14d4ce7c2d69ef50a0ee8
Mikolian <mikolian@mail.ru>
2023-09-17 12:50:46 +0300

    2 hw

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+import math
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,7 +7,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int):
         """
         Создание экземпляра класса item.
 
@@ -13,11 +15,26 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.__name = name
+        self._name = name
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self) -> float:
+        @property
+        def name(self):
+            return self._name
+
+        @name.setter
+        def name(self, name):
+        """проверка длины"""
+
+            self._name = name[:10]
+
+    @classmethod
+    def string_to_number(cls, string):
+        pass
+
+
+def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
@@ -26,8 +43,27 @@ class Item:
         self.total_price = self.price * self.quantity
         return self.total_price
 
-    def apply_discount(self) -> None:
+    def apply_discount(self):
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+        return self.price
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', encoding='cp1251') as csvfile:
+            read = csv.DictReader(csvfile)
+            for x in read:
+               name = x["name"]
+               price = x["price"]
+               quantity = x["quantity"]
+               item = cls(name, price, quantity)
+               Item.all.append(item)
+            return Item.all
+
+
+    @staticmethod
+    def string_to_number(line):
+        x = float(line)
+        return int(x)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,3 +1,30 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
+@pytest.fixture()
+def item():
+    item1 = Item("Смартфон", 10000, 20)
+    return item1
+
+def test_item_initialization(item):
+    assert item.name == "Смартфон"
+    assert item.price == 10000
+    assert item.quantity == 20
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+def test_string_to_number_1():
+    string = "2"
+    assert Item.string_to_number(string) == 2
+
+def test_string_to_number_2():
+    string = "2.3"
+    assert Item.string_to_number(string) == 2
+
+def test_apply_discount(item):
+    assert Item.apply_discount(item) == 10000.0
+
+def test_name_setter(item):
+    item.name = 'FLŰGGÅƏNK∂€ČHIŒβØL∫ÊN'
+    assert item._name == "FLŰGGÅƏNK∂"
