025269c47c91c48148f3d093e5772590fa424d01
Acer <dmv053@gmail.com>
2023-09-19 00:33:27 +0400

    написаны тесты на ДЗ №2

--- tests/test_item.py
+++ tests/test_item.py
@@ -7,18 +7,30 @@ from src.item import Item
 def item_test():
     return Item("Миксер", 10.2, 15)
 
+
 def test_item_init(item_test):
 
     assert item_test.name == "Миксер"
     assert item_test.price == 10.2
     assert item_test.quantity == 15
 
+
 def test_item_calculate_total_price (item_test):
 
     assert item_test.calculate_total_price() == 153
+
 
 def test_item_apply_discount (item_test):
 
     Item.pay_rate = 0.5
     item_test.apply_discount()
     assert item_test.price == 5.1
+
+def test_item_instantiate_from_csv():
+    Item.all = []
+    Item.instantiate_from_csv('tests/test_items.csv')
+    assert len(Item.all) == 4
+    assert Item.all[0].name == 'Осцилограф'
+    assert Item.all[0].quantity == 0
+    assert Item.all[2].quantity == 5
+    assert Item.all[3].price == 50.5
