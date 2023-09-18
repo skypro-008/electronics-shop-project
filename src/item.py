fd3d0d235a1961290ce3608405b8dc71e6ec7074
Acer <dmv053@gmail.com>
2023-09-19 00:33:01 +0400

    Выполнены задания ДЗ №2 арибут name - приватный, создан сеттер для name с обрезкой до 10 символов реализован класс-метод instantiate_from_csv() реализован стат-метод string_to_number()

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os.path
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,7 +9,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name_: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
@@ -13,11 +17,52 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name_
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
 
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self,name_: str):
+        """
+        Присваивает экземпляру атрибут __name: str ,
+        после проверки: длина строки должна быть не более 10,
+        если входящая строка больше 10, то обрезается до 10  символов.
+        """
+        if len(name_) <= 10:
+            self.__name = name_
+        else:
+            self.__name = name_[0:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, file_name):
+        n_dir, n_file = file_name.split('/')
+        file_name = os.path.join('..', n_dir, n_file)
+        with open(file_name, "r") as file:
+            reader = csv.DictReader(file)
+            for line in reader:
+                name_ = line['name']
+                price = float(line['price'])
+                quantity = Item.string_to_number(line['quantity'])
+                x = cls(name_, price, quantity)
+                x.name = name_
+
+
+    @staticmethod
+    def string_to_number(str_):
+        '''статический метод, возвращающий число из числа-строки'''
+
+        l = str_.split('.')
+        if not l[0].isdigit():
+            number = 0
+        else:
+            number = int(l[0])
+        return number
 
     def calculate_total_price(self) -> float:
         """
