8bc0f32ff5e03ac8ddebdb90b4bec86e5674a03a
wwwru <www.rufat@bk.ru>
2023-09-21 20:37:31 +0300

    TWO_dz_item_9

--- src/phone.py
+++ src/phone.py
@@ -14,3 +14,14 @@ class Phone(Item):
         if not isinstance(other, Item):
             raise ValueError('Можно складывать только объекты классов Item и Phone.')
         return self.quantity+other.quantity
+
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, numb):
+        if numb > 0:
+            self._number_of_sim = numb
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
