2e8681b8a92538ac0f720d397f09919c834ac44f
VladimirKovaliev <kovaliev.v@list.ru>
2023-09-22 17:20:06 +0300

    add fixture

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,68 +1,70 @@
-"""Тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
 
-def test_calculate_total_price():
-    item1 = Item("Смартфон", 10000, 20)
+@pytest.fixture
+def item_fixture() -> Item:
+    item = Item("Смартфон", 1000, 2)
+    Item.all.append(item)
+    yield item
+
+def test_calculate_total_price(item_fixture):
+    item1 = item_fixture
     item2 = Item("Ноутбук", 20000, 5)
 
-    assert item1.calculate_total_price() == 200000
+    assert item1.calculate_total_price() == 2000
     assert item2.calculate_total_price() == 100000
 
-def test_apply_discount():
-    item = Item("Смартфон", 10000, 20)
+def test_apply_discount(item_fixture):
+    item = item_fixture
 
-    Item.pay_rate = 0.8
+    item.pay_rate = 0.8
     item.apply_discount(0.8)
 
-    assert item.price == 8000
+    assert item.price == 800
 
-def test_apply_discount_for_all_items():
+def test_apply_discount_for_all_items(item_fixture):
 
-    item1 = Item("Смартфон", 10000, 20)
+    item1 = item_fixture
     item2 = Item("Ноутбук", 20000, 5)
 
-
-    Item.pay_rate = 0.8
+    item1.pay_rate = 0.8
+    item2.pay_rate = 0.8
 
     item1.apply_discount(0.8)
+    item1.apply_discount(0.2)
     item2.apply_discount(0.5)
 
-
-    assert item1.price == 8000
+    assert item1.price == 160
     assert item2.price == 10000
-    assert item1.calculate_total_price() == 128000.0
-    assert item2.calculate_total_price() == 40000.0
+    assert item1.calculate_total_price() == 256
+    assert item2.calculate_total_price() == 40000
 
-from src.item import Item
-
-
-def test_item():
-
+def test_item(item_fixture):
     # создание и проверка первого объекта
-    item1 = Item('Телефон', 10000, 5)
-    assert len(Item.all) == 1
-    assert item1.name == 'Телефон'
-    assert item1.price == 10000
-    assert item1.quantity == 5
-    assert item1.calculate_total_price() == 50000.0
+    item1 = item_fixture
+    assert len(Item.all) == 2
+    assert item1.name == 'Смартфон'
+    assert item1.price == 1000
+    assert item1.quantity == 2
+    assert item1.calculate_total_price() == 2000
 
     # изменение свойства name
-    item1.name = 'Смартфон'
-    assert item1.name == 'Смартфон'
+    item1.name = 'Телефон'
+    assert item1.name == 'Телефон'
 
     # создание и проверка второго объекта с длинным name
     big_item = Item('СуперСмартфон', 5000, 10)
+    Item.all.append(big_item)
     assert big_item.name == 'СуперСмартфон'
 
     # создание объектов из файла csv
     Item.instantiate_from_csv('src/items.csv')
-    assert len(Item.all) == 7
+    assert len(Item.all) == 9
     item2 = Item.all[0]
-    assert item2.name == 'Смартфон'
-    assert item2.price == 10000
-    assert item2.quantity == 5
-    assert item2.calculate_total_price() == 50000.0
+    assert item2.name == 'Телефон'
+    assert item2.price == 1000
+    assert item2.quantity == 2
+    assert item2.calculate_total_price() == 2000
 
     # проверка метода string_to_number
     assert Item.string_to_number('5') == 5.0
@@ -72,6 +74,7 @@ def test_item():
 def test___repr__():
     item = Item("Ноутбук", 20000, 5)
     assert item.__repr__() == "Item('Ноутбук', 20000, 5)"
+
 def test___str__():
     item = Item("Ноутбук", 20000, 5)
     assert item.__str__() == 'Ноутбук'
