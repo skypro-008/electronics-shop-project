a6e6377f8eecc40413f05d2d61d81e31498ce681
Tatiana Makarova <tanymv2004@gmail.com>
2023-09-23 14:21:55 +0500

    ready

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import unittest
+from src import item
+class TestItem(unittest.TestCase):
+    """Тест для класса Item"""
+
+    def test_calculate_total_price(self):
+        item1 = item.Item("Смартфон", 10000, 20)
+        self.assertEqual(item1.calculate_total_price(), 200000)
+
+
+if __name__ == '__main__':
+    unittest.main()
