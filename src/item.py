975227a1fe6a69540b885ecdf360849a09dd2b13
Nikolay Gordeev <ncgordeev@gmail.com>
2023-09-23 22:25:16 +0300

    Добавлены docstrings для тестов и методов

--- src/item.py
+++ src/item.py
@@ -1,4 +1,7 @@
 class Item:
+    """
+    Класс представления товара
+    """
     pay_rate = 1
     all = []
 
@@ -9,11 +12,17 @@ class Item:
         Item.all.append(self)
 
     def calculate_total_price(self):
+        """
+        Метод возвращает общее кол-во товара в магазине
+        """
         total_price = self.price * self.total_items
         if self.total_items == 0:
             return f"Товар закончился"
         return total_price
 
     def apply_discount(self) -> float:
+        """
+        Метод возвращает цену в зависимости от коэ-та скидки
+        """
         self.price *= Item.pay_rate
         return self.price
--- tests/test_item.py
+++ tests/test_item.py
@@ -9,12 +9,18 @@ item3 = Item("Фотоаппарат", 50000, 0)
 
 
 def test_calculate_total_price():
+    """
+    Тест метода, вычисляющего общую цену товара
+    """
     assert item1.calculate_total_price() == 200000
     assert item2.calculate_total_price() == 100000
     assert item3.calculate_total_price() == "Товар закончился"
 
 
 def test_apply_discount():
+    """
+    Тест метода, применяющего скидку
+    """
     assert item1.apply_discount() == 8500
     assert item2.apply_discount() == 17000
     assert item3.apply_discount() == 42500
