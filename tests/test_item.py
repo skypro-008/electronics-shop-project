1ab76e82bb67ad8cbd0dd577b0b8d703c8545172
Tatiana Makarova <tanymv2004@gmail.com>
2023-09-24 11:51:37 +0500

    ready_done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,13 +1,44 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-import unittest
+import pytest
 from src import item
-class TestItem(unittest.TestCase):
-    """Тест для класса Item"""
 
-    def test_calculate_total_price(self):
-        item1 = item.Item("Смартфон", 10000, 20)
-        self.assertEqual(item1.calculate_total_price(), 200000)
+
+def item_item():
+    return item.Item("Смартфон", 10000, 20)
+
+
+def test___init__():
+    assert item_item().__init__("Смартфон", 10000, 20) is None
+
+
+def test_calculate_total_price():
+    assert item_item().calculate_total_price() == 200000
+
+
+def test_apply_discount():
+    assert item_item().apply_discount() is None
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
+
+#
+# # пример класса для тестирования
+# class MyClass:
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+#
+#     def sum(self):
+#         return self.a + self.b
+#
+#
+# # создание fixture для создания экземпляра класса в каждом тесте
+# @pytest.fixture
+# def my_class():
+#     return MyClass(2, 3)
+#
+#
+# # тест функции sum класса MyClass
+# def test_sum(my_class):
+#     assert my_class.sum() == 5
