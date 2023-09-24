6a02b381d73866f0bef61de792aec0d1c1f56d6d
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2023-09-24 17:04:06 +0300

    Final

--- src/item.py
+++ src/item.py
@@ -13,7 +13,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +23,15 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        total_price = self.price*self.quantity
+        return total_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        new_price = float(self.price * self.pay_rate)
+        return new_price
+
+
+
