a507ddd05474d4f3431dda8035890fa8d54d966a
Tatiana Makarova <tanymv2004@gmail.com>
2023-09-26 13:36:49 +0500

    intermediate result

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,22 +1,29 @@
-from src.item import Item
+import csv
 
-if __name__ == '__main__':
-    item = Item('Телефон', 10000, 5)
-
-    # длина наименования товара меньше 10 символов
-    item.name = 'Смартфон'
-    assert item.name == 'Смартфон'
-
-    # длина наименования товара больше 10 символов
-    item.name = 'СуперСмартфон'
-    # Exception: Длина наименования товара превышает 10 символов.
-
-    Item.instantiate_from_csv('src/items.csv')  # создание объектов из данных файла
-    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
-
-    item1 = Item.all[0]
-    assert item1.name == 'Смартфон'
-
-    assert Item.string_to_number('5') == 5
-    assert Item.string_to_number('5.0') == 5
-    assert Item.string_to_number('5.5') == 5
+# from src.item import Item
+#
+# if __name__ == '__main__':
+#     item = Item('Телефон', 10000, 5)
+#
+#     # длина наименования товара меньше 10 символов
+#     item.name = 'Смартфон'
+#     assert item.name == 'Смартфон'
+#
+#     # длина наименования товара больше 10 символов
+#     item.name = 'СуперСмартфон'
+#     # Exception: Длина наименования товара превышает 10 символов.
+#
+#     Item.instantiate_from_csv('src/items.csv')  # создание объектов из данных файла
+#     assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+#
+#     item1 = Item.all[0]
+#     assert item1.name == 'Смартфон'
+#
+#     assert Item.string_to_number('5') == 5
+#     assert Item.string_to_number('5.0') == 5
+#     assert Item.string_to_number('5.5') == 5
+file = 'src/items.csv'
+with open(file, 'r', encoding='utf-8') as r_file:
+    file_reader = csv.reader(r_file, delimiter=",")
+    for row in file_reader:
+        print(row)
--- src/item.py
+++ src/item.py
@@ -1,34 +1,61 @@
-class Item:
-    """
-    Класс для представления товара в магазине.
-    """
-    pay_rate = 1.0
-    all = []
+import csv
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-
-    def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
-        """
-        total_cost = self.price * self.quantity
-        return total_cost
-
-    def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        self.price = self.price - self.price * self.pay_rate / 100
-        self.all.append(Item(self.name, self.price, self.quantity))
+#
+# class Item:
+#     """
+#     Класс для представления товара в магазине.
+#     """
+#     pay_rate = 1.0
+#     all = []
+#     file_name = 'src/items.csv'
+#     def __init__(self, name: str, price: float, quantity: int) -> None:
+#         """
+#         Создание экземпляра класса item.
+#
+#         :param name: Название товара.
+#         :param price: Цена за единицу товара.
+#         :param quantity: Количество товара в магазине.
+#         """
+#         self.__name = name
+#         self.price = price
+#         self.quantity = quantity
+#
+#     def calculate_total_price(self) -> float:
+#         """
+#         Рассчитывает общую стоимость конкретного товара в магазине.
+#
+#         :return: Общая стоимость товара.
+#         """
+#         total_cost = self.price * self.quantity
+#         return total_cost
+#
+#     def apply_discount(self) -> None:
+#         """
+#         Применяет установленную скидку для конкретного товара.
+#         """
+#         self.price = self.price - self.price * self.pay_rate / 100
+#         self.all.append(Item(self.__name, self.price, self.quantity))
+#
+#     @property
+#     def fullname(self):
+#         """Возвращает полное наименование товара"""
+#         return self.__name
+#
+#     @fullname.setter
+#     def fullname(self, __name):
+#         """Проверяет длину наименования товара"""
+#         if len(self.__name) < 10:
+#             print(self.__name)
+#         else:
+#             print(self.__name[:11])
+#
+#     @classmethod
+#     def instantiate_from_csv(cls, file_name):
+#         with open(file_name)as r_file:
+#             file_reader = csv.reader(r_file, delimiter=",")
+#             for row in file_reader:
+#                 return cls.all.append([row[0], int(row[1]), int(row[2])])
+#
+#     @staticmethod
+#     def string_to_number(number):
+#         return int(number)
