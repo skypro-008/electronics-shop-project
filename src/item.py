307c3a4396dd62146b0854cc178beb558b51a0e3
denis ukrajev <humanwithbeard@gmail.com>
2023-09-26 18:03:56 +0300

    main,py works

--- src/item.py
+++ src/item.py
@@ -15,6 +15,12 @@ class Item:
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
 
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
+        else:
+            raise ValueError("Ошибка")
+
     def __str__(self):
         return f"{self.__name}"
 
---
+++ src/phone.py
@@ -0,0 +1,22 @@
+from src.item import Item
+
+
+class Phone(Item):
+
+    def __init__(self, name, price, quantity, number_of_sim):
+        super().__init__(name, price, quantity)
+        self.number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
+        elif isinstance(other, Phone):
+            return self.quantity + other.quantity
+        else:
+            raise ValueError("Ошибка")
