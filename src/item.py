eba1dc29484c530594f47fcb33c0282e532ad409
VladimirKovaliev <kovaliev.v@list.ru>
2023-09-26 17:41:51 +0300

    add __add__

--- src/item.py
+++ src/item.py
@@ -24,6 +24,12 @@ class Item:
 
     def __str__(self):
         return f'{self.__name}'
+
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
+        else:
+            raise TypeError("Нельзя складывать разные типы товаров!")
     @property
     def name(self) -> str:
         return self.__name
