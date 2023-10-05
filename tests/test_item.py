bfb9e589e68432cd42ed76d9003d851013b88878
Artem <levkin_mizuno@mail.ru>
2023-10-05 08:34:19 +0700

    Homework 1.1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,7 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
 item_1 = Item("TV", 20000, 100)
+Item.pay_rate = 2.0
 
 def test_calculate_total_price():
     assert Item("TV", 20000, 100).calculate_total_price == 2000000
@@ -8,6 +9,5 @@ def test_calculate_total_price():
 
 
 def test_apply_discount():
-    assert item_1.apply_discount() == None
-
-
+    item_1.apply_discount()
+    assert 50000 * Item.pay_rate == float(100000)
