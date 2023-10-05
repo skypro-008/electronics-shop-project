04cd713dc828a7b98cad8872e9e5c0135cfee7cf
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-10-05 17:28:54 +0800

    added class Item to src.item.py and tests to tests.test_item

--- src/item.py
+++ src/item.py
@@ -1,30 +1,21 @@
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
-    pay_rate = 1.0
-    all = []
+    """Класс для общих данных по товару в магазине: название, цена, количество"""
+    pay_rate = 0.8
+    all_items = []
+    def __init__(self,name, price, amount):
+        self.name = name
+        self.price = price
+        self.amount = amount
+        self.all_items.append({"название": self.name, "цена": self.price, "количество": self.amount})
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
+    def __str__(self):
+        return f"товар {self.name} цена за единицу {self.price} количество {self.amount}"
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+    def __repr__(self):
+        return f'Item(\'{self.name}\', {self.price}, {self.amount})'
 
-    def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+    def calculate_total_price(self):
+        return self.price * self.amount
 
-        :return: Общая стоимость товара.
-        """
-        pass
-
-    def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+    def apply_discount(self):
+        return self.price * self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def test_item():
+    return Item("Watches", 10000, 5)
+
+def test_calculate_total_price():
+    assert test_item.calulate_total_price() == 50000
+
+def test_apply_discount():
+    assert test_item.apply_discount() == 8000
