2b039f27cd6cd3304d4d5166954252530b979e70
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-10-07 23:20:20 +0800

    fixed items

--- src/item.py
+++ src/item.py
@@ -1,12 +1,13 @@
 class Item:
     """Класс для общих данных по товару в магазине: название, цена, количество"""
     pay_rate = 0.8
-    all_items = []
+    all = []
     def __init__(self,name, price, amount):
         self.name = name
         self.price = price
         self.amount = amount
-        self.all_items.append({"название": self.name, "цена": self.price, "количество": self.amount})
+        self.all.append(self)
+        #self.all.append({"название": self.name, "цена": self.price, "количество": self.amount})
 
     def __str__(self):
         return f"товар {self.name}, цена за единицу {self.price}, количество {self.amount}"
@@ -18,4 +19,4 @@ class Item:
         return self.price * self.amount
 
     def apply_discount(self):
-        return self.price * self.pay_rate
+        self.price *= self.pay_rate
