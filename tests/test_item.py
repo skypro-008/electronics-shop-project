fd2e07aa76799d9c26972577a0a6079668ced6b7
SerjMonstrX <SerjMonstrX@gmail.com>
2023-10-15 22:37:40 +0700

    Added tests for calculate_total_price and apply_discount class methods

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+def test_apply_discount():
+    item = Item("Смартфон", 10000, 1)
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8000
+
+if __name__ == '__main__':
+    pytest.main()
