615bc22d090904cceaf1029e5c386c6ad3e4cfe3
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2023-10-17 10:03:16 +0300

    Домашнее задание 1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import os.path
+from csv import DictReader
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +17,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +29,61 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.quantity * self.price
 
-    def apply_discount(self) -> None:
+    def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+        return self.price
+
+    @classmethod
+    def verify_name(cls, name):
+        """
+        Имя должно быть не больше 10 символов
+        """
+        return name[:10]
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        name = self.verify_name(name)
+        self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, path_name):
+        """
+        Создаёт объекты из данных файла .csv
+        """
+
+        items = []
+        path_name = str(cls.path_file(path_name))
+        with open(path_name, newline='', encoding='windows-1251') as csv_f:
+            reader = DictReader(csv_f, delimiter=',')
+            for row in reader:
+                name = str(row['name'])
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                item = cls(name, price, quantity)
+                items.append(item)
+            cls.all = items
+
+    @staticmethod
+    def path_file(path_name):
+        """
+        Создаёт путь для файла при условии, что файл лежит в другой папке родительского каталога
+
+        :param path_name: путь к файлу в подобном формате 'src/items.csv'
+        """
+        path_list = path_name.split('/')
+        path_file = os.path.join('..', path_list[0], path_list[1])
+        return path_file
+
+    @staticmethod
+    def string_to_number(string):
+        string = string.split('.')
+        return int(string[0])
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,32 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def test_class():
+    item = Item(name='test', price=5, quantity=10,)
+    return item
+
+
+def test_calculate_total_price(test_class):
+    assert test_class.calculate_total_price() == 50
+
+
+def test_apply_discount(test_class):
+    assert test_class.apply_discount() == 5
+
+
+def test_name_setter(test_class):
+    test_class.name = 'qwertyuiopasd'
+    assert test_class.name == 'qwertyuiop'
+
+
+def test_instantiate_from_csv(test_class):
+    test_class.instantiate_from_csv('src/items.csv')
+    assert len(test_class.all) == 5
+
+
+def test_string_to_number(test_class):
+    assert test_class.string_to_number('15.0') == 15
