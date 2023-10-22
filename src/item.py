7b967f02b30a595ba94db5c6cbc586343220d752
SerjMonstrX <SerjMonstrX@gmail.com>
2023-10-22 14:05:02 +0700

    completed HW#3 added __repr__ and __str__ method

--- src/item.py
+++ src/item.py
@@ -21,6 +21,12 @@ class Item:
         self.quantity = quantity
         Item.all.append(self)
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f"{self.name}"
+
     @property
     def name(self):
         return self.__name
