e4d882d0bf242210950d1642cd3dfa58be56ab93
petrova_juliana <juliana6k6@gmail.com>
2023-10-27 00:56:58 +0300

    HW1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+item1 = Item("Смартфон", 1000, 20)
+item2 = Item("Планшет", 10000, 2)
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 20000
+    assert item2.calculate_total_price() == 20000
+
+
+def test_apply_discount():
+    Item.pay_rate = 0.5
+    item1.apply_discount()
+    assert item1.price == 500
+    item2.apply_discount()
+    assert item2.price == 5000
+
