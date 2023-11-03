561ce42f845c03f1673448f61a0c88f6f2518397
Juliana <juliana6k@mail.ru>
2023-11-03 16:15:00 +0300

    HW2.4

--- tests/test_item.py
+++ tests/test_item.py
@@ -19,3 +19,26 @@ def test_apply_discount():
     item2.apply_discount()
     assert item2.price == 5000
 
+item = Item('Телефон', 10000, 5)
+
+def test_person_name():
+    # длина наименования товара меньше 10 символов
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+    # длина наименования товара больше 10 символов
+    item.name = 'СуперСмартфон'
+    assert item.name == "Суперсмарт"
+    # Exception: Длина наименования товара превышает 10 символов.
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')  # создание объектов из данных файла
+    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
