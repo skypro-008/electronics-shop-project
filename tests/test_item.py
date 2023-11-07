d5f203a72a3111d9309d9f73dbf039f21f25dbde
Evgeniya <osipova.dr17@yandex.ru>
2023-11-07 21:17:09 +1100

    make test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item_test():
+    return Item("Телевизор", 150000, 5)
+
+
+def calculate_total_price():
+    assert item_test.calculate_total_price() == 750000
+
+
+def apply_discount():
+    assert item_test.apply_discount() == 150000
