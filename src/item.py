87a10eb65d6103b1cc3ac7fd5db8d53cf3070c88
Arina Astanina <timyr.kiyan@gmail.com>
2023-11-13 22:56:45 +0300

    Выполнено задание 1

--- src/item.py
+++ src/item.py
@@ -5,6 +5,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
+
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -13,7 +14,12 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        self.all.append(self)
+
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +27,12 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
+
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
+
