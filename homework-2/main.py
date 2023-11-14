171a281286233ecc54655047479e35aba0d4f141
__Vitaliy__ <akkauntmoy124@gmail.com>
2023-11-14 17:27:50 +0300

    File adapted to specifications

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,21 +1,23 @@
 from src.item import Item
 
+# Файл 'main.py' подкорректирован под выполнение ТЗ из файла 'README'.
+
 if __name__ == '__main__':
     item = Item('Телефон', 10000, 5)
 
     # длина наименования товара меньше 10 символов
-    item.name = 'Смартфон'
-    assert item.name == 'Смартфон'
+    item.get_name = 'Смартфон'
+    assert item.get_name == 'Смартфон'
 
     # длина наименования товара больше 10 символов
-    item.name = 'СуперСмартфон'
-    # Exception: Длина наименования товара превышает 10 символов.
+    item.get_name = 'СуперСмартфон'
+    # В атрибут __name записывается первые 10 символов названия.
 
-    Item.instantiate_from_csv('src/items.csv')  # создание объектов из данных файла
-    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+    Item.instantiate_from_csv('../src/items.csv')  # создание объектов из данных файла
+    assert len(Item.all) == 6  # в файле 1 + 5 записей с данными по товарам
 
     item1 = Item.all[0]
-    assert item1.name == 'Смартфон'
+    assert item1.get_name == 'СуперСмарт'
 
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
