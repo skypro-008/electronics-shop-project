1a6c94d98c63a26ee0828e92db65a996f14e605f
Vladislav Vasilev <akiro_jpg@vk.com>
2023-11-21 04:35:02 +0500

    Created item class for hw1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,30 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+item_1 = Item("Смартфон", 10000, 5)
+item_2 = Item("Ноутбук", 25000, 3)
+item_1.pay_rate = 0.8
+item_2.pay_rate = 0.95
+
+
+def test_valid_item():
+    assert item_1.name == "Смартфон"
+    assert item_1.price == 10000
+    assert item_1.quantity == 5
+
+    assert item_2.name == "Ноутбук"
+    assert item_2.price == 25000
+    assert item_2.quantity == 3
+
+
+def test_calculate_total_price():
+    assert item_1.calculate_total_price() == 50000
+    assert item_2.calculate_total_price() == 75000
+
+
+def test_apply_discount():
+    item_1.apply_discount()
+    assert item_1.price == 8000
+    item_2.apply_discount()
+    assert item_2.price == 23750
