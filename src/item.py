5aab345def6f66f0d2aada4515f4a279fff4575e
Alina Mordovina <alina182@yandex.ru>
2023-11-21 21:14:17 +0400

    homework_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+from csv import DictReader
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,12 +16,20 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
-        if self not in Item.all:
-            Item.all.append(self)
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
 
     def calculate_total_price(self) -> float:
         """
@@ -35,3 +46,24 @@ class Item:
         """
 
         self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, file_items):
+        cls.all = []
+
+        with open(file_items, encoding='cp1251') as csv_file:
+            file = DictReader(csv_file)
+
+            for element in file:
+                name = element['name']
+                price = float(element['price'])
+                quantity = int(element['quantity'])
+                item = cls(name, price, quantity)
+                item.name = name
+                item.price = price
+                item.quantity = quantity
+                cls.all.append(item)
+
+    @staticmethod
+    def string_to_number(str_num):
+        return int(float(str_num))
--- tests/test_item.py
+++ tests/test_item.py
@@ -28,3 +28,13 @@ def test_apply_discount(item):
     item.pay_rate = 0.7
     item.apply_discount()
     assert item.price == 14000
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('items_test.csv')
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number("5.5") == 5
