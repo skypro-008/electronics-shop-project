9e6c1294795e8c8af681b216e1b48bcfc8cd1c2d
Khitrov <Hitrov.95@yandex.ru>
2023-11-24 23:46:46 +0300

    денис_хитров_4_1_1_дз

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def tv():
+    return Item("tv", 10000, 2)
+
+
+def test_calculate_total_price(tv):
+    tv.apply_discount()
+    assert tv.calculate_total_price() == 20000.0
+
+
+def test_apply_discount(tv):
+    assert tv.apply_discount() is None
