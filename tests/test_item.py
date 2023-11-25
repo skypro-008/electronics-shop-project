e26a4ffc04c23a61d684d390b022b44629f86280
Vladislav Vasilev <akiro_jpg@vk.com>
2023-11-26 03:00:43 +0500

    fixed review issues

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,29 +2,33 @@ import pytest
 
 from src.item import Item
 
-item_1 = Item("Смартфон", 10000, 5)
-item_2 = Item("Ноутбук", 25000, 3)
-item_1.pay_rate = 0.8
-item_2.pay_rate = 0.95
+
+@pytest.fixture
+def test_data():
+    item_1 = Item("Смартфон", 10000, 5)
+    item_2 = Item("Ноутбук", 25000, 3)
+    item_1.pay_rate = 0.8
+    item_2.pay_rate = 0.95
+    return item_1, item_2
 
 
-def test_valid_item():
-    assert item_1.name == "Смартфон"
-    assert item_1.price == 10000
-    assert item_1.quantity == 5
+def test_valid_item(test_data):
+    assert test_data[0].name == "Смартфон"
+    assert test_data[0].price == 10000
+    assert test_data[0].quantity == 5
 
-    assert item_2.name == "Ноутбук"
-    assert item_2.price == 25000
-    assert item_2.quantity == 3
-
-
-def test_calculate_total_price():
-    assert item_1.calculate_total_price() == 50000
-    assert item_2.calculate_total_price() == 75000
+    assert test_data[1].name == "Ноутбук"
+    assert test_data[1].price == 25000
+    assert test_data[1].quantity == 3
 
 
-def test_apply_discount():
-    item_1.apply_discount()
-    assert item_1.price == 8000
-    item_2.apply_discount()
-    assert item_2.price == 23750
+def test_calculate_total_price(test_data):
+    assert test_data[0].calculate_total_price() == 50000
+    assert test_data[1].calculate_total_price() == 75000
+
+
+def test_apply_discount(test_data):
+    test_data[0].apply_discount()
+    assert test_data[0].price == 8000
+    test_data[1].apply_discount()
+    assert test_data[1].price == 23750
