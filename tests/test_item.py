03f7fac25e532a27e79dbf95d683b964cf986606
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2023-11-25 21:09:42 +0500

    homework_1 electronics_shop

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def first_item():
+    return Item("Лампа", 100, 10)
+
+
+def test_apply_discount(first_item):
+    first_item.apply_discount()
+    assert first_item.price == 100
+
+
+def test_calculate_total_price(first_item):
+    assert first_item.calculate_total_price() == 1000
