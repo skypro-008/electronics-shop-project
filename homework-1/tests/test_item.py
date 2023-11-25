0348b845783988c4061325cf6fee2d770a2c03d4
Roman Makhnev <romanmahnef777@gmail.com>
2023-11-25 11:56:06 +0400

    1

--- homework-1/tests/test_item.py
+++ homework-1/tests/test_item.py
@@ -1,6 +1,7 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
 
+
 def test_calculate_total_price():
     item = Item("Test", 10, 5)
     assert item.calculate_total_price() == 50
