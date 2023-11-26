b0f78045c3ed63f30288c99bd9f73cb58123326b
Alexei Kasaev <kasaev_97@mail.ru>
2023-11-26 23:01:06 +0300

    add test for item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == item.price * item.quantity
+
+
+def test_apply_discount(item):
+    item.apply_discount()
+    assert item.price == 0.0
+
+
+def test_all(item):
+    assert item in Item.all
