a6a2be115279b91d9bb5f96274b3adab86a78192
Roman Makhnev <romanmahnef777@gmail.com>
2023-11-26 11:55:51 +0400

    E-shop second task

--- src/item.py
+++ src/item.py
@@ -31,3 +31,28 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            self.__name = value[:10]
+        else:
+            self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        items = []
+        with open('src/items.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                item = cls(row['name'], row['price'])
+                items.append(item)
+        return items
+
+    @staticmethod
+    def string_to_number(string):
+        return int(string)
--- tests/test_item.py
+++ tests/test_item.py
@@ -10,3 +10,11 @@ def test_apply_discount():
     Item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 80
+
+def test_instantiate_from_csv():
+    items = Item.instantiate_from_csv()
+    assert len(items) == 3
+
+def test_string_to_number():
+    result = Item.string_to_number("100")
+    assert result == 100
