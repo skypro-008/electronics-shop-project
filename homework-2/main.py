40425bdc794f22182e3359de4742d1786a57ecba
Nikolay <ya6998@mail.ru>
2023-11-27 21:06:18 +0300

    DZ_13.2

--- homework-2/main.py
+++ homework-2/main.py
@@ -10,12 +10,13 @@ if __name__ == '__main__':
     # длина наименования товара больше 10 символов
     item.name = 'СуперСмартфон'
     # Exception: Длина наименования товара превышает 10 символов.
+
     Item.instantiate_from_csv()  # создание объектов из данных файла
     assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
 
     item1 = Item.all[0]
     assert item1.name == 'Смартфон'
 
-    assert Item.string_to_number() == 5
-    assert Item.string_to_number() == 5
-    assert Item.string_to_number() == 5
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
