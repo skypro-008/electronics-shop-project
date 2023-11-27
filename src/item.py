d7f1f50a1eb463a2d7bb2e1235b7edc2fe20217e
Khitrov <Hitrov.95@yandex.ru>
2023-11-27 21:50:12 +0300

    денис_хитров_4_1_2_дз

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +17,33 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name[0:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, file):
+        cls.all = []
+        with open(os.path.join('..', file), newline='', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(data):
+        return int(float(data))
 
     def calculate_total_price(self) -> float:
         """
@@ -31,4 +58,3 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= Item.pay_rate
-
--- tests/test_item.py
+++ tests/test_item.py
@@ -8,6 +8,17 @@ def tv():
     return Item("tv", 10000, 2)
 
 
+@pytest.fixture
+def ctv():
+    return Item("colortelevision", 15000, 3)
+
+
+def test_item_initialization(tv):
+    assert tv.name == "tv"
+    assert tv.price == 10000
+    assert tv.quantity == 2
+
+
 def test_calculate_total_price(tv):
     tv.apply_discount()
     assert tv.calculate_total_price() == 20000.0
@@ -15,3 +26,28 @@ def test_calculate_total_price(tv):
 
 def test_apply_discount(tv):
     assert tv.apply_discount() is None
+
+
+def test_name_property(ctv):
+    assert ctv.name == "colortelevision"
+
+
+def test_name_setter(ctv):
+    ctv.name = ctv.name[0:10]
+    assert ctv.name == "colortelev"
+
+
+def test_name_property_1(tv):
+    assert tv.name == "tv"
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) == 5
+    assert Item.all[0].name == "Смартфон"
+    assert Item.all[1].price == 1000
+    assert Item.all[4].quantity == 5
+
+
+def test_string_to_number_static():
+    assert Item.string_to_number("5.76") == 5
