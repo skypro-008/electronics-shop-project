7757ee67603c07b720c73dc3a34963fc27490187
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2023-11-28 21:17:13 +0500

    homework_2 electronics_shop Доработка

--- tests/test_item.py
+++ tests/test_item.py
@@ -17,3 +17,15 @@ def test_apply_discount(first_item):
 
 def test_calculate_total_price(first_item):
     assert first_item.calculate_total_price() == 1000
+
+
+def test_name_setter():
+    item = Item("TVset", 50000, 1)
+    assert item.name == "TVset"
+    assert len(item.name) == 5
+
+
+def test_instantiate_from_csv():
+    csv_data = "Laptop,1500,2\nKabel,15,5\n"
+    with open("test_item.csv", "w", encoding="utf-8") as csv_file:
+        csv_file.write(csv_data)
