24a14d9bc557bdb004474b9f77ee924af9ce25d6
Alexei Kasaev <kasaev_97@mail.ru>
2023-11-29 20:32:10 +0400

    add new func and tests

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import math
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -18,6 +22,19 @@ class Item:
         self.quantity = quantity
         self.all.append(self)
 
+    @property
+    def name(self) -> str:
+        """ Возвращает наименование товара. """
+        return self.__name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        """ Устанавливает наименование товара. """
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -31,3 +48,20 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price - self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, filename):
+        """инициализирует экземпляры класса Item данными из файла src/items.csv"""
+        with open(filename, 'r') as file:
+            reader = csv.DictReader(file, delimiter=',')
+            for line in reader:
+                name = line["name"]
+                price = Item.string_to_number(line['price'])
+                quantity = int(line['quantity'])
+                item = cls(name, price, quantity)
+                Item.all.append(item)
+
+    @staticmethod
+    def string_to_number(string: str) -> float:
+        """Преобразование строки в число"""
+        return int(math.floor(float((string.replace(',', '.')))))
--- tests/test_item.py
+++ tests/test_item.py
@@ -8,13 +8,36 @@ def item():
 
 
 def test_calculate_total_price(item):
-    assert item.calculate_total_price() == item.price * item.quantity
+    assert item.calculate_total_price() == 200000
 
 
 def test_apply_discount(item):
-    item.apply_discount()
-    assert item.price == 0.0
+    if Item.pay_rate == 0.8:
+        item.price = item.price * item.pay_rate
+        assert item.price == 8000.0
 
 
-def test_all(item):
-    assert item in Item.all
+@pytest.fixture
+def item_notebook():
+    return Item("Ноутбук", 20000, 5)
+
+
+def test_calculate_total_price_notebook(item_notebook):
+    assert item_notebook.calculate_total_price() == 100000
+
+
+def test_apply_discount_notebook(item_notebook):
+    if Item.pay_rate == 0.8:
+        item_notebook.price = item_notebook.price * item_notebook.pay_rate
+        assert item_notebook.price == 20000
+
+
+def test_name_setter():
+    item = Item('Компьютер', 100, 2)
+    assert item.name == 'Компьютер'
+
+
+def test_instantiate_from_csv():
+    file_csv = "Смартфон,100,1\nНоутбук, 1000, 3\n"
+    with open("test_items.csv", "w", encoding="utf-8") as file:
+        file.write(file_csv)
