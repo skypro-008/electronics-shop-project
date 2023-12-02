039588f2bcf7328b393fc5a02edef1dfab408749
Vladislav Vasilev <akiro_jpg@vk.com>
2023-12-03 03:03:23 +0500

    writed homework tasks

--- src/item.py
+++ src/item.py
@@ -1,11 +1,17 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
+
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(
+        self, name: str = None, price: float = None, quantity: int = None
+    ) -> None:
         """
         Создание экземпляра класса item.
 
@@ -13,10 +19,33 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        """
+        Возвращает значение свойства name.
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        Устанавливает значение атрибута name.
+
+        :params:
+            name (str): Новое значение для атрибута name.
+
+        :return:
+            None
+        """
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +60,20 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, path: str):
+        with open(path, "r") as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get("name"),
+                price=float(item.get("price")),
+                quantity=int(item.get("quantity")),
+            )
+
+    @staticmethod
+    def string_to_number(text: str) -> int:
+        return int(float(text))
