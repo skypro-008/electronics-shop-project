b9ef94b07418aedb208815d0ec41fde9a30e375d
gremv <gremvanek@gmail.com>
2023-12-04 22:33:51 +0500

    ДЗ 1. 1 задание

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+# Создаем тестовый экземпляр товара для использования в тестах
+@pytest.fixture
+def test_item():
+    return Item("Test Item", 10.0, 5)
+
+
+def test_calculate_total_price(test_item):
+    assert test_item.calculate_total_price() == 50.0
+
+
+def test_apply_discount(test_item):
+    test_item.apply_discount()
+    assert test_item.price == 10.0 * Item.pay_rate
