0c0caa3a747de9cdab675b7ef4187bd612858c66
IvanPyrlik <ivan.pyrlik59@gmail.com>
2023-12-05 21:12:50 +0500

    fix test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,27 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item3():
+    return Item("Телевизор", 15000, 5)
+
+
+@pytest.fixture
+def item4():
+    return Item("Холодильник", 30000, 3)
+
+
+def test_calculate_total_price(item3, item4):
+    assert item3.calculate_total_price() == 75000
+    assert item4.calculate_total_price() == 90000
+
+
+def test_apply_discount(item3, item4):
+    Item.pay_rate = 0.5
+    item3.apply_discount()
+    item4.apply_discount()
+    assert item3.price == 7500
+    assert item4.price == 15000
