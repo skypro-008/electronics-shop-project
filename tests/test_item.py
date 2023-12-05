142d89c0ce6660e8d2ac40052566a39c0c7f4fbb
Alexey Elyakin <ieliakin98@mail.ru>
2023-12-05 21:32:03 +0300

    Поправил pep8

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,8 +2,10 @@
 import pytest
 from src.item import Item
 
+
 def test_calculate_total_price():
     assert Item("Смартфон", 10000, 20).calculate_total_price() == 200000
+
 
 def test_apply_discount():
     Item("Смартфон", 10000, 20).apply_discount()
