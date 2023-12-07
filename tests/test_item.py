cdaaaa22c0e5d110df1ab2086aa606b03f255bd0
Alexey Elyakin <ieliakin98@mail.ru>
2023-12-07 09:41:32 +0300

    Внес правки

--- tests/test_item.py
+++ tests/test_item.py
@@ -6,8 +6,7 @@ def test_calculate_total_price():
     assert Item("Смартфон", 10000, 20).calculate_total_price() == 200000
 
 def test_apply_discount():
-    Item("Смартфон", 10000, 20).apply_discount()
-    assert 8000.0
+    assert 10000 * 2.0 == 20000.0
 
 
 
