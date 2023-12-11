0cdc3bf3e98a03874dd01c725cb9c81745a4bae2
gremv <gremvanek@gmail.com>
2023-12-11 23:35:59 +0500

    ДЗ 3. 1 задание

--- src/item.py
+++ src/item.py
@@ -19,6 +19,12 @@ class Item:
 
         Item.all.append(self)
 
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.name
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
--- tests/test_item.py
+++ tests/test_item.py
@@ -16,3 +16,22 @@ def test_calculate_total_price(test_item):
 def test_apply_discount(test_item):
     test_item.apply_discount()
     assert test_item.price == 10.0 * Item.pay_rate
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('./src/items.csv')
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+def test_str_and_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    assert str(item1) == 'Смартфон'
