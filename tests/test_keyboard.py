f9b08526e41cc3893eed3d41f718b758b6e435dc
__Vitaliy__ <akkauntmoy124@gmail.com>
2023-12-14 21:03:40 +0300

    Add test for 'Keyboard' class.

---
+++ tests/test_keyboard.py
@@ -0,0 +1,15 @@
+from src.keyboard import Keyboard
+
+
+def test_class():
+    kb = Keyboard("Dark Project KD87A", 9600, 5)
+    assert str(kb) == "Dark Project KD87A"
+
+    assert str(kb.language) == "EN"
+
+    kb.change_lang()
+    assert str(kb.language) == "RU"
+
+    # Сделали EN -> RU -> EN
+    kb.change_lang()
+    assert str(kb.language) == "EN"
