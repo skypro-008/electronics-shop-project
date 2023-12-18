2321dfcd52899f3a7a25c2ef577ddc81a4f5a5b3
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2023-12-18 23:13:20 +0300

    Написала тесты к модулю item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+@pytest.fixture
+def item1():
+    item1 = Item("Часы", 16000, 30)
+    return item1
+
+def test_init(item1):
+    assert item1.price == 16000
+    assert item1.quantity == 30
+
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 480000
+
+
