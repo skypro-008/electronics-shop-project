bc626dab6813d2a266915895fa486233ce79d2b4
angelinakhalueva <sprite.spirit.t.me@gmail.com>
2024-01-03 16:41:36 +0300

    Заполнила поля класса (атрибуты): name, price, quantity и автоматическое пополнение списка all товаром, при создании экземпляра класса Item.

--- src/item.py
+++ src/item.py
@@ -13,7 +13,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
