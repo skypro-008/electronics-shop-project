1fe28e5415852baed845eb6d46d57fbef1cc3dbd
alinadolbilova <genin.10@mail.ru>
2024-01-05 17:53:26 +0300

    add_test_function_hw2

--- homework-2/tests/test_item.py
+++ homework-2/tests/test_item.py
@@ -7,7 +7,7 @@ def test_calculate_total_price():
     item1 = Item("Смартфон", 10000, 20)
     item2 = Item("Ноутбук", 20000, 5)
     assert item1.calculate_total_price() == 200000
-    assert item2.calculate_total_price() == 100000
+    assert item2.calculate_total_price() == 10000
 
 
 def test_apply_discount():
