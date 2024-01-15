1c2ac15cd5a44a464da0ed9c11affecf5032ee09
xrtnv <xrtnv@xrtnv.com>
2024-01-15 18:45:35 +0300

    hw1 added item class and made tests

--- src/item.py
+++ src/item.py
@@ -13,18 +13,38 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        if not name:
+            raise ValueError("Name cannot be empty.")
+        if not isinstance(quantity, int) or quantity < 0:
+            raise ValueError("Quantity must be a non-negative integer.")
+        if price <= 0:
+            raise ValueError("Price must be a positive number.")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
-        :return: Общая стоимость товара.
+        :return: Общая стоимость товара, округленная до 2 десятичных знаков.
         """
-        pass
 
-    def apply_discount(self) -> None:
+        total_price = self.price * self.quantity
+        return round(total_price, 2)
+
+    def apply_discount(self):
         """
-        Применяет установленную скидку для конкретного товара.
+        Применяет установленную скидку для конкретного товара и возвращает новую цену после применения скидки.
+
+        :param discount: Скидка в процентах.
         """
-        pass
+
+        if not isinstance(self.pay_rate, (float, int)):
+            raise ValueError("Pay rate must be a number.")
+
+        if not 0 <= self.pay_rate <= 1:
+            raise ValueError("Pay rate must be a valid percentage between 0 and 1.")
+
+        self.price = round(self.price * self.pay_rate, 2)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,38 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+def test_valid_parameters_no_errors():
+    item = Item("Item 1", 100, 1)
+    assert item.name == "Item 1"
+    assert item.price == 100
+    assert item.quantity == 1
+    assert item in Item.all
+
+
+def test_calculate_total_price():
+    item = Item("Item 2", 50, 2)
+    total_price = item.calculate_total_price()
+    assert total_price == 100
+
+
+def test_apply_discount_valid_discount():
+    item = Item("Item 3", 100, 1)
+    item.pay_rate = 0.1
+    item.apply_discount()
+    assert item.price == 10
+
+
+def test_empty_name_raises_value_error():
+    with pytest.raises(ValueError):
+        item = Item("", 100, 1)
+
+
+def test_negative_quantity_raises_value_error():
+    with pytest.raises(ValueError):
+        item = Item("Item 4", 100, -1)
+
+
+def test_zero_or_negative_price_raises_value_error():
+    with pytest.raises(ValueError):
+        item = Item("Item 5", -100, 1)
