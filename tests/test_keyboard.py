e3c7c48ef9455e207f2380c8dc67f97dd9bb82df
aleksey milyaev <trane94.milyaev@gmail.com>
2024-01-18 18:53:21 +0700

    "Homework-5_fix"

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -4,12 +4,22 @@ from src.keyboard import Keyboard
 kb = Keyboard('Dark Project KD87A', 9600, 5)
 
 def test_keyboard_init():
+    """
+    Тест создания экземпляра класса Keyboard.
+    :param name: Название товара.
+    :param price: Цена за единицу товара.
+    :param quantity: Количество товара в магазине.
+    :param language: Раскладка клавиатуры
+    """
     assert kb.name == 'Dark Project KD87A'
     assert kb.price == 9600
     assert kb.quantity == 5
     assert kb.language == "EN"
 
 def test_change_lang():
+    """
+    Тест смены раскладки клавиатуры.
+    """
     kb.change_lang()
     assert kb.language == "RU"
     kb.change_lang()
