936edf19e4b16e2475cac78ed3e98cf6e1160161
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-01-22 00:59:15 +0300

    HW4: phone.py: - реализованы getter и setter
    test_phone.py
    - реализованы 2 теста для геттера и сеттера на валидные  и невалидные данные
    - покрытие 100%

--- src/phone.py
+++ src/phone.py
@@ -5,10 +5,23 @@ class Phone(Item):
 
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
     def __str__(self):
         return f'{self.name}'
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if isinstance(value, int) and 0 < value < 3:
+            self.__number_of_sim = value
+        else:
+            raise ValueError
+
+
--- tests/test_phone.py
+++ tests/test_phone.py
@@ -7,6 +7,7 @@ import pytest
     ('Xiaomi', 40_000, 8, 2, "Phone('Xiaomi', 40000, 8, 2)"),
 ])
 def test_repr(phone, name, price, quantity, number_of_sim, expected_result):
+
     phone.name = name
     phone.price = price
     phone.quantity = quantity
@@ -21,6 +22,26 @@ def test_repr(phone, name, price, quantity, number_of_sim, expected_result):
     ('LG', 'LG'),
 ])
 def test_str(phone, name, expected_result):
-    phone.name = name
 
+    phone.name = name
     assert str(phone) == expected_result
+
+
+@pytest.mark.parametrize('number_of_sim, expected_result', [
+    (1, 1),
+    (2, 2),
+])
+def test_number_of_sim_valid(phone, number_of_sim, expected_result):
+
+    phone.number_of_sim = number_of_sim
+    assert phone.number_of_sim == expected_result
+
+
+@pytest.mark.parametrize('number_of_sim', [
+    (0, ),
+    (3, ),
+])
+def test_number_of_sim_invalid(phone, number_of_sim):
+
+    with pytest.raises(ValueError):
+        phone.number_of_sim = number_of_sim
