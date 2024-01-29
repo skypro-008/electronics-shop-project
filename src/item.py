497296eca6689c0a19643e2a86578dd9b8e3eae6
mariaonderoz <mskorneeva.87@gmail.com>
2024-01-29 20:33:26 +0700

    Class implemented

--- src/item.py
+++ src/item.py
@@ -2,10 +2,16 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
+
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
+
         """
         Создание экземпляра класса item.
 
@@ -21,10 +27,12 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        total_price = self.price * self.quantity
+        return total_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+
