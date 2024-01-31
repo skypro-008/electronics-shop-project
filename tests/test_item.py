a3bb11672960d15ad40752a5a31e3184b2aa53a6
FAIRU <skyfairrr@gmail.com>
2024-01-31 20:40:09 +0500

    fix item.py and test_item (finished)

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def example():
+    return(Item("Смартфон", 10000, 20))
+
+
+def test_calculate_total_price(example):
+    assert example.calculate_total_price() == 200000
+
+
+def test_apply_discount(example):
+    example.pay_rate = 0.5
+    example.apply_discount()
+    assert example.price == 5000.0
+
+
+
