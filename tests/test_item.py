c0d6f8522090510a14fdd47fb9328229bd49880a
alinadolbilova <genin.10@mail.ru>
2024-01-31 13:44:35 +0300

    add_test_file

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,44 +2,35 @@
 from src.item import Item
 from src.phone import Phone
 import pytest
-from config import OPERATIONS_PATH
-
-
+import csv
+from src.instantiatecsv import InstantiateCSVError
+from config import OPERATIONS_PATH, TEST_PATH, DAMAGE_PATH
 def test_calculate_total_price():
     item1 = Item("Смартфон", 10000, 20)
     item2 = Item("Ноутбук", 20000, 5)
     assert item1.calculate_total_price() == 200000
     assert item2.calculate_total_price() == 100000
-
-
 def test_apply_discount():
     item1 = Item("Смартфон", 10000, 20)
     Item.pay_rate = 0.8
     assert item1.price * Item.pay_rate == 8000.0
-
-
 def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
-
-
 def test_all():
     Item.instantiate_from_csv(OPERATIONS_PATH)
     assert len(Item.all) == 5
     item1 = Item.all[0]
     assert item1.name == 'Смартфон'
 
-
 def test_repr():
     item1 = Item("Смартфон", 10000, 20)
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
 
-
 def test_str():
     item1 = Item("Смартфон", 10000, 20)
     assert str(item1) == 'Смартфон'
-
 
 def test_add():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
@@ -47,16 +38,24 @@ def test_add():
     assert item1 + phone1 == 25
     assert phone1 + phone1 == 10
 
-
 class Ph:
-    def __init__(self, name: str, price: float, quantity: int):
+    def __init__(self, name:str, price:float, quantity:int):
         self.quantity = quantity
         self.price = price
         self.name = name
 
-
-def another_class():
+def test_value_error():
     ph = Ph("iPhone 14", 120_000, 5)
-    item1 = Item('Смартфон', 10000, 20)
+    item1 = Item("Смартфон", 10000, 20)
     with pytest.raises(TypeError, match='Нельзя складывать с другими классами, кроме Phone'):
         item1 + ph
+
+def test_file_not_found():
+    file_name = TEST_PATH
+    with pytest.raises(FileNotFoundError, match='_Отсутствует файл item.csv_'):
+        Item.instantiate_from_csv(file_name)
+
+
+def test_file_damaged():
+    with pytest.raises(InstantiateCSVError, match='_Файл item.csv поврежден_'):
+        Item.instantiate_from_csv(DAMAGE_PATH)
