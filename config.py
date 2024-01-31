86f8ac743f89f9d0b62d0630c2ad91648177413c
alinadolbilova <genin.10@mail.ru>
2024-01-31 13:45:47 +0300

    add_damage and test

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 ROOT_DIR = os.path.dirname(__file__)
 OPERATIONS_PATH = os.path.join(ROOT_DIR, "src", "items.csv")
+TEST_PATH = os.path.join(ROOT_DIR, "src", "test.csv")
+DAMAGE_PATH = os.path.join(ROOT_DIR, "src", "damage.csv")
