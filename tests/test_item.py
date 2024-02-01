eb6f48938ba2d20c70501b97ba22d07ae9f6da09
Alyona Starkova <alyona.starrkova@yandex.ru>
2024-02-01 20:51:32 +0500

    add class Item and tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+@pytest.fixture()
+def item_example():
+    return Item("Наушники", 5000, 10)
+
+def test_item_calculate_total_price(item_example):
+    assert item_example.calculate_total_price() == 50000
+
+def test_item_apply_discount(item_example):
+    assert item_example.apply_discount() == 5000
+
+
+
+
+
+
+
