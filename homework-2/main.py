395636dcfd82b9f16195decce05c66801edf82b1
FAIRU <skyfairrr@gmail.com>
2024-02-04 01:06:01 +0500

    finished - Work1

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,4 +1,5 @@
 from src.item import Item
+from config import root_path
 
 if __name__ == '__main__':
     item = Item('Телефон', 10000, 5)
--- tests/test_item.py
+++ tests/test_item.py
@@ -9,6 +9,8 @@ def test_init(example):
     assert example.name == "Смартфон"
     assert example.price == 10000
     assert example.quantity == 20
+    example.name = 'СуперСмартфон'
+    assert example.name == 'СуперСмарт'
 
 def test_calculate_total_price(example):
     assert example.calculate_total_price() == 200000
@@ -18,6 +20,17 @@ def test_apply_discount(example):
     example.pay_rate = 0.5
     example.apply_discount()
     assert example.price == 5000.0
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
 
 
 
