b4393b2462290647d7f44aac877c94a721d7c2e4
Konstantin Sobolev <comediant683@mail.ru>
2024-02-07 16:52:04 +0300

    commit test and Classes

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
+
+def test_item(item):
+    assert item.name == "Смартфон"
+    assert item.price == 10000
+    assert item.quantity == 20
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+def test_apply_discount(item):
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8000.0
+
+
+
