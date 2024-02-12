ab1e980972b57345e629488effba247ac99a1ddf
mariaonderoz <mskorneeva.87@gmail.com>
2024-02-12 11:53:38 +0700

    added test_init_phone test_number_of_sim corrected

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -2,8 +2,6 @@ from src.phone import Phone
 import pytest
 
 
-
-
 @pytest.fixture
 def test_phone1():
     return Phone("iPhone 14", 120_000, 5, 2)
@@ -18,8 +16,24 @@ def test_str():
     assert str(phone1) == 'iPhone 14'
 
 
-def test_number_of_sim():
-    phone1 = Phone("iPhone 14", 120_000, 5, 2)
-    assert phone1.number_of_sim == 2
+def test_init_phone(test_phone1):
+    assert test_phone1.name == "iPhone 14"
+    assert test_phone1.price == 120_000
+    assert test_phone1.quantity == 5
+    assert test_phone1.number_of_sim == 2
+
+
+def test_number_of_sim(test_phone1):
+    with pytest.raises(ValueError):
+        test_phone1.number_of_sim = 0
+        test_phone1.number_of_sim = -1
+        test_phone1.number_of_sim = 1.5
+
+
+
+
+
+
+
 
 
