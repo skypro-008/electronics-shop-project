a24126915d1d0b1891d49c6bc1b12ae2c7fd8ad9
mariaonderoz <mskorneeva.87@gmail.com>
2024-02-12 11:56:50 +0700

    modified

--- src/phone.py
+++ src/phone.py
@@ -1,6 +1,5 @@
 from src.item import Item
 
-
 class Phone(Item):
     """Класс содержит все атрибуты класса `Item` и дополнительно атрибут, содержащий количество поддерживаемых сим-карт"""
 
@@ -17,7 +16,7 @@ class Phone(Item):
         if sim > 0:
             self.__number_of_sim = sim
         else:
-            print('ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.')
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
