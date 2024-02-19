4964c90e0c17f01b7409d5509ae0e87d4173f4b6
Vadim <vadimka.abazov@gmail.com>
2024-02-19 15:55:08 +0700

    hw1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+data = Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price():
+    assert data.calculate_total_price() == 200000
+    assert data.quantity == 20
+
+
+def test_apply_discount():
+    data.pay_rate = 0.8
+    data.apply_discount()
+    assert data.price == 8000.0
