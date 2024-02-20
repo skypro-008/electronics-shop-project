ab4371577a53c3e4b9e87d8c0b12277b986ae5b4
vsymnyivs <vsymnyivs@gmail.com>
2024-02-20 21:03:10 +0300

    add function home work 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +16,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
-
 
     def calculate_total_price(self):
         """
@@ -28,9 +30,41 @@ class Item:
         total_price = self.price * self.quantity
         return total_price
 
-
     def apply_discount(self):
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name_item(self):
+        return self.__name
+
+    @name_item.setter
+    def name_item(self, name):
+        """
+        Проверка на длину наименования товаров (не более 10 символов)
+        """
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, csv_file):
+        """
+        Добавление экземпляров класса из csv файла
+        """
+        cls.all = []
+        with open(csv_file) as csv_file:
+            data = csv.DictReader(csv_file)
+            for item in data:
+                cls(str(item["name"]), float(item["price"]), int(item["quantity"]))
+
+    @staticmethod
+    def string_to_number(str_number):
+        """
+        Возвращает число из числа-строки
+        """
+        number = float(str_number)
+        return int(number)
