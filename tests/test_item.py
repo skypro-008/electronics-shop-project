369d83dac38a527751d199d03f1976376e6a5c62
Max <mack.mair@yandex.ru>
2024-02-21 20:33:42 +0300

    test

---
+++ tests/test_item.py
@@ -0,0 +1,19 @@
+from src.item import Item
+
+date = Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price():
+    """Тест проверки выплнения метода total price"""
+    assert date.calculate_total_price() == 200000
+    assert date.quantity == 20
+
+
+def test_apply_discount():
+    """Тест проверки выплнения метода установочной скидки для конкретного товара"""
+    date.pay_rate = 0.8
+    date.apply_discount()
+    assert date.price == 8000.0
+
+
+"""Здесь надо написать тесты с использованием pytest для модуля item."""
