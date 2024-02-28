3e9e0e22f01e306836a9b934a9b53194f26104c8
vsymnyivs <vsymnyivs@gmail.com>
2024-02-28 21:30:47 +0300

    fix function in item.py in home work 3, and add tests in test_item.py for in home work 3

--- src/item.py
+++ src/item.py
@@ -7,11 +7,9 @@ class Item:
     """
     pay_rate = 1.0
     all = []
-
-    def __init__(self, name: str, price: float, quantity: int):
+    def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
@@ -21,47 +19,50 @@ class Item:
         self.quantity = quantity
         self.all.append(self)
 
-    def calculate_total_price(self):
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
-        total_price = self.price * self.quantity
-        return total_price
-
-    def apply_discount(self):
+        full_price = self.price * self.quantity
+        return full_price
+    def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
-
     @property
     def name(self):
         return self.__name
-
     @name.setter
     def name(self, name):
-        """Функция проверяет длину наименования товара, чтобы было не более 10 символов"""
+        """
+        Функция проверяет длину наименования товара, чтобы было не более 10 символов
+        """
         if len(name) > 10:
             self.__name = name[:10]
         else:
             self.__name = name
-
     @classmethod
-    def instantiate_from_csv(cls, path='../src/items.csv'):
+    def instantiate_from_csv(cls, csv_file):
         """
-        Создает экземпляры класса Item из данных в файле items.csv
+        Добавление экземпляра класса из csv файла
         """
         cls.all = []
-        with open(path, 'r', encoding='windows-1251') as csv_file:
-            reader = csv.DictReader(csv_file)
-            rows = list(reader)
-            for row in rows:
-                Item(str(row['name']), float(row['price']), int(row['quantity']))
-
+        with open(csv_file, newline="", encoding="windows-1251") as file:
+            data = csv.DictReader(file)
+            for item in data:
+                cls(str(item["name"]), float(item["price"]), int(item["quantity"]))
     @staticmethod
     def string_to_number(str_number):
-        """Возвращает число из числа-строки"""
+        """
+        Возвращает число из числа-строки
+        """
         number = float(str_number)
         return int(number)
