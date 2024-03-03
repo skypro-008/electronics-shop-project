40f889b79b3ddab6d01ba1992e7cdd2a5b37518e
Alex <modernswk@gmail.com>
2024-03-03 11:56:19 +0300

    hw 2

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,4 +1,13 @@
+
+import os
+import sys
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.join(current_directory, '..')
+sys.path.append(parent_directory)
+
 from src.item import Item
+
 
 if __name__ == '__main__':
     item = Item('Телефон', 10000, 5)
@@ -11,7 +20,8 @@ if __name__ == '__main__':
     item.name = 'СуперСмартфон'
     # Exception: Длина наименования товара превышает 10 символов.
 
-    Item.instantiate_from_csv('src','items.csv')  # создание объектов из данных файла
+    csv_file_path = os.path.join(parent_directory, 'src', 'items.csv')
+    Item.instantiate_from_csv(csv_file_path)  # создание объектов из данных файла
     assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
 
     item1 = Item.all[0]
